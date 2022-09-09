71e930766c60b9d2cc995367d6499899a2646f94
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2022-09-09 12:59:19 +0200

    rules fix cred

--- lib/cors.go
+++ lib/cors.go
@@ -5,5 +5,7 @@ import "net/http"
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	(*w).Header().Set("Access-Control-Max-Age", "3600")
 	(*w).Header().Set("Content-Type", "application/json")
 }
