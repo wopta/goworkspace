86481980b823e36c57c74a5e6a70e18161703723
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2022-11-05 15:07:59 +0100

    calim insert user fix

--- models/claim.go
+++ models/claim.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"encoding/json"
-	"time"
 )
 
 func UnmarshalClaim(data []byte) (Claim, error) {
@@ -17,16 +16,19 @@ func (r *Claim) Marshal() ([]byte, error) {
 }
 
 type Claim struct {
-	InsertDate  time.Time    `firestore:"insertDate,omitempty" json:"insertDate,omitempty"`
-	Updated     string       `firestore:"updated,omitempty" json:"updated,omitempty"`
-	Company     string       `firestore:"company,omitempty" json:"company,omitempty"`
-	Policy      string       `firestore:"policy,omitempty" json:"policy,omitempty"`
-	Description string       `firestore:"description,omitempty" json:"description,omitempty"`
-	IdCompany   string       `firestore:"idCompany,omitempty" json:"idCompany,omitempty"`
-	Uid         string       `firestore:"uid,omitempty" json:"uid,omitempty"`
-	Status      string       `firestore:"status,omitempty" json:"status,omitempty"`
-	Document    []Attachment `firestore:"attachment,omitempty" json:"attachment,omitempty"`
-	History     []Claim      `firestore:"history,omitempty" json:"history,omitempty"`
+	PolicyDescription string       `firestore:"policyDescription,omitempty" json:"policyDescription,omitempty"`
+	PolicyId          string       `firestore:"policyId,omitempty" json:"policyId,omitempty"`
+	PolicyNumber      string       `firestore:"policyNumber,omitempty" json:"policyNumber,omitempty"`
+	CreationDate      string       `firestore:"creationDate,omitempty" json:"creationDate,omitempty"`
+	Updated           string       `firestore:"updated,omitempty" json:"updated,omitempty"`
+	Company           string       `firestore:"company,omitempty" json:"company,omitempty"`
+	Policy            string       `firestore:"policy,omitempty" json:"policy,omitempty"`
+	Description       string       `firestore:"description,omitempty" json:"description,omitempty"`
+	IdCompany         string       `firestore:"idCompany,omitempty" json:"idCompany,omitempty"`
+	Uid               string       `firestore:"uid,omitempty" json:"uid,omitempty"`
+	Status            string       `firestore:"status,omitempty" json:"status,omitempty"`
+	Document          []Attachment `firestore:"attachment,omitempty" json:"attachment,omitempty"`
+	History           []Claim      `firestore:"history,omitempty" json:"history,omitempty"`
 }
 
 type Attachment struct {
--- models/user.go
+++ models/user.go
@@ -14,21 +14,24 @@ func (r *User) Marshal() ([]byte, error) {
 }
 
 type User struct {
-	Name         string  `firestore:"name" json:"name,omitempty"`
-	Type         string  `firestore:"type" json:"type,omitempty"`
-	Cluster      string  `firestore:"cluster" json:"cluster,omitempty"`
-	Surname      string  `firestore:"surname" json:"surname,omitempty"`
-	Address      string  `firestore:"address" json:"address,omitempty"`
-	PostalCode   string  `firestore:"postalCode" json:"postalCode,omitempty"`
-	Role         string  `firestore:"role" json:"role,omitempty"`
-	Work         string  `firestore:"work" json:"work,omitempty"`
-	WorkType     string  `firestore:"workType" json:"workType,omitempty"`
-	Mail         string  `firestore:"mail" json:"mail,omitempty"`
-	Phone        string  `firestore:"phone" json:"phone,omitempty"`
-	FiscalCode   string  `firestore:"fiscalCode" json:"fiscalCode,omitempty"`
-	VatCode      string  `firestore:"vatCode" json:"vatCode,omitempty"`
-	RiskClass    string  `firestore:"riskClass" json:"riskClass,omitempty"`
-	CreationDate string  `firestore:"creationDate" json:"creationDate,omitempty"`
-	UpdatedDate  string  `firestore:"updatedDate" json:"updatedDate,omitempty"`
-	Claims       []Claim `firestore:"claims" json:"claims,omitempty"`
+	EmailVerified bool     `firestore:"emailVerified" json:"emailVerified,omitempty"`
+	Uid           string   `firestore:"uid" json:"uid,omitempty"`
+	Name          string   `firestore:"name" json:"name,omitempty"`
+	Type          string   `firestore:"type" json:"type,omitempty"`
+	Cluster       string   `firestore:"cluster" json:"cluster,omitempty"`
+	Surname       string   `firestore:"surname" json:"surname,omitempty"`
+	Address       string   `firestore:"address" json:"address,omitempty"`
+	PostalCode    string   `firestore:"postalCode" json:"postalCode,omitempty"`
+	Role          string   `firestore:"role" json:"role,omitempty"`
+	Work          string   `firestore:"work" json:"work,omitempty"`
+	WorkType      string   `firestore:"workType" json:"workType,omitempty"`
+	Mail          string   `firestore:"mail" json:"mail,omitempty"`
+	Phone         string   `firestore:"phone" json:"phone,omitempty"`
+	FiscalCode    string   `firestore:"fiscalCode" json:"fiscalCode,omitempty"`
+	VatCode       string   `firestore:"vatCode" json:"vatCode,omitempty"`
+	RiskClass     string   `firestore:"riskClass" json:"riskClass,omitempty"`
+	CreationDate  string   `firestore:"creationDate,omitempty" json:"creationDate,omitempty"`
+	UpdatedDate   string   `firestore:"updatedDate" json:"updatedDate,omitempty"`
+	PoliciesUid   []string `firestore:"policiesUid" json:"policiesUid,omitempty"`
+	Claims        []Claim  `firestore:"claims" json:"claims,omitempty"`
 }
