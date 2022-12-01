2fd79bc3126fb9258a3393c5cf3cb6909655ab3b
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2022-12-01 11:01:26 +0100

    fix dep

--- payment/payment.go
+++ payment/payment.go
@@ -61,7 +61,7 @@ func FabrickPayObj(data model.Policy) <-chan string {
 		log.Println("FabrickPay")
 		//var b bytes.Buffer
 		//fileReader := bytes.NewReader([]byte())
-		var urlstring = os.Getenv("FABRICK_BASEURL") + "api/fabrick/pace/v4.0/mods/standard-payment-apis/v1.0/payments/"
+		var urlstring = os.Getenv("FABRICK_BASEURL") + "api/fabrick/pace/v4.0/mods/back/v1.0/payments/"
 		client := &http.Client{
 			Timeout: time.Second * 2,
 		}
