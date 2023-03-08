15e19ccc1a470779258e093e2c4044eb82ceefad
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-03-08 16:19:59 +0100

    fix dep

--- _script/updateAllDep.bat
+++ _script/updateAllDep.bat
@@ -5,10 +5,7 @@ set location="callback"
 set v="0.6  .0.dev"
 set m="major allineament"
 cd ..
-git add .
-git commit -m %m%
-git push work master 
-git push google master  
+
 cd document
 go get github.com/wopta/goworkspace/lib
 go get github.com/wopta/goworkspace/models
@@ -56,7 +53,3 @@ go get github.com/wopta/goworkspace/mail
 cd ..
 cd form
 go get github.com/wopta/goworkspace/lib
-git add .
-git commit -m %m%
-git push work master 
-git push google master 
