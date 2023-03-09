caffb9314081d31cd5802bc44e8eb8fe5faed84b
Nelson Hidalgo <nelson.herrera@wopta.it>
2023-03-09 12:15:28 +0100

    Updated user domain

--- user/can-user-register-use-case.go
+++ user/can-user-register-use-case.go
@@ -2,22 +2,49 @@ package user
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+
+	"golang.org/x/exp/slices"
 
 	lib "github.com/wopta/goworkspace/lib"
 	models "github.com/wopta/goworkspace/models"
 	wiseProxy "github.com/wopta/goworkspace/wiseproxy"
 )
 
-func CanUserRegisterUseCase(fiscalCode string) (bool, *string) {
+func CanUserRegisterUseCase(fiscalCode string) (bool, *string, *string) {
 	var (
 		wiseResponse models.WiseUserRegistryResponseDto
 		e            error
 		userId       string
 		canRegister  bool
+		hasWiseData  bool
+		email        string
 	)
 
+	hasWiseData, e = userHasDataInWise(fiscalCode, &wiseResponse)
+	if hasWiseData {
+		log.Println("Found a register in wise for the user")
+		canRegister = true
+	}
+
+	// Check for policies in Firestore
+	woptaUser, e := GetUserByFiscalCode(fiscalCode)
+
+	if e == nil {
+		userId = woptaUser.Uid
+		email = woptaUser.Mail
+		log.Println(`Found user in Wopta DB for the user ` + userId)
+		canRegister = true
+	} else {
+		log.Printf(`Error trying to find user in Firebase %v\n`, e)
+	}
+
+	return canRegister, &userId, &email
+}
+
+func userHasDataInWise(fiscalCode string, wiseResponse *models.WiseUserRegistryResponseDto) (bool, error) {
 	request := []byte(`{
 		"idNodo": "1",
 		"cdFiscale": "` + fiscalCode + `",
@@ -26,24 +53,30 @@ func CanUserRegisterUseCase(fiscalCode string) (bool, *string) {
 
 	responseReader := wiseProxy.WiseProxyObj("WebApiProduct/Api/RicercaAnagSemplice", request, "POST")
 	jsonData, e := ioutil.ReadAll(responseReader)
-	lib.CheckError(e)
-
+	
+	if e != nil {
+		return false, e
+	}
+	
+	log.Printf("%s", jsonData)
 	e = json.Unmarshal(jsonData, &wiseResponse)
-	lib.CheckError(e)
-	if len(*wiseResponse.UserRegistries) > 0 {
-		log.Println("Found a register in wise for the user")
-		canRegister = true
+	
+	if wiseResponse == nil || len(*wiseResponse.UserRegistries) == 0 {
+		return false, e
 	}
 
-	// Check for policies in Firestore
-	rn := lib.WhereLimitFirestore("policy", "contractor.fiscalCode", "==", fiscalCode, 1)
+	idx := slices.IndexFunc((*wiseResponse.UserRegistries), func(registry models.WiseUserRegistryDto) bool { return registry.RegistryType == "PERSONA FISICA" })
+	subjectId := (*wiseResponse.UserRegistries)[idx].Id
+	request = []byte(`{
+		"idNodo": "1",
+		"cdFiscale": "` + fiscalCode + `",
+		"idSoggetto": "` + fmt.Sprint(subjectId) + `",
+		"cdLingua": "it"
+	}`)
+	responseReader = wiseProxy.WiseProxyObj("WebApiProduct/Api/RicercaAnagCompleta", request, "POST")
+	jsonData, e = ioutil.ReadAll(responseReader)
+	log.Printf("%s", jsonData)
 	lib.CheckError(e)
-	policies := models.PolicyToListData(rn)
-	if len(policies) > 0 {
-		userId = policies[0].Contractor.Uid
-		log.Println(`Found a policy in Wopta DB for the user ` + userId)
-		canRegister = true
-	}
 
-	return canRegister, &userId
+	return len(*wiseResponse.UserRegistries) > 0, e
 }
