b01525584b317191f82b82a46e05d07f9cd522b5
Nelson Hidalgo <nelson.herrera@wopta.it>
2023-03-28 01:32:25 +0200

    Added Wise Annex mapping

--- models/wise-complete-policy.go
+++ models/wise-complete-policy.go
@@ -1,8 +1,6 @@
 package models
 
-import (
-	"time"
-)
+import "time"
 
 type WiseCompletePolicy struct {
 	Id              int              `json:"idPolizza"`
@@ -19,7 +17,9 @@ type WiseCompletePolicy struct {
 
 func (wisePolicy *WiseCompletePolicy) ToDomain() Policy {
 	var (
-		policy Policy
+		policy     Policy
+		wiseToken  *string = nil
+		attachment Attachment
 	)
 
 	for _, wiseAsset := range *wisePolicy.Assets {
@@ -27,7 +27,16 @@ func (wisePolicy *WiseCompletePolicy) ToDomain() Policy {
 	}
 	policy.Contractor = *wisePolicy.Contractors[0].Registry.ToDomain()
 	policy.EndDate = wisePolicy.Contract.PolicyExpirationDate
-	// policy.EmitDate = wisePolicy.Events.f
+
+	policy.Attachments = &[]Attachment{}
+	for _, wiseAttachment := range wisePolicy.Attachments {
+		attachment, wiseToken = wiseAttachment.ToDomain(wiseToken)
+		*policy.Attachments = append(*policy.Attachments, attachment)
+	}
 
 	return policy
+}
+
+type WiseBase64Annex struct {
+	Bytes string `json:"fileAllegato"`
 }
