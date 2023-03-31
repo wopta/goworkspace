0d9eefe49fe24b79eb9840e5315318fd13a14e47
Yousef Hammar <yousef.hammar@wopta.it>
2023-03-31 15:23:45 +0200

    refactor

--- quote/life.go
+++ quote/life.go
@@ -32,14 +32,9 @@ func Life(data models.Policy) (models.Policy, error) {
 	df := lib.CsvToDataframe(b)
 	var selectRow []string
 
-	deathSumInsuredLimitOfIndemnity := 0.0
-	for _, asset := range data.Assets {
-		for _, guarantee := range asset.Guarantees {
-			if guarantee.Slug == "death" {
-				deathSumInsuredLimitOfIndemnity = guarantee.SumInsuredLimitOfIndemnity
-			}
-		}
-	}
+	deathSumInsuredLimitOfIndemnity := getDeathSumInsuredLimitOfIndemnty(data)
+
+	calculateSumInsuredLimitOfIndemnity(data, deathSumInsuredLimitOfIndemnity)
 
 	for _, row := range df.Records() {
 		if row[0] == strconv.Itoa(year) {
@@ -61,56 +56,104 @@ func Life(data models.Policy) (models.Policy, error) {
 			var baseTax int
 			var offset int
 
+			base, baseTax = getMultipliersIndex(guarantee.Slug, base, baseTax)
+
+			offset = getOffset(guarantee, offset)
+
+			baseFloat, taxFloat := getMultipliers(selectRow, base, offset, baseTax)
+
+			calculateGuaranteePrices(guarantee, baseFloat, taxFloat)
+
+			calculateOfferPrices(data, guarantee)
+		}
+
+	}
+
+	return data, err
+}
+
+func calculateOfferPrices(data models.Policy, guarantee models.Guarante) {
+	data.OffersPrices["default"]["yearly"].Gross += guarantee.Offer["default"].PremiumGrossYearly
+	data.OffersPrices["default"]["yearly"].Net += guarantee.Offer["default"].PremiumNetYearly
+	data.OffersPrices["default"]["monthly"].Gross += guarantee.Offer["default"].PremiumGrossMonthly
+	data.OffersPrices["default"]["monthly"].Net += guarantee.Offer["default"].PremiumNetMonthly
+}
+
+func calculateGuaranteePrices(guarantee models.Guarante, baseFloat float64, taxFloat float64) {
+	guarantee.Offer["default"].PremiumNetYearly = guarantee.Value.SumInsuredLimitOfIndemnity * baseFloat
+	guarantee.Offer["default"].PremiumGrossYearly = guarantee.Value.SumInsuredLimitOfIndemnity * taxFloat
+	guarantee.Offer["default"].PremiumNetMonthly = guarantee.Value.SumInsuredLimitOfIndemnity * baseFloat / 12
+	guarantee.Offer["default"].PremiumGrossMonthly = guarantee.Value.SumInsuredLimitOfIndemnity * taxFloat / 12
+}
+
+func getMultipliers(selectRow []string, base int, offset int, baseTax int) (float64, float64) {
+	baseFloat, _ := strconv.ParseFloat(strings.Replace(strings.Replace(selectRow[base+offset], "%", "", 1), ",", ".", 1), 64)
+	taxFloat, _ := strconv.ParseFloat(strings.Replace(strings.Replace(selectRow[baseTax+offset], "%", "", 1), ",", ".", 1), 64)
+	baseFloat = baseFloat / 100
+	taxFloat = taxFloat / 100
+	return baseFloat, taxFloat
+}
+
+func getMultipliersIndex(guaranteeSlug string, base int, baseTax int) (int, int) {
+	switch guaranteeSlug {
+	case "death":
+		base = 1
+		baseTax = 2
+	case "permanent-disability":
+		base = 3
+		baseTax = 4
+	case "temporary-disability":
+		base = 5
+		baseTax = 6
+	case "serious-ill":
+		base = 7
+		baseTax = 8
+	}
+	return base, baseTax
+}
+
+func getOffset(guarantee models.Guarante, offset int) int {
+	switch guarantee.Value.Duration.Year {
+	case 5:
+		offset = 8 * 0
+	case 10:
+		offset = 8 * 1
+	case 15:
+		offset = 8 * 2
+	case 20:
+		offset = 8 * 3
+	}
+	return offset
+}
+
+func getDeathSumInsuredLimitOfIndemnty(data models.Policy) float64 {
+	deathSumInsuredLimitOfIndemnity := 0.0
+	for _, asset := range data.Assets {
+		for _, guarantee := range asset.Guarantees {
+			if guarantee.Slug == "death" {
+				deathSumInsuredLimitOfIndemnity = guarantee.SumInsuredLimitOfIndemnity
+				break
+			}
+		}
+	}
+	return deathSumInsuredLimitOfIndemnity
+}
+
+func calculateSumInsuredLimitOfIndemnity(data models.Policy, deathSumInsuredLimitOfIndemnity float64) {
+	for _, asset := range data.Assets {
+		for _, guarantee := range asset.Guarantees {
 			switch guarantee.Slug {
-			case "death":
-				base = 1
-				baseTax = 2
 			case "permanent-disability":
-				base = 3
-				baseTax = 4
 				guarantee.SumInsuredLimitOfIndemnity = deathSumInsuredLimitOfIndemnity
 			case "temporary-disability":
-				base = 5
-				baseTax = 6
 				guarantee.SumInsuredLimitOfIndemnity = (deathSumInsuredLimitOfIndemnity / 100) * 1
 			case "serious-ill":
-				base = 7
-				baseTax = 8
 				if deathSumInsuredLimitOfIndemnity > 100000 {
 					guarantee.SumInsuredLimitOfIndemnity = 10000
 				} else {
 					guarantee.SumInsuredLimitOfIndemnity = 5000
 				}
 			}
-
-			switch guarantee.Value.Duration.Year {
-			case 5:
-				offset = 8 * 0
-			case 10:
-				offset = 8 * 1
-			case 15:
-				offset = 8 * 2
-			case 20:
-				offset = 8 * 3
-			}
-
-			baseFloat, _ := strconv.ParseFloat(strings.Replace(strings.Replace(selectRow[base+offset], "%", "", 1), ",", ".", 1), 64)
-			taxFloat, _ := strconv.ParseFloat(strings.Replace(strings.Replace(selectRow[baseTax+offset], "%", "", 1), ",", ".", 1), 64)
-			baseFloat = baseFloat / 100
-			taxFloat = taxFloat / 100
-
-			guarantee.Offer["default"].PremiumNetYearly = guarantee.Value.SumInsuredLimitOfIndemnity * baseFloat
-			guarantee.Offer["default"].PremiumGrossYearly = guarantee.Value.SumInsuredLimitOfIndemnity * taxFloat
-			guarantee.Offer["default"].PremiumNetMonthly = guarantee.Value.SumInsuredLimitOfIndemnity * baseFloat / 12
-			guarantee.Offer["default"].PremiumGrossMonthly = guarantee.Value.SumInsuredLimitOfIndemnity * taxFloat / 12
-
-			data.OffersPrices["default"]["yearly"].Gross += guarantee.Offer["default"].PremiumGrossYearly
-			data.OffersPrices["default"]["yearly"].Net += guarantee.Offer["default"].PremiumNetYearly
-			data.OffersPrices["default"]["monthly"].Gross += guarantee.Offer["default"].PremiumGrossMonthly
-			data.OffersPrices["default"]["monthly"].Net += guarantee.Offer["default"].PremiumNetMonthly
 		}
-
 	}
-
-	return data, err
 }
