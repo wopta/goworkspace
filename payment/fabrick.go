9bdbc5b3787d6cf0cf25a84a4da7220f3271a91d
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-04-06 09:36:04 +0200

    fix description

--- payment/fabrick.go
+++ payment/fabrick.go
@@ -107,7 +107,7 @@ func FabbrickMontlyPay(data model.Policy) FabrickPaymentResponse {
 	log.Println(data.Uid + " FabbrickMontlyPay")
 	layout := "2006-01-02"
 	firstres := <-FabrickPayObj(data, true, data.StartDate.Format(layout), customerId, installment)
-	time.Sleep(500)
+	time.Sleep(100)
 	for i := 1; i <= 11; i++ {
 		date := data.StartDate.AddDate(0, i, 0)
 		res := <-FabrickPayObj(data, false, date.Format(layout), customerId, installment)
