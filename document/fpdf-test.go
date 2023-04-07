e5f0171043786f5a13c60f4b1b6aeddd28826b8c
Yousef Hammar <yousef.hammar@wopta.it>
2023-04-07 16:47:44 +0200

    refactor

--- document/fpdf-test.go
+++ document/fpdf-test.go
@@ -21,7 +21,7 @@ func FpdfHandler(w http.ResponseWriter, r *http.Request) (string, interface{}, e
 }
 
 func initFpdf() *fpdf.Fpdf {
-	pdf := fpdf.New("P", "mm", "A4", "")
+	pdf := fpdf.New(fpdf.OrientationPortrait, fpdf.UnitMillimeter, fpdf.PageSizeA4, "")
 	pdf.SetMargins(10, 15, 10)
 	loadCustomFonts(pdf)
 	return pdf
@@ -47,26 +47,29 @@ func GetHeader(pdf *fpdf.Fpdf, name string) {
 	pdf.SetHeaderFunc(func() {
 		opt.ImageType = "png"
 		pdf.ImageOptions(logoPath, 10, 6, 13, 13, false, opt, 0, "")
-		pdf.SetXY(23, 6)
+		pdf.SetXY(23, 7)
 		pdf.SetTextColor(229, 0, 117)
 		pdf.SetFont("Montserrat", "B", 18)
-		pdf.Text(24, 12, "Wopta per te")
+		pdf.Cell(10, 6, "Wopta per te")
 		pdf.SetFont("Montserrat", "I", 18)
+		pdf.SetXY(23, 13)
 		pdf.SetTextColor(92, 89, 92)
-		pdf.Text(24, 18, product)
+		pdf.Cell(10, 6, product)
 		pdf.ImageOptions("document/assets/ARTW_LOGO_RGB_400px.png", 158, 6, 0, 10, false, opt, 0, "")
 
 		pdf.SetTextColor(0, 0, 0)
 		pdf.SetFont("Montserrat", "B", 8)
-		pdf.Text(12, 23, "I dati della tua polizza")
+		pdf.SetXY(11, 20)
+		pdf.Cell(0, 3, "I dati della tua polizza")
 		pdf.SetFont("Montserrat", "", 8)
-		pdf.SetXY(11, pdf.GetY()+18)
+		pdf.SetXY(11, pdf.GetY()+3)
 		pdf.MultiCell(0, 3, "Numero: 12345\nDecorre dal: 03/04/2023 ore 24:00\nScade il: 03/04/2043 ore 24:00\nPrima scadenza annuale il: 04/04/2024\nNon si rinnova a scadenza.", "", "", false)
 
 		pdf.SetFont("Montserrat", "B", 8)
-		pdf.Text(120, 23, "I tuoi dati")
+		pdf.SetXY(-90, 20)
+		pdf.Cell(0, 3, "I tuoi dati")
 		pdf.SetFont("Montserrat", "", 8)
-		pdf.SetXY(119, 24)
+		pdf.SetXY(-90, pdf.GetY()+3)
 		pdf.MultiCell(0, 3, "Contraente: HAMMAR YOUSEF\nC.F./P.IVA: HMMYSF94R07D912M\nIndirizzo: Via Unicef, 4\n20033 SOLARO (MI)\nMail: yousef.hammar@wopta.it\nTelefono: +393451031004", "", "", false)
 	})
 }
