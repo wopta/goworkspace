4a2fac5a9f8455dafffd335463e64933bdeda0b4
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-04-16 22:15:55 +0200

    fix data

--- lib/enviroment.go
+++ lib/enviroment.go
@@ -4,7 +4,7 @@ import "strings"
 
 func GetDatasetByContractorName(name string, dataset string) string {
 	var result string
-	if strings.Contains(name, "@") {
+	if strings.Contains(name, "Woptatest") {
 		result = "uat-" + dataset
 	} else {
 		result = dataset
