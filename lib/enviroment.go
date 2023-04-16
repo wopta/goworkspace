b57106e1dffa7f86f71c8d216debc826f6542a81
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-04-17 01:00:38 +0200

    fix data

--- lib/enviroment.go
+++ lib/enviroment.go
@@ -16,3 +16,14 @@ func GetDatasetByContractorName(name string, dataset string) string {
 	log.Println("GetDatasetByContractorName result: ", result)
 	return result
 }
+func GetDatasetByEnv(origin string, dataset string) string {
+	var result string
+	if strings.Contains(origin, "uat") || strings.Contains(origin, "dev") {
+		result = "uat_" + dataset
+	} else {
+		result = dataset
+	}
+	log.Println("GetDatasetByEnv: name:", origin)
+	log.Println("GetDatasetByEnv result: ", result)
+	return result
+}
