24111545c1f6e84638f53ea76a9aaaa066be71f6
Yousef Hammar <yousef.hammar@wopta.it>
2023-05-02 11:25:51 +0200

    added sellable domain

---
+++ sellable/go.mod
@@ -0,0 +1,5 @@
+module github.com/wopta/goworkspace/sellable
+
+go 1.19
+
+replace github.com/wopta/goworkspace/sellable => ./
---
+++ sellable/sellable.go
@@ -0,0 +1,30 @@
+package sellable
+
+import (
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/wopta/goworkspace/lib"
+	"log"
+	"net/http"
+)
+
+func init() {
+	log.Println("INIT Sellable")
+
+	functions.HTTP("Sellable", Sellable)
+}
+
+func Sellable(w http.ResponseWriter, r *http.Request) {
+	lib.EnableCors(&w, r)
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	route := lib.RouteData{
+		Routes: []lib.Route{
+			/*{
+				Route:   "/v1/sales/life",
+				Handler: Life,
+				Method:  http.MethodPost,
+			},*/
+		},
+	}
+	route.Router(w, r)
+
+}
