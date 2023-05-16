e20747ea40478d0dd494cf33e8a8c5b3d6111c2d
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-05-17 01:08:29 +0200

    fix Employer

--- lib/excel.go
+++ lib/excel.go
@@ -11,17 +11,10 @@ import (
 )
 
 func ExcelRead(r io.Reader) (map[string][][]string, error) {
-	// f, err := excelize.OpenFile("Book1.xlsx")
 	var res map[string][][]string
 	var rows [][]string
 	var err error
 	f, err := excelize.OpenReader(r, excelize.Options{})
-
-	// Get value from cell by given worksheet name and cell reference.
-	//cell, err := f.GetCellValue("Sheet1", "B2")
-	//fmt.Println(cell)
-	// Get all the rows in the Sheet1.
-
 	for _, sheet := range f.GetSheetList() {
 		rows, err = f.GetRows(sheet)
 		res[sheet] = rows
