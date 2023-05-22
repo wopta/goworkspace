a5158dab8812fdc9801647c10be4201836a60ba5
Yousef Hammar <yousef.hammar@wopta.it>
2023-05-22 12:55:48 +0200

    added origin to PutProduct API

--- product/product.go
+++ product/product.go
@@ -134,14 +134,16 @@ func GetProduct(name string, version string) (models.Product, error) {
 
 func PutFx(resp http.ResponseWriter, r *http.Request) (string, interface{}, error) {
 	log.Println(productCollection)
+	origin := r.Header.Get("origin")
 	request := lib.ErrorByte(io.ReadAll(r.Body))
 	pr, e := models.UnmarshalProduct([]byte(request))
-	p, e := Put(pr)
+	p, e := Put(origin, pr)
 	return "{}", p, e
 }
 
-func Put(p models.Product) (models.Product, error) {
-	r, _, e := lib.PutFirestoreErr("products", p)
+func Put(origin string, p models.Product) (models.Product, error) {
+	fireProducts := lib.GetDatasetByEnv(origin, "products")
+	r, _, e := lib.PutFirestoreErr(fireProducts, p)
 	log.Println(r.ID)
 
 	return p, e
