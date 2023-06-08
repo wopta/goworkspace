6490aa5c04b2bfd1a8ecd4c0529937f306e178d5
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-06-08 09:16:19 +0200

    company data

--- companydata/life-axa-emit.go
+++ companydata/life-axa-emit.go
@@ -31,7 +31,7 @@ func LifeAxalEmit(w http.ResponseWriter, r *http.Request) (string, interface{}, 
 	dateString := "2021-11-22"
 	date, _ := time.Parse("2006-01-02", dateString)
 	log.Println(date)
-	if now.Day() == 15 {
+	if now.Day() == 15 || now.Day() == 15 {
 		from = fromQ
 		filenamesplit = "Q"
 	} else {
