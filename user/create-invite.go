7b6ea152e6718299d6df1ad8c46e5beda776d0ab
Nelson Hidalgo <nelson.herrera@wopta.it>
2023-06-13 01:24:46 +0200

    Changed mail for invite creation

--- user/create-invite.go
+++ user/create-invite.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/wopta/goworkspace/lib"
@@ -59,6 +60,24 @@ func SendInviteMail(inviteUid, email string) {
 	mailRequest.Content = `<p>` + inviteUid + `</p>`
 	mailRequest.Subject = "Benvenuto in Wopta!"
 	mailRequest.IsHtml = true
+
+	lines := []string{
+		"Ciao,",
+		"Ecco il tuo invito al tuo account wopta.it.",
+		"Accedi al link sottostante e crea la tua password.",
+	}
+	for _, line := range lines {
+		mailRequest.Message = mailRequest.Message + `<p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#000000;font-size:14px">` + line + `</p>`
+	}
+
+	mailRequest.Message = mailRequest.Message + ` 
+	<p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#000000;font-size:14px">Se hai bisogno di ulteriore supporto, non scrivere a questo indirizzo email, puoi compilare il <a class="button" href='` + linkForm + ` '>Form </a> oppure scrivere alla mail e verrai contattato da un nostro esperto.</p>
+	<p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#000000;font-size:14px"><br></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#000000;font-size:14px">A presto,</p>
+	<p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#e50075;font-size:14px"><strong>Anna</strong> di Wopta Assicurazioni</p> `
+	mailRequest.Title = "Invito a wopta.it"
+	mailRequest.IsHtml = true
+	mailRequest.IsLink = true
+	mailRequest.Link = os.Getenv("WOPTA_CUSTOMER_AREA_BASE_URL") + "/login/inviteregistration?inviteUid=" + inviteUid
 
 	mail.SendMail(mailRequest)
 }
