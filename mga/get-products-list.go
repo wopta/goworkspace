61671fc246e32aa14091a9004f2879a5941e7069
Yousef Hammar <yousef.hammar@wopta.it>
2023-06-22 18:23:45 +0200

    refactor: created multiple function for each channel

--- mga/get-products-list.go
+++ mga/get-products-list.go
@@ -8,6 +8,10 @@ import (
 	"net/http"
 )
 
+const (
+	pathPrefix = "products/"
+)
+
 type GetProductListResp struct {
 	Name    string `json:"name"`
 	Company string `json:"company"`
@@ -15,24 +19,119 @@ type GetProductListResp struct {
 }
 
 func GetProductsListByEntitlementFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
+
 	var (
-		err error
+		err          error
+		userRole     string
+		productsList = make([]GetProductListResp, 0)
 	)
 	log.Println("GetProductsListByEntitlement")
 
-	productsList := make([]models.Product, 0)
+	idToken := r.Header.Get("Authorization")
 
-	res := lib.GetFolderContentByEnv("products/agency")
+	if idToken == "" {
+		productsList = getEcommerceProductsList()
+		output, err := json.Marshal(productsList)
+		return string(output), productsList, err
+	}
 
-	//res := lib.ReadStorageDirContent(os.Getenv("GOOGLE_STORAGE_BUCKET"), "products/agency/")
+	userRole, err = lib.GetUserRoleFromIdToken(idToken)
+	lib.CheckError(err)
 
-	for _, file := range res {
-		var product models.Product
-		err = json.Unmarshal(file, &product)
-		productsList = append(productsList, product)
+	switch userRole {
+	case models.UserRoleAdmin, models.UserRoleManager:
+		productsList = getMgaProductsList()
+	case models.UserRoleAgency:
+		productsList = getAgencyProductsList()
+	case models.UserRoleAgent:
+		productsList = getAgentProductsList()
 	}
 
 	jsonOut, err := json.Marshal(productsList)
 
 	return string(jsonOut), productsList, err
+}
+
+func getMgaProductsList() []GetProductListResp {
+	productsList := make([]GetProductListResp, 0)
+	res := lib.GetFolderContentByEnv(pathPrefix + "agent")
+	for _, file := range res {
+		var product models.Product
+		err := json.Unmarshal(file, &product)
+		lib.CheckError(err)
+		for _, company := range product.Companies {
+			productsList = append(productsList, GetProductListResp{
+				Name:    product.Name,
+				Company: company.Name,
+				Logo:    "",
+			})
+		}
+	}
+	return productsList
+}
+
+func getAgencyProductsList() []GetProductListResp {
+	productsList := make([]GetProductListResp, 0)
+	res := lib.GetFolderContentByEnv(pathPrefix + "agent")
+	for _, file := range res {
+		var product models.Product
+		err := json.Unmarshal(file, &product)
+		lib.CheckError(err)
+		if product.IsAgentActive {
+			for _, company := range product.Companies {
+				if company.IsAgencyActive {
+					productsList = append(productsList, GetProductListResp{
+						Name:    product.Name,
+						Company: company.Name,
+						Logo:    "",
+					})
+				}
+			}
+		}
+	}
+	return productsList
+}
+
+func getAgentProductsList() []GetProductListResp {
+	productsList := make([]GetProductListResp, 0)
+	res := lib.GetFolderContentByEnv(pathPrefix + "agent")
+	for _, file := range res {
+		var product models.Product
+		err := json.Unmarshal(file, &product)
+		lib.CheckError(err)
+		if product.IsAgentActive {
+			for _, company := range product.Companies {
+				if company.IsAgentActive {
+					productsList = append(productsList, GetProductListResp{
+						Name:    product.Name,
+						Company: company.Name,
+						Logo:    "",
+					})
+				}
+			}
+		}
+	}
+	return productsList
+}
+
+func getEcommerceProductsList() []GetProductListResp {
+	productsList := make([]GetProductListResp, 0)
+	res := lib.GetFolderContentByEnv(pathPrefix + "e-commerce")
+	for _, file := range res {
+		var product models.Product
+		err := json.Unmarshal(file, &product)
+		lib.CheckError(err)
+		if product.IsEcommerceActive {
+			for _, company := range product.Companies {
+				if company.IsEcommerceActive {
+					productsList = append(productsList, GetProductListResp{
+						Name:    product.Name,
+						Company: company.Name,
+						Logo:    "",
+					})
+				}
+			}
+		}
+	}
+	return productsList
 }
