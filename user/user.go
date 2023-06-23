9a1ef78b58010e2e2bbad6564393dc06e83391a2
pierlucatonioloreply <125584336+pierlucatonioloreply@users.noreply.github.com>
2023-06-23 09:35:38 +0200

    Wb 376 (#58)
    * add GetUsersFx and UsersToListData
    
    * add checkerror
    
    * refactor: add Query struct to models and refactor GetPolicies and GetUsers
    
    * refactor: changed user role to Admin for GetUsers API
    
    ---------
    
    Co-authored-by: pierlucat <pierluca1992@gmail.com>
    Co-authored-by: Yousef Hammar <yousef.hammar@wopta.it>

--- user/user.go
+++ user/user.go
@@ -81,6 +81,12 @@ func User(w http.ResponseWriter, r *http.Request) {
 				Method:  http.MethodPatch,
 				Roles:   []string{models.UserRoleAdmin},
 			},
+			{
+				Route:   "/v1",
+				Handler: GetUsersFx,
+				Method:  http.MethodPost,
+				Roles:   []string{models.UserRoleAdmin},
+			},
 		},
 	}
 	route.Router(w, r)
