1ddbf09e448d3340fb8dab86c814ec1d4062fb57
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-06-30 16:29:08 +0200

    init inclusive

--- test/test.go
+++ test/test.go
@@ -6,9 +6,13 @@ package test
 import (
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
+	"cloud.google.com/go/civil"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
 )
 
 var (
@@ -28,5 +32,29 @@ func Test(w http.ResponseWriter, r *http.Request) {
 		Routes: []lib.Route{},
 	}
 	route.Router(w, r)
-
+	creationDateFrom := time.Now()
+	q := lib.Firequeries{
+		Queries: []lib.Firequery{
+			{
+				Field:      "creationDate",
+				Operator:   "<",
+				QueryValue: creationDateFrom,
+			},
+		},
+	}
+	fireTransactions := "transactions"
+	query, _ := q.FirestoreWherefields(fireTransactions)
+	transactions := models.TransactionToListData(query)
+	for _, transaction := range transactions {
+		transaction.BigPayDate = lib.GetBigQueryNullDateTime(transaction.PayDate)
+		transaction.BigTransactionDate = lib.GetBigQueryNullDateTime(transaction.TransactionDate)
+		transaction.BigCreationDate = civil.DateTimeOf(transaction.CreationDate)
+		transaction.BigStatusHistory = strings.Join(transaction.StatusHistory, ",")
+		log.Println("Transaction save BigQuery: " + transaction.Uid)
+		err := lib.InsertRowsBigQuery("wopta", fireTransactions, transaction)
+		if err != nil {
+			log.Println("ERROR Transaction "+transaction.Uid+" save BigQuery: ", err)
+			return
+		}
+	}
 }
