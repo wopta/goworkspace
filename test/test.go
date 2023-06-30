f5261de517ee4c4eabd93d5254d487064085e927
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-06-30 18:42:28 +0200

    init inclusive

--- test/test.go
+++ test/test.go
@@ -6,10 +6,8 @@ package test
 import (
 	"log"
 	"net/http"
-	"strings"
 	"time"
 
-	"cloud.google.com/go/civil"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/models"
@@ -29,7 +27,7 @@ func Test(w http.ResponseWriter, r *http.Request) {
 	lib.EnableCors(&w, r)
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
 
-	creationDateFrom := time.Now()
+	creationDateFrom := time.Now().AddDate(0, 0, 9)
 	q := lib.Firequeries{
 		Queries: []lib.Firequery{
 			{
@@ -39,20 +37,28 @@ func Test(w http.ResponseWriter, r *http.Request) {
 			},
 		},
 	}
-	fireTransactions := "transactions"
-	query, _ := q.FirestoreWherefields(fireTransactions)
-	transactions := models.TransactionToListData(query)
-	for i, transaction := range transactions {
-		transaction.BigPayDate = lib.GetBigQueryNullDateTime(transaction.PayDate)
-		transaction.BigTransactionDate = lib.GetBigQueryNullDateTime(transaction.TransactionDate)
-		transaction.BigCreationDate = civil.DateTimeOf(transaction.CreationDate)
-		transaction.BigStatusHistory = strings.Join(transaction.StatusHistory, ",")
+	query, _ := q.FirestoreWherefields("policy")
+	policies := models.PolicyToListData(query)
+	for i, policy := range policies {
 		log.Println(i)
-		log.Println(" Transaction save BigQuery: " + transaction.Uid)
-		err := lib.InsertRowsBigQuery("wopta", fireTransactions, transaction)
-		if err != nil {
-			log.Println("ERROR Transaction "+transaction.Uid+" save BigQuery: ", err)
-
-		}
+		policy.BigquerySave("")
 	}
+	/*
+		fireTransactions := "transactions"
+
+		transactions := models.TransactionToListData(query)
+		for i, transaction := range transactions {
+			transaction.BigPayDate = lib.GetBigQueryNullDateTime(transaction.PayDate)
+			transaction.BigTransactionDate = lib.GetBigQueryNullDateTime(transaction.TransactionDate)
+			transaction.BigCreationDate = civil.DateTimeOf(transaction.CreationDate)
+			transaction.BigStatusHistory = strings.Join(transaction.StatusHistory, ",")
+			log.Println(i)
+			log.Println(" Transaction save BigQuery: " + transaction.Uid)
+			err := lib.InsertRowsBigQuery("wopta", fireTransactions, transaction)
+			if err != nil {
+				log.Println("ERROR Transaction "+transaction.Uid+" save BigQuery: ", err)
+
+			}
+		}
+	*/
 }
