836446466066900c1f3e8b41a73dbe599a7e7cfe
Yousef Hammar <yousef.hammar@wopta.it>
2023-07-03 14:22:22 +0200

    refactor: claim attachments now are saved in assets/users folder on Google Storage

--- claim/claim.go
+++ claim/claim.go
@@ -2,7 +2,7 @@ package claim
 
 import (
 	b64 "encoding/base64"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -13,7 +13,6 @@ import (
 	lib "github.com/wopta/goworkspace/lib"
 	mail "github.com/wopta/goworkspace/mail"
 	"github.com/wopta/goworkspace/models"
-	model "github.com/wopta/goworkspace/models"
 )
 
 func init() {
@@ -24,49 +23,68 @@ func init() {
 func Claim(w http.ResponseWriter, r *http.Request) {
 	log.Println("Claim")
 	lib.EnableCors(&w, r)
-
-	if r.Method == http.MethodGet {
-		w.Header().Set("Access-Control-Allow-Methods", "GET")
-		get(w, r)
+	route := lib.RouteData{
+		Routes: []lib.Route{
+			{
+				Route:   "",
+				Handler: PutClaimFx,
+				Method:  http.MethodPut,
+				Roles:   []string{models.UserRoleAll},
+			},
+		},
 	}
-	if r.Method == http.MethodPut {
-		w.Header().Set("Access-Control-Allow-Methods", "PUT")
-		put(w, r)
-	}
-	if r.Method == http.MethodPost {
-		w.Header().Set("Access-Control-Allow-Methods", "POST")
-		put(w, r)
-	}
+	route.Router(w, r)
 }
-func get(w http.ResponseWriter, r *http.Request) {
+
+/*func get(w http.ResponseWriter, r *http.Request) {
 
 	var user model.User
 	docsnap := lib.GetFirestore("users", "")
 	docsnap.DataTo(&user)
 
 }
-func post(w http.ResponseWriter, r *http.Request) {
+*/
 
-}
-func put(w http.ResponseWriter, r *http.Request) {
-	log.Println("Put")
-	var user model.User
+func PutClaimFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
+	log.Println("PutClaim")
 
-	req := lib.ErrorByte(ioutil.ReadAll(r.Body))
+	body := lib.ErrorByte(io.ReadAll(r.Body))
 	defer r.Body.Close()
-	log.Println(string(req))
-	claim, e := model.UnmarshalClaim(req)
-	lib.CheckError(e)
-	log.Println("GetFirestore")
-	docsnap := lib.GetFirestore("users", claim.UserUid)
-	e = docsnap.DataTo(&user)
-	lib.CheckError(e)
-	claim.CreationDate = time.Now()
-	claim.Updated = time.Now()
-	uidClaim := uuid.New().String()
-	claim.ClaimUid = uidClaim
-	log.Println(user)
-	var obj mail.MailRequest
+	log.Println(string(body))
+
+	claim, err := models.UnmarshalClaim(body)
+	lib.CheckError(err)
+
+	return PutClaim(r.Header.Get("Origin"), &claim)
+}
+
+func PutClaim(origin string, claim *models.Claim) (string, interface{}, error) {
+	var (
+		user models.User
+		obj  mail.MailRequest
+		att  []mail.Attachment
+	)
+
+	log.Printf("Get user %s from firestore", claim.UserUid)
+	fireUsers := lib.GetDatasetByEnv(origin, "users")
+	docsnap, err := lib.GetFirestoreErr(fireUsers, claim.UserUid)
+	if err != nil {
+		log.Printf("[PutClaim] error retrieving user %s from firestore, error message: %s", claim.UserUid, err.Error())
+		return `{"success":false}`, `{"success":false}`, nil
+	}
+	err = docsnap.DataTo(&user)
+	if err != nil {
+		log.Println("[PutClaim] error convert docsnap to user")
+		return `{"success":false}`, `{"success":false}`, nil
+	}
+
+	claim.CreationDate = time.Now().UTC()
+	claim.Updated = time.Now().UTC()
+	claim.ClaimUid = uuid.New().String()
+	claim.Status = "open"
+
+	log.Println("User: ", user)
+
 	obj.From = "noreply@wopta.it"
 	obj.To = []string{"sinistri@wopta.it"}
 	obj.Message = `<p>ciao il cliente ` + claim.Name + ` ` + claim.Surname + `</p> <p>desidera notificare un sinistro per la polizza: ` + claim.PolicyId + ` per i seguenti motivi: ` + claim.Description + `</p> `
@@ -75,28 +93,39 @@ func put(w http.ResponseWriter, r *http.Request) {
 	if len(claim.Documents) > 0 {
 		obj.IsAttachment = true
 	}
-	var att []mail.Attachment
+
+	log.Println("[PutClaim] uploading attachments to google storage")
 	for i, doc := range claim.Documents {
-		byteFile, e := b64.StdEncoding.DecodeString(doc.Byte)
-		lib.CheckError(e)
-		link := lib.PutToStorage(os.Getenv("USER_BUCKET"), "users/"+claim.UserUid+"/claims/"+uidClaim+"/"+doc.FileName, byteFile)
+		byteFile, err := b64.StdEncoding.DecodeString(doc.Byte)
+		if err != nil {
+			log.Println("[PutClaim] error decoding base64 document encoding")
+			return `{"success":false}`, `{"success":false}`, nil
+		}
+		gsLink := lib.PutToStorage(os.Getenv("GOOGLE_STORAGE_BUCKET"), "assets/users/"+claim.UserUid+"/claims/"+
+			claim.ClaimUid+"/"+doc.FileName, byteFile)
 		att = append(att, mail.Attachment{Byte: doc.Byte, Name: doc.FileName, ContentType: doc.ContentType})
 		claim.Documents[i].Byte = ""
-		claim.Documents[i].Link = link
+		claim.Documents[i].Link = gsLink
 	}
 	obj.Attachments = &att
+	log.Println("[PutClaim] attachments uploaded to google storage")
 
-	userClaims := make([]models.Claim, 0)
-	if user.Claims != nil {
-		userClaims = append(userClaims, *user.Claims...)
+	if user.Claims == nil {
+		*user.Claims = make([]models.Claim, 0)
 	}
-	userClaims = append(userClaims, claim)
+	*user.Claims = append(*user.Claims, *claim)
 
-	user.Claims = &userClaims
-	log.Println("SetFirestore")
-	lib.UpdateFirestoreErr("users", claim.UserUid, map[string]interface{}{
-		"claims": userClaims,
+	log.Printf("[PutClaim] update user %s on firestore", claim.UserUid)
+	err = lib.UpdateFirestoreErr(fireUsers, claim.UserUid, map[string]interface{}{
+		"claims":  user.Claims,
+		"updated": time.Now().UTC(),
 	})
+	if err != nil {
+		log.Println("[PutClaim] error during user update")
+		return `{"success":false}`, `{"success":false}`, nil
+	}
+
 	mail.SendMail(obj)
-	// lib.PutFirestore("users")
+
+	return `{"success":true}`, `{"success":true}`, nil
 }
