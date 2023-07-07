51873ab93f566851476a07bfd8876aedea01e9df
Xu Ruixuan <r.xu@reply.it>
2023-07-07 16:44:35 +0200

    Added proposal support for GAP

--- mail/mail-policy.go
+++ mail/mail-policy.go
@@ -1,27 +1,37 @@
 package mail
 
 import (
+	"fmt"
+
 	"github.com/wopta/goworkspace/models"
 )
 
-func GetMailPolicy(policy models.Policy, subject string, islink bool, link string, linkLabel string, lines []string, isAttachment bool, at *[]Attachment) MailRequest {
-	var name string
-	var linkForm string
-	if policy.Name == "pmi" {
+func GetMailPolicy(
+	policy models.Policy,
+	subject string,
+	isLink bool,
+	link string,
+	linkLabel string,
+	lines []string,
+	isAttachment bool,
+	at *[]Attachment,
+) MailRequest {
+	var (
+		name     string
+		obj      MailRequest
+		linkForm = "https://www.wopta.it/it/multi-rischio/"
+	)
+
+	switch policy.Name {
+	case "pmi":
 		name = "Artigiani & Imprese"
-		linkForm = "https://www.wopta.it/it/multi-rischio/"
-
-	}
-	if policy.Name == "persona" {
+	case "persona":
 		name = "Persona"
-		linkForm = "https://www.wopta.it/it/multi-rischio/"
-	}
-	if policy.Name == "life" {
+	case "life":
 		name = "Vita"
-		linkForm = "https://www.wopta.it/it/multi-rischio/"
-
+	case "gap":
+		name = "GAP"
 	}
-	var obj MailRequest
 
 	obj.From = "anna@wopta.it"
 	obj.To = []string{policy.Contractor.Mail}
@@ -39,8 +49,8 @@ func GetMailPolicy(policy models.Policy, subject string, islink bool, link strin
 	obj.SubTitle = subject
 	obj.IsHtml = true
 	obj.IsAttachment = isAttachment
-	obj.IsLink = islink
-	if islink {
+	obj.IsLink = isLink
+	if isLink {
 		obj.Link = link
 		obj.LinkLabel = linkLabel
 	} else {
@@ -54,57 +64,124 @@ func GetMailPolicy(policy models.Policy, subject string, islink bool, link strin
 }
 
 func SendMailProposal(policy models.Policy) {
-	var link, name string
-	if policy.Name == "pmi" {
-		link = "https://storage.googleapis.com/documents-public-dev/information-sets/pmi/v1/Precontrattuale.pdf"
-	}
-	if policy.Name == "persona" {
-		link = "https://storage.googleapis.com/documents-public-dev/information-sets/persona/v1/Precontrattuale.pdf"
-	}
-	if policy.Name == "life" {
-		link = "https://storage.googleapis.com/documents-public-dev/information-sets/life/v1/Precontrattuale.pdf"
+	var (
+		name        string
+		link_format = "https://storage.googleapis.com/documents-public-dev/information-sets/%s/v1/Precontrattuale.pdf"
+		link        = fmt.Sprint(link_format, policy.Name)
+		message     = make([]string, 0, 3)
+	)
 
-	}
-	var message []string
-	message = append(message, `	<br>Grazie per aver compilato, la richiesta di preventivo per una polizza <b>`+name+`</b>.
-	<br><br>Per poter valutare completamente la soluzione che sceglierai, ti alleghiamo tutti i documenti che ti consentiranno di prendere una decisione pienamente consapevole ed informata.<br> `)
-	message = append(message, `<br>Se ci fosse la necessità di richiederti alcune informazioni aggiuntive, ti contatteremo ai recapiti da te forniti.`)
-	message = append(message, `<br>Prima della sottoscrizione, leggi quanto trovi in questa mail, la <b>documentazione precontrattuale</b> che,
-	 per trasparenza e tua adeguata informazione, trovi sempre disponibile cliccando sul bottone sottostante.`)
+	message = append(
+		message,
+		`	<br>Grazie per aver compilato, la richiesta di preventivo per una polizza <b>`+name+`</b>.
+	<br><br>Per poter valutare completamente la soluzione che sceglierai, ti alleghiamo tutti i documenti che ti consentiranno di prendere una decisione pienamente consapevole ed informata.<br> `,
+	)
+	message = append(
+		message,
+		`<br>Se ci fosse la necessità di richiederti alcune informazioni aggiuntive, ti contatteremo ai recapiti da te forniti.`,
+	)
+	message = append(
+		message,
+		`<br>Prima della sottoscrizione, leggi quanto trovi in questa mail, la <b>documentazione precontrattuale</b> che,
+	 per trasparenza e tua adeguata informazione, trovi sempre disponibile cliccando sul bottone sottostante.`,
+	)
 
-	SendMail(GetMailPolicy(policy, "Documenti precontrattuali", true, link, "Leggi documentazione", message, false, nil))
-
+	SendMail(
+		GetMailPolicy(
+			policy,
+			"Documenti precontrattuali",
+			true,
+			link,
+			"Leggi documentazione",
+			message,
+			false,
+			nil,
+		),
+	)
 }
 
 func SendMailPay(policy models.Policy) {
 	var message []string
-	message = append(message, `<p>hai firmato correttamente la polizza. Sei più vicino a sentirti più protetto.</br> `)
-	message = append(message, `Ti invitiamo ora ad accedere a cliccare sul bottone sottostante per perfezionare il pagamento.</p> `)
-	message = append(message, `<p>Infatti senza pagamento la polizza non è attiva e, solo a pagamento avvenuto, ti invieremo una mail in cui trovi tutti i documenti contrattuali completi.</br> `)
+	message = append(
+		message,
+		`<p>hai firmato correttamente la polizza. Sei più vicino a sentirti più protetto.</br> `,
+	)
+	message = append(
+		message,
+		`Ti invitiamo ora ad accedere a cliccare sul bottone sottostante per perfezionare il pagamento.</p> `,
+	)
+	message = append(
+		message,
+		`<p>Infatti senza pagamento la polizza non è attiva e, solo a pagamento avvenuto, ti invieremo una mail in cui trovi tutti i documenti contrattuali completi.</br> `,
+	)
 	message = append(message, `Qualora tu abbia già provveduto, ignora questa comunicazione </p> `)
-	SendMail(GetMailPolicy(policy, "Paga la tua polizza"+" n° "+policy.CodeCompany, true, policy.PayUrl, "Paga la tua polizza", message, false, nil))
-
+	SendMail(
+		GetMailPolicy(
+			policy,
+			"Paga la tua polizza"+" n° "+policy.CodeCompany,
+			true,
+			policy.PayUrl,
+			"Paga la tua polizza",
+			message,
+			false,
+			nil,
+		),
+	)
 }
+
 func SendMailSign(policy models.Policy) {
 	var message []string
 	message = append(message, `Puoi ora completare la sottoscrizione della tua polizza.`)
-	message = append(message, `<br>Clicca sul bottone sottostante per accedere alla procedura semplice e guidata di firma elettronica avanzata tramite 
-	utilizzo di un codice usa e getta che verrà inviato via sms sul tuo cellulare a noi comunicato.`)
-	message = append(message, `<br>Ti verrà richiesta l’adesione al servizio che è fornito in maniera gratuita da Wopta. Potrai prendere visione delle condizioni generali di servizio e delle caratteristiche tecniche.`)
-	message = append(message, `<br>Ultimata la procedura di firma potrai procedere al pagamento.<br>`)
+	message = append(
+		message,
+		`<br>Clicca sul bottone sottostante per accedere alla procedura semplice e guidata di firma elettronica avanzata tramite 
+	utilizzo di un codice usa e getta che verrà inviato via sms sul tuo cellulare a noi comunicato.`,
+	)
+	message = append(
+		message,
+		`<br>Ti verrà richiesta l’adesione al servizio che è fornito in maniera gratuita da Wopta. Potrai prendere visione delle condizioni generali di servizio e delle caratteristiche tecniche.`,
+	)
+	message = append(
+		message,
+		`<br>Ultimata la procedura di firma potrai procedere al pagamento.<br>`,
+	)
 	message = append(message, `Qualora tu abbia già provveduto, ignora questa comunicazione </br> `)
-	SendMail(GetMailPolicy(policy, "Firma la tua polizza"+" n° "+policy.CodeCompany, true, policy.SignUrl, "Firma la tua polizza", message, false, nil))
+	SendMail(
+		GetMailPolicy(
+			policy,
+			"Firma la tua polizza"+" n° "+policy.CodeCompany,
+			true,
+			policy.SignUrl,
+			"Firma la tua polizza",
+			message,
+			false,
+			nil,
+		),
+	)
 }
 
 func SendMailContract(policy models.Policy, at *[]Attachment) {
 	var message []string
-	message = append(message, `<p>ti confermiamo che la protezione offerta dalla tua polizza è ora attiva. 
+	message = append(
+		message,
+		`<p>ti confermiamo che la protezione offerta dalla tua polizza è ora attiva. 
 	in allegato trovi la documentazione firmata tramite l’utilizzo della Firma Elettronica. Salva e conserva i documenti con cura, ti serviranno in caso di sinistro.
 	Ti consigliamo di scaricare l’App di Wopta dagli store tramite il comodo QR code che trovi nell area sottostante per accedere 
 	alla tua area riservata nella quale troverai tutte le informazioni sulle polizze in tuo possesso e altri servizi a te riservati.</p>
 	Puoi usare anche questi canali per effettuare una denuncia di sinistro e verificare lo stato delle tue polizze e dei pagamenti.
 Seguici su nostri canali social o sul sito e scopri le iniziative a te riservate.
-	`)
-	SendMail(GetMailPolicy(policy, "Contratto"+" n° "+policy.CodeCompany, false, "", "", message, true, at))
-
+	`,
+	)
+	SendMail(
+		GetMailPolicy(
+			policy,
+			"Contratto"+" n° "+policy.CodeCompany,
+			false,
+			"",
+			"",
+			message,
+			true,
+			at,
+		),
+	)
 }
