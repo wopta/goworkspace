832642f3894ce0d4ae2f59bc5ef9f14f88acfae9
Yousef Hammar <yousef.hammar@wopta.it>
2023-07-19 09:34:48 +0200

    feat: add new beneficiary field to Guarantee and Beneficiary structs

--- models/beneficiary.go
+++ models/beneficiary.go
@@ -2,7 +2,8 @@ package models
 
 type Beneficiary struct {
 	User
-	IsFamilyMember         bool `json:"isFamilyMember" firestore:"isFamilyMember"`
-	IsContactable          bool `json:"isContactable" firestore:"isContactable"`
-	IsLegitimateSuccessors bool `json:"isLegitimateSuccessors" firestore:"isLegitimateSuccessors"`
+	IsFamilyMember         bool   `json:"isFamilyMember" firestore:"isFamilyMember"`
+	IsContactable          bool   `json:"isContactable" firestore:"isContactable"`
+	IsLegitimateSuccessors bool   `json:"isLegitimateSuccessors" firestore:"isLegitimateSuccessors"`
+	BeneficiaryType        string `json:"beneficiaryType" firestore:"beneficiaryType"`
 }
