64a5235fad76016631b0748c45d7017be6d1288d
Diogo Carvalho <diogo.carvalho@wopta.it>
2023-07-26 10:56:10 -0300

    feat: agent rename Portfolio into Users and add UpdateAgentPortfolio function

--- callback/payment.go
+++ callback/payment.go
@@ -89,14 +89,14 @@ func Payment(w http.ResponseWriter, r *http.Request) (string, interface{}, error
 				docsnap.DataTo(&agent)
 				agent.Policies = append(agent.Policies, p.Uid)
 				found := false
-				for _, contractorUid := range agent.Portfolio {
+				for _, contractorUid := range agent.Users {
 					if contractorUid == p.Contractor.Uid {
 						found = true
 						break
 					}
 				}
 				if !found {
-					agent.Portfolio = append(agent.Portfolio, p.Contractor.Uid)
+					agent.Users = append(agent.Users, p.Contractor.Uid)
 				}
 				err = lib.SetFirestoreErr(fireAgent, agent.Uid, agent)
 				lib.CheckError(err)
