becf6ea146a26c7ca8c9ceb94e13837867ba094a
Yousef Hammar <yousef.hammar@wopta.it>
2023-08-22 16:55:18 +0200

    feat: add Title field to ReservedInfo struct

--- models/reserved-info.go
+++ models/reserved-info.go
@@ -1,15 +1,16 @@
 package models
 
 type ReservedInfo struct {
-	MedicalDocuments  []string     `json:"medicalDocuments,omitempty" firestore:"medicalDocuments,omitempty" bigquery:"-"`
-	Contacts          []Contact    `json:"contacts,omitempty" firestore:"medicalDocuments,omitempty" bigquery:"-"`
-	DownloadDocuments []Attachment `json:"downloadDocuments,omitempty" firestore:"downloadDocuments,omitempty" bigquery:"-"`
-	Reasons           []string     `json:"reasons,omitempty" firestore:"reasons,omitempty" bigquery:"-"`
-	BigReasons        string       `json:"-" firestore:"-" bigquery:"reasons"`
+	RequiredExams []string     `json:"requiredExams,omitempty" firestore:"requiredExams,omitempty" bigquery:"-"`
+	Contacts      []Contact    `json:"contacts,omitempty" firestore:"contacts,omitempty" bigquery:"-"`
+	Documents     []Attachment `json:"documents,omitempty" firestore:"documents,omitempty" bigquery:"-"`
+	Reasons       []string     `json:"reasons,omitempty" firestore:"reasons,omitempty" bigquery:"-"`
+	BigReasons    string       `json:"-" firestore:"-" bigquery:"reasons"`
 }
 
 type Contact struct {
-	ContactType string `json:"contactType"`
-	Address     string `json:"address"`
-	Object      string `json:"object,omitempty"`
+	Title   string `json:"title,omitempty" firestore:"title,omitempty"`
+	Type    string `json:"type,omitempty" firestore:"type,omitempty"`
+	Address string `json:"address,omitempty" firestore:"address,omitempty"`
+	Object  string `json:"object,omitempty" firestore:"object,omitempty"`
 }
