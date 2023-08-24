f6cfb76ffccffab29987016de4242f6b415810a9
Yousef Hammar <yousef.hammar@wopta.it>
2023-08-24 15:55:42 +0200

    refactor: removed checkError call in firestore function

--- lib/firestore.go
+++ lib/firestore.go
@@ -167,7 +167,6 @@ func (queries *Firequeries) FirestoreWherefields(collection string) (*firestore.
 	ctx := context.Background()
 	var query firestore.Query
 	client, err := firestore.NewClient(ctx, os.Getenv("GOOGLE_PROJECT_ID"))
-	CheckError(err)
 	col := client.Collection(collection)
 	query = col.Where(queries.Queries[0].Field, queries.Queries[0].Operator, queries.Queries[0].QueryValue)
 	for i := 1; i <= len(queries.Queries)-1; i++ {
