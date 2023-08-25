3c0e51e530f55c3799af8602242d0e4d5fd6dcfc
Yousef Hammar <yousef.hammar@wopta.it>
2023-08-25 16:30:01 +0200

    feat: wrapped GetActiveProductsByChannel handler resp into object

--- mga/get-active-products-by-channel.go
+++ mga/get-active-products-by-channel.go
@@ -2,19 +2,28 @@ package mga
 
 import (
 	"encoding/json"
+	"github.com/wopta/goworkspace/models"
 	"github.com/wopta/goworkspace/product"
 	"log"
 	"net/http"
 )
 
+type GetActiveProductsByChannelResp struct {
+	Products []models.Product `json:"products"`
+}
+
 func GetActiveProductsByChannelFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
+	var (
+		resp GetActiveProductsByChannelResp
+	)
+
 	log.Println("[GetActiveProductsByChannelFx]")
 
 	channel := r.Header.Get("channel")
 
-	products := product.GetAllProductsByChannel(channel)
+	resp.Products = product.GetAllProductsByChannel(channel)
 
-	jsonOut, err := json.Marshal(products)
+	jsonOut, err := json.Marshal(resp)
 
-	return string(jsonOut), products, err
+	return string(jsonOut), resp, err
 }
