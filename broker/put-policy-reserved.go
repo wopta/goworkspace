c8ee8e5eaff0adfe5237b8af9c0ba1ce43345bc8
Diogo Carvalho <diogo.carvalho@wopta.it>
2023-09-06 12:39:51 +0200

    feat: save acceptance data in ReservedInfo, use audit  and enhance logging

--- broker/put-policy-reserved.go
+++ broker/put-policy-reserved.go
@@ -27,7 +27,19 @@ func PutPolicyReservedFx(w http.ResponseWriter, r *http.Request) (string, interf
 
 	origin := r.Header.Get("origin")
 	policyUid := r.Header.Get("policyUid")
-	firePolicy := lib.GetDatasetByEnv(origin, "policy")
+	firePolicy := lib.GetDatasetByEnv(origin, models.PolicyCollection)
+
+	log.Printf("[PutPolicyReservedFx] Policy Uid %s", policyUid)
+
+	body := lib.ErrorByte(io.ReadAll(r.Body))
+	defer r.Body.Close()
+	log.Printf("[PutPolicyReservedFx] Payload: %s", string(body))
+
+	err = lib.CheckPayload[PutPolicyReservedPayload](body, &payload, []string{"action"})
+	if err != nil {
+		log.Printf("[PutPolicyReservedFx] ERROR: %s", err.Error())
+		return `{"success":false}`, `{"success":false}`, nil
+	}
 
 	policy, err = GetPolicy(policyUid, origin)
 	lib.CheckError(err)
@@ -37,34 +49,41 @@ func PutPolicyReservedFx(w http.ResponseWriter, r *http.Request) (string, interf
 		return `{"success":false}`, `{"success":false}`, nil
 	}
 
-	body := lib.ErrorByte(io.ReadAll(r.Body))
-	defer r.Body.Close()
-
-	err = lib.CheckPayload[PutPolicyReservedPayload](body, &payload, []string{"action"})
-	if err != nil {
-		log.Printf("[PutPolicyReservedFx] ERROR: %s", err.Error())
-		return `{"success":false}`, `{"success":false}`, nil
-	}
-
 	switch payload.Action {
 	case models.PolicyStatusRejected:
 		rejectPolicy(&policy, payload.Reasons)
 	case models.PolicyStatusApproved:
-		approvePolicy(&policy)
+		approvePolicy(&policy, payload.Reasons)
 	default:
 		log.Printf("[PutPolicyReservedFx] Unhandled action %s", payload.Action)
 		return `{"success":false}`, `{"success":false}`, nil
 	}
 
 	policy.Updated = time.Now().UTC()
+
+	log.Println("[PutPolicyReservedFx] saving to firestore...")
 	err = lib.SetFirestoreErr(firePolicy, policy.Uid, &policy)
 	lib.CheckError(err)
+	log.Println("[PutPolicyReservedFx] firestore saved!")
+
 	policy.BigquerySave(origin)
 
-	policyJsonLog, _ := policy.Marshal()
+	policyJsonLog, err := policy.Marshal()
+	if err != nil {
+		log.Printf("[PutPolicyReservedFx] error marshaling policy: %s", err.Error())
+	}
 	log.Printf("[PutPolicyReservedFx] Policy: %s", string(policyJsonLog))
 
+	log.Println("[PutPolicyReservedFx] sending acceptance email...")
 	mail.SendMailReservedResult(policy)
+
+	log.Println("[PutPolicyReservedFx] saving audit trail...")
+	audit, err := models.ParseHttpRequest(r, string(body))
+	if err != nil {
+		log.Printf("[PutPolicyReservedFx] error creating audit log: %s", err.Error())
+	}
+	log.Printf("[PutPolicyReservedFx] audit log: %v", audit)
+	audit.SaveToBigQuery()
 
 	return `{"success":true}`, `{"success":true}`, nil
 }
@@ -73,11 +92,14 @@ func rejectPolicy(policy *models.Policy, reasons string) {
 	log.Printf("[rejectPolicy] Policy Uid %s REJECTED", policy.Uid)
 	policy.Status = models.PolicyStatusRejected
 	policy.StatusHistory = append(policy.StatusHistory, policy.Status)
-	policy.RejectReasons = reasons
+	policy.ReservedInfo.AcceptanceNote = reasons
+	policy.ReservedInfo.AcceptanceDate = time.Now().UTC()
 }
 
-func approvePolicy(policy *models.Policy) {
+func approvePolicy(policy *models.Policy, reasons string) {
 	log.Printf("[approvePolicy] Policy Uid %s APPROVED", policy.Uid)
 	policy.Status = models.PolicyStatusApproved
 	policy.StatusHistory = append(policy.StatusHistory, policy.Status)
+	policy.ReservedInfo.AcceptanceNote = reasons
+	policy.ReservedInfo.AcceptanceDate = time.Now().UTC()
 }
