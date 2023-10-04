db1a5a329721edb0c55696cd1257db80b3d6a65c
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-10-04 09:24:01 +0200

    init

--- auth/auth.go
+++ auth/auth.go
@@ -37,7 +37,7 @@ func Auth(w http.ResponseWriter, r *http.Request) {
 				Roles:   []string{models.UserRoleAll},
 			},
 			{
-				Route:   "/token/v1/sso/jwt/aua",
+				Route:   "/sso/jwt/aua/v1",
 				Handler: JwtFx,
 				Method:  http.MethodGet,
 				Roles:   []string{models.UserRoleAll},
