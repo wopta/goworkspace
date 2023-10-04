5d6cb0b711a18b35615f30afd507c06f5eb08d90
woptaassicurazioni@gmail.com <woptaassicurazioni@gmail.com>
2023-10-04 14:13:46 +0200

    init

--- auth/sso.go
+++ auth/sso.go
@@ -51,7 +51,7 @@ func verifyAuaJwt(tokenReq string) (*AuaClaims, bool, error) {
 
 	token, e := jwt.ParseWithClaims(tokenReq, &AuaClaims{}, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
