860341f2cd5242c7f78279ac6cfa1e68560eb550
Diogo Carvalho <diogo.carvalho@wopta.it>
2023-10-05 16:41:23 +0200

    feat: accounting get network transaction handler

--- accounting/accounting.go
+++ accounting/accounting.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
 )
 
 func init() {
@@ -18,7 +19,22 @@ func Accounting(w http.ResponseWriter, r *http.Request) {
 	lib.EnableCors(&w, r)
 
 	route := lib.RouteData{
-		Routes: []lib.Route{},
+		Routes: []lib.Route{
+			{
+				Route:   "/network/transactions/v1/transaction/:transactionUid",
+				Handler: GetNetworkTransactions,
+				Method:  http.MethodGet,
+				Roles:   []string{models.UserRoleAdmin, models.UserRoleManager},
+			},
+			{
+				Route:   "/network/transactions/v1/:uid",
+				Handler: PutNetworkTransaction,
+				Method:  http.MethodPut,
+				// Roles:   []string{models.UserRoleAdmin, models.UserRoleManager},
+				Roles: []string{},
+			},
+			// POST
+		},
 	}
 
 	route.Router(w, r)
---
+++ accounting/get-network-transactions.go
@@ -0,0 +1,63 @@
+package accounting
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
+)
+
+type GetNetworkTransactionsResponse struct {
+	NetworkTransactions []models.NetworkTransaction `json:"networkTransactions"`
+}
+
+func GetNetworkTransactions(w http.ResponseWriter, r *http.Request) (string, any, error) {
+	log.Println("[GetNetworkTransactions] Handler start ---------------------")
+
+	var response GetNetworkTransactionsResponse
+
+	transactionUid := r.Header.Get("transactionUid")
+
+	log.Printf("[GetNetworkTransactions] transactionUid %s", transactionUid)
+
+	netTranscations := GetNetworkTransactionsByTransactionUid(transactionUid)
+	if len(netTranscations) == 0 {
+		return "", "", fmt.Errorf("no network transactions found for transaction %s", transactionUid)
+	}
+
+	response.NetworkTransactions = netTranscations
+	responseByte, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("[GetNetworkTransactions] error marshaling response: %s", err.Error())
+		return "", "", err
+	}
+
+	return string(responseByte), response, nil
+}
+
+func GetNetworkTransactionsByTransactionUid(transactionUid string) []models.NetworkTransaction {
+	log.Printf("[GetNetworkTransactionsByTransactionUid] transactionUid %s", transactionUid)
+
+	var (
+		netTransactions []models.NetworkTransaction
+		err             error
+	)
+
+	query := fmt.Sprintf(
+		"SELECT * FROM `%s.%s` WHERE transactionUid='%s'",
+		models.WoptaDataset,
+		models.NetworkTransactionCollection,
+		transactionUid,
+	)
+
+	netTransactions, err = lib.QueryRowsBigQuery[models.NetworkTransaction](query)
+	if err != nil {
+		log.Printf("[GetNetworkTransactionsByTransactionUid] error getting network transactions: %s", err.Error())
+	}
+
+	log.Printf("[GetNetworkTransactionsByTransactionUid] found %d network transactions", len(netTransactions))
+	return netTransactions
+}
