5f08db880942c7d1ad9b21ac74fd877fff1b071e
Diogo Carvalho <diogo.carvalho@wopta.it>
2023-10-13 14:31:04 +0200

    fix: GetProductsListByChannelFx uses authToken channel for control flow

--- mga/get-products-list-v2.go
+++ mga/get-products-list-v2.go
@@ -30,18 +30,22 @@ func GetProductsListByChannelFx(w http.ResponseWriter, r *http.Request) (string,
 		return "", "", err
 	}
 
-	// TODO use authToken.GetChannel so we only extract the node for networks
-	networkNode := network.GetNetworkNodeByUid(authToken.UserID)
-
-	if authToken.Role == models.UserRoleAdmin {
+	channel := authToken.GetChannelByRoleV2()
+	switch channel {
+	case models.MgaChannel:
 		log.Println("[GetProductsListByChannelFx] getting mga products")
 		response.Products = product.GetProductsByChannel(models.MgaChannel)
-	} else if networkNode == nil {
+	case models.ECommerceChannel:
 		log.Println("[GetProductsListByChannelFx] getting e-commerce products")
 		response.Products = product.GetProductsByChannel(models.ECommerceChannel)
-	} else {
+	case models.NetworkChannel:
 		log.Println("[GetProductsListByChannelFx] getting network products")
-		warrant := network.GetWarrant(networkNode.Warrant)
+		networkNode := network.GetNetworkNodeByUid(authToken.UserID)
+		if networkNode == nil {
+			log.Println("[GetProductsListByChannelFx] node not found")
+			return "", "", fmt.Errorf("no node set for authToken")
+		}
+		warrant := networkNode.GetWarrant()
 		if warrant == nil {
 			log.Println("[GetProductsListByChannelFx] warrant not found")
 			return "", "", fmt.Errorf("no warrant set for node")
@@ -49,6 +53,9 @@ func GetProductsListByChannelFx(w http.ResponseWriter, r *http.Request) (string,
 		productList := lib.SliceMap[models.Product, string](warrant.Products, func(p models.Product) string { return p.Name })
 		log.Printf("[GetProductsListByChannelFx] product list '%s'", productList)
 		response.Products = product.GetNetworkNodeProducts(productList)
+	default:
+		log.Printf("[GetProductsListByChannelFx] error channel %s unaavailable", channel)
+		return "", "", fmt.Errorf("unavailable channel")
 	}
 
 	responseBytes, err := json.Marshal(response)
