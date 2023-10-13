8737225310e12c57f2243a801603f213f536f2da
Yousef Hammar <yousef.hammar@wopta.it>
2023-10-13 22:33:44 +0200

    feat: implemented sellable life v2

--- sellable/life.go
+++ sellable/life.go
@@ -2,6 +2,7 @@ package sellable
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 	prd "github.com/wopta/goworkspace/product"
 )
 
+// DEPRECATED
 func LifeHandler(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
 	var (
 		policy models.Policy
@@ -30,6 +32,7 @@ func LifeHandler(w http.ResponseWriter, r *http.Request) (string, interface{}, e
 	return Life(authToken.GetChannelByRole(), policy)
 }
 
+// DEPRECATED
 func Life(channel string, policy models.Policy) (string, *models.Product, error) {
 	var (
 		err error
@@ -56,6 +59,85 @@ func Life(channel string, policy models.Policy) (string, *models.Product, error)
 	jsonOut, err := json.Marshal(product)
 
 	return string(jsonOut), product, err
+}
+
+func LifeV2Fx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
+	var (
+		policy *models.Policy
+		err    error
+	)
+
+	log.Println("[LifeV2Fx] handler start ----------- ")
+
+	body := lib.ErrorByte(io.ReadAll(r.Body))
+	log.Printf("[LifeV2Fx] body: %s", string(body))
+
+	err = json.Unmarshal(body, &policy)
+	if err != nil {
+		return "", nil, err
+	}
+
+	authToken, err := models.GetAuthTokenFromIdToken(r.Header.Get("Authorization"))
+	lib.CheckError(err)
+
+	log.Println("[LifeV2Fx] calling vendibility rules function")
+
+	product, err := LifeV2(policy, authToken.GetChannelByRoleV2())
+	if err != nil {
+		log.Printf("[LifeV2Fx] vednibility rules error: %s", err.Error())
+		return "", nil, err
+	}
+
+	jsonOut, err := product.Marshal()
+
+	return string(jsonOut), product, err
+}
+
+func LifeV2(policy *models.Policy, channel string) (*models.Product, error) {
+	var (
+		err     error
+		product *models.Product
+	)
+	const (
+		sellableFileName = "sellable"
+	)
+
+	log.Println("[LifeV2] function start -----------")
+
+	log.Println("[LifeV2] loading input data")
+
+	in, err := getInputData(*policy)
+	if err != nil {
+		log.Printf("[LifeV2] error getting input data: %s", err.Error())
+		return nil, err
+	}
+
+	log.Println("[LifeV2] loading vendibility rules file")
+	rulesFile := lib.GetRulesFileV2(policy.Name, policy.ProductVersion, sellableFileName)
+
+	// TODO: controllare questo caso
+	/*
+		corretto prendere ultima versione attiva e non versione del prodotto all'interno della policy?
+		potrebbero esserci problemi per la chiamata di quote all'emit (tentativo emissione di una proposta
+		creata con versione vecchia di un prodotto)
+	*/
+
+	log.Println("[LifeV2] loading product")
+	product = prd.GetDefaultProduct(policy.Name, channel)
+	if product == nil {
+		return nil, fmt.Errorf("no product found")
+	}
+
+	log.Println("[LifeV2] applying vendibility rules")
+
+	fx := new(models.Fx)
+	_, ruleOutput := lib.RulesFromJsonV2(fx, rulesFile, product, in, nil)
+
+	product = ruleOutput.(*models.Product)
+
+	log.Println("[LifeV2] function end ----------")
+
+	return product, nil
 }
 
 func getInputData(policy models.Policy) ([]byte, error) {
