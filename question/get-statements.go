e438e704a9a27d82da15a908abdcb799e6cf9657
Yousef Hammar <yousef.hammar@wopta.it>
2023-10-15 12:13:44 +0200

    feat: updated GetStatements function to new products-v2 folder structure

---
+++ question/get-statements.go
@@ -0,0 +1,42 @@
+package question
+
+import (
+	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
+	"log"
+)
+
+type Statements struct {
+	Statements []*models.Statement
+	Text       string
+}
+
+func GetStatementsV2(policy *models.Policy) ([]models.Statement, error) {
+	log.Println("[GetStatementsV2] function start ----------------")
+
+	policyJson, err := policy.Marshal()
+	if err != nil {
+		log.Printf("[GetStatementsV2] error marshaling policy: %s", err.Error())
+		return nil, err
+	}
+
+	fx := new(models.Fx)
+	rulesStatements := &Statements{
+		Statements: make([]*models.Statement, 0),
+		Text:       "",
+	}
+
+	rulesFile := lib.GetRulesFileV2(policy.Name, policy.ProductVersion, statements)
+	data := loadExternalData(policy.Name)
+
+	_, ruleOutput := lib.RulesFromJsonV2(fx, rulesFile, rulesStatements, policyJson, data)
+
+	result := make([]models.Statement, 0)
+	for _, statement := range ruleOutput.(*Statements).Statements {
+		result = append(result, *statement)
+	}
+
+	log.Println("[GetStatementsV2] function end ----------------")
+
+	return result, err
+}
--- question/question.go
+++ question/question.go
@@ -1,11 +1,19 @@
 package question
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/models"
+	"io"
 	"log"
 	"net/http"
+)
+
+const (
+	statements = "statements"
+	surveys    = "surveys"
 )
 
 func init() {
@@ -25,18 +33,99 @@ func Question(w http.ResponseWriter, r *http.Request) {
 				Method:  http.MethodPost,
 				Roles:   []string{models.UserRoleAll},
 			},
+			{
+				Route:   "/v2/:questionType",
+				Handler: GetQuestionsV2Fx,
+				Method:  http.MethodPost,
+				Roles:   []string{models.UserRoleAll},
+			},
 		},
 	}
 	route.Router(w, r)
 
 }
 
-type Statements struct {
-	Statements []*models.Statement
-	Text       string
+func GetQuestionsV2Fx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
+	var (
+		result interface{}
+		policy *models.Policy
+	)
+
+	log.Println("[GetQuestionsV2Fx] handler start -------------------")
+
+	questionType := r.Header.Get("questionType")
+	log.Printf("[GetQuestionsV2Fx] questions: %s", questionType)
+
+	body := lib.ErrorByte(io.ReadAll(r.Body))
+
+	log.Printf("[GetQuestionsV2Fx] req body: %s", string(body))
+
+	err := json.Unmarshal(body, &policy)
+	if err != nil {
+		log.Printf("[GetQuestionsV2] error unmarshaling request body: %s", err.Error())
+		return "", nil, err
+	}
+
+	switch questionType {
+	case statements:
+		log.Printf("[GetQuestionsV2Fx] loading statements for %s product", policy.Name)
+		result, err = GetStatementsV2(policy)
+	case surveys:
+		log.Printf("[GetQuestionV2Fx] loading surveys for %s product", policy.Name)
+		//out = GetSurveysV2(policy)
+	default:
+		log.Printf("[GetQuestionV2Fx] questionType %s not allowed", questionType)
+		return "", nil, fmt.Errorf("questionType %s not allowed", questionType)
+	}
+
+	if err != nil {
+		log.Printf("[GetQuestionsV2Fx] error: %s", err.Error())
+		return "", nil, err
+	}
+
+	jsonOut, err := json.Marshal(result)
+
+	log.Println("[GetQuestionsV2Fx] handler end -------------------")
+
+	return `{"` + questionType + `": ` + string(jsonOut) + `}`, result, err
 }
 
-type Surveys struct {
-	Surveys []*models.Survey
-	Text    string
+func loadExternalData(productName string) []byte {
+	var data []byte
+	switch productName {
+	case models.PersonaProduct:
+		data = []byte(getCoherenceData())
+	}
+	return data
+}
+
+func getCoherenceData() string {
+	return `{
+		"AA": {
+			"extra": "nel tempo libero,",
+			"professionale": "nello svolgimento dell'attivtà lavorativa,",
+			"24h": "sia al lavoro che nel tempo libero,"
+		},
+		"BB": {
+			"dipendente": "lavoratore dipendente",
+			"autonomo": "lavoratore autonomo",
+			"non lavoratore": "non lavoratore"
+		},
+		"CC1": "un capitale all'Assicurato, in caso di Invalidità Permanente,",
+		"FR": {
+			"3": "oltre il 3% di invalidità,",
+			"5": "oltre il 5% di invalidità,",
+			"10": "oltre il 10% di invalidità,"
+		},
+		"CC2": "a copertura della minore capacità di reddito;",
+		"DD": "un capitale in caso di Decesso, avente finalità previdenziale, a copertura delle minori disponibilità che risulterebbero, in seguito al decesso dell'Assicurato, a favore dei beneficiari designati;",
+		"EE": "una Diaria (importo giornaliero) in caso di ricovero o gessatura;",
+		"FF": "una Diaria in caso di convalescenza post ricovero;",
+		"GG": "un indennizzo per ogni giorno in cui l'Assicurato è inabile, in tutto o in parte, allo svolgimento delle proprie attività lavorative;",
+		"HH": "inoltre l'assicurazione, sempre in caso di infortunio, risponde al bisogno di",
+		"II": "rimborsare le spese mediche sostenute;",
+		"JJ": "assicurare la difesa legale, per fatti illeciti di terzi o malpractice sanitaria;",
+		"KK": "aiutare l'Assicurato con servizi di assistenza utili in momenti di bisogno (es. invio di un medico, consulto medico, trasporto in ambulanza, …);",
+		"LL": "in caso di malattia, infine, consente di disporre di un capitale, a integrazione del reddito, qualora all'Assicurato derivi dalla malattia stessa una riduzione della capacità lavorativa (invalidità permanente) oltre il 24%"
+	}`
 }
