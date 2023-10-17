3b68adc9f1a4682a32b2fb604fe005b332cddb65
Yousef Hammar <yousef.hammar@wopta.it>
2023-10-17 17:28:08 +0200

    fix

--- document/fpdf-utils.go
+++ document/fpdf-utils.go
@@ -43,10 +43,10 @@ func downloadAssets() error {
 		folderPath = "./tmp"
 	)
 
-	/*env := os.Getenv("env")
+	env := os.Getenv("env")
 	if env == "local" {
 		return nil
-	}*/
+	}
 
 	bucket := os.Getenv("GOOGLE_STORAGE_BUCKET")
 
@@ -58,10 +58,10 @@ func downloadAssets() error {
 		return fmt.Errorf("no files found")
 	}
 
-	err = os.Mkdir(folderPath, 0750)
+	/*err = os.Mkdir(folderPath, 0750)
 	if err != nil {
 		return err
-	}
+	}*/
 
 	for _, file := range filesList {
 		rawFile := lib.GetFromStorage(bucket, file, "")
