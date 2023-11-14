5ce86a91005a57a68c8c8c9d412f37e27aef04e7
Diogo Carvalho <diogo.carvalho@wopta.it>
2023-11-14 13:04:43 +0100

    fix: check if network transaction has already been modified

--- _script/update_network_transactions.go
+++ _script/update_network_transactions.go
@@ -2,10 +2,11 @@ package _script
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/models"
+	plc "github.com/wopta/goworkspace/policy"
+	"github.com/wopta/goworkspace/product"
 	tr "github.com/wopta/goworkspace/transaction"
 )
 
@@ -15,6 +16,8 @@ func UpdateNetworkTransactions() {
 		transaction     *models.Transaction
 		err             error
 		originalAmount  float64
+		origin          = ""
+		modifiedCounter = make([]string, 0)
 	)
 
 	// get all network transactions of RemittanceCompany
@@ -26,18 +29,27 @@ func UpdateNetworkTransactions() {
 	)
 	netTransactions, err = lib.QueryRowsBigQuery[models.NetworkTransaction](query)
 	if err != nil {
-		log.Printf("[UpdateNetworkTransactions] error getting network transactions: %s", err.Error())
+		fmt.Printf("[UpdateNetworkTransactions] error getting network transactions: %s", err.Error())
 		return
 	}
-	log.Printf("[UpdateNetworkTransactions] found %d netTransactions", len(netTransactions))
+	fmt.Printf("[UpdateNetworkTransactions] found %d netTransactions\n", len(netTransactions))
 	// loop nt
 	for _, nt := range netTransactions {
 		// for each nt get its parent transaction (t)
-		transaction = tr.GetTransactionByUid(nt.TransactionUid, "")
+		transaction = tr.GetTransactionByUid(nt.TransactionUid, origin)
 		// update the nt.Amount and nt.AmountNet with t.Amount - nt.Amount
 		if transaction == nil {
-			log.Printf("[UpdateNetworkTransactions] error getting transaction '%s': %s", nt.TransactionUid, err.Error())
+			fmt.Printf("[UpdateNetworkTransactions] error getting transaction '%s': %s", nt.TransactionUid, err.Error())
 			return
+		}
+
+		policy := plc.GetPolicyByUid(transaction.PolicyUid, origin)
+		mgaProduct := product.GetProductV2(policy.Name, policy.ProductVersion, models.MgaChannel, nil, nil)
+		commissionMga := product.GetCommissionByProduct(&policy, mgaProduct, false)
+
+		if commissionMga != nt.Amount {
+			fmt.Printf("[UpdateNetworkTransactions] netTransaction '%s' with amount '%f' already modified\n", nt.Uid, nt.Amount)
+			continue
 		}
 
 		originalAmount = nt.Amount
@@ -46,8 +58,15 @@ func UpdateNetworkTransactions() {
 
 		// save to bigquery
 		// TODO: remember to manually allow for the modification of amount and amountNet fields
-		nt.SaveBigQuery()
-		log.Printf("[UpdateNetworkTransactions] netTransaction '%s' original amount '%f' modified amount '%f'", nt.Uid, originalAmount, nt.Amount)
+		err = nt.SaveBigQuery()
+		if err != nil {
+			fmt.Printf("[UpdateNetworkTransactions] error updating network transaction '%s': %s\n", nt.Uid, err.Error())
+			break
+		}
+
+		modifiedCounter = append(modifiedCounter, nt.Uid)
+		fmt.Printf("[UpdateNetworkTransactions] netTransaction '%s' original amount '%f' modified amount '%f'\n", nt.Uid, originalAmount, nt.Amount)
 	}
-	log.Println("[UpdateNetworkTransactions] script done")
+	fmt.Printf("[UpdateNetworkTransactions] modified network transactions %s\n", modifiedCounter)
+	fmt.Println("[UpdateNetworkTransactions] script done")
 }
