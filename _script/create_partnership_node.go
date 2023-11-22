aaf1012032f34b4772651c3b6ec68b722a1acad4
Yousef Hammar <yousef.hammar@wopta.it>
2023-11-22 10:02:10 +0100

    feat: add script to create partnership nodes and update network nodes with company code and external network code

---
+++ _script/create_partnership_node.go
@@ -0,0 +1,205 @@
+package _script
+
+import (
+	"github.com/wopta/goworkspace/models"
+	"github.com/wopta/goworkspace/network"
+	"log"
+)
+
+func createPartnerhipNodes() {
+	nameDesc := "Wopta per te Vita"
+
+	partnershipModel := models.NetworkNode{
+		Uid:      "beprof",
+		Code:     "beprof",
+		Type:     "partnership",
+		IsActive: true,
+		Partnership: &models.PartnershipNode{
+			Name: "beprof",
+			Skin: &models.Skin{
+				PrimaryColor:   "#30415f",
+				SecondaryColor: "#f1bd12",
+				LogoUrl:        "assets/images/logo-beprof-trasp-72dpi.png",
+			},
+		},
+		Products: []models.Product{
+			models.Product{
+				Name:         "life",
+				NameDesc:     &nameDesc,
+				Version:      "v2",
+				NameTitle:    "Wopta per te",
+				NameSubtitle: "Vita",
+				Companies: []models.Company{
+					models.Company{
+						Name: "axa",
+					},
+				},
+				Steps: []models.Step{
+					models.Step{
+						Children: []models.Child{
+							{
+								Attributes: map[string]interface{}{
+									"consens": "Il sottoscritto, letta e compresa l'informativa sul trattamento dei dati personali, ACCONSENTE al trattamento dei propri dati personali da parte di Wopta Assicurazioni per l'invio di comunicazioni e proposte commerciali e di marketing, incluso l'invio di newsletter e ricerche di mercato, attraverso strumenti automatizzati (sms, mms, e-mail, ecc.) e non (posta cartacea e telefono con operatore).",
+									"key":     2,
+									"title":   "Privacy",
+								},
+								Widget: "privacyConsent",
+							}},
+						Widget: "guaranteeconfigurationstep",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"companyPrivacy":      "PRESTO IL CONSENSO al trattamento dei miei dati personali ad AXA France VIE S.A. – Rappresentanza Generale per l’Italia, ivi inclusi quelli eventualmente da me conferiti in riferimento al mio stato di salute, per le finalità indicate nell’informativa, consultabile all’interno dei documenti precontrattuali (ricevuti via mail o consultabili al link nella pagina che precede), nonché alla loro comunicazione, per successivo trattamento, da parte dei soggetti indicati nella informativa predetta.",
+							"companyPrivacyTitle": "Privacy Assicurativa",
+							"statementsEndpoint":  "question/v1/surveys",
+						},
+						Widget: "quotersurvey",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"statementsEndpoint": "question/v1/statements",
+						},
+						Widget: "quoterstatements",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"beneficiaryText":              "Per procedere indicare chi sono i beneficiari della polizza in caso di decesso dell'assicurato.\\nPuoi indicare genericamente i tuoi eredi legittimi e/o testamentari. Oppure inserire in maniera puntuale i nomi dei Beneficiari (Massimo due).",
+							"guaranteeSlug":                "death",
+							"maximumNumberOfBeneficiaries": 2,
+							"thirdPartyReferenceText":      "In caso di specifiche esigenze di riservatezza, potrai indicare il nominativo ed i dati di recapito (inluso email e/o telefono) di un soggetto terno (diverso dal Beneficiario) a cui l'impresa di Assicurazione potrà rivolgersi in caso di decesso dell'Assicurato al fine di contattare il Beneficiario.",
+						},
+						Widget: "quoterbeneficiary",
+					},
+					models.Step{
+						Widget: "quotercontractordata",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"guaranteeSlug": "death",
+						},
+						Widget: "quoteruploaddocuments",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"showDuration":   false,
+							"showEndDate":    true,
+							"showGuarantees": true,
+						},
+						Widget: "quoterrecap",
+					},
+					models.Step{
+						Widget: "quotersignpay",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"productLogo": "assets/images/wopta-logo-vita-magenta.png",
+						},
+						Widget: "quoterthankyou",
+					},
+				},
+			},
+		},
+	}
+
+	_, err := network.CreateNode(partnershipModel)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	partnershipModel = models.NetworkNode{
+		Uid:      "facile",
+		Code:     "facile",
+		Type:     "partnership",
+		IsActive: true,
+		Partnership: &models.PartnershipNode{
+			Name: "facile",
+			Skin: &models.Skin{
+				PrimaryColor:   "",
+				SecondaryColor: "",
+				LogoUrl:        "https://upload.wikimedia.org/wikipedia/commons/7/78/Logo_facile_%28azienda%29.png",
+			},
+		},
+		Products: []models.Product{
+			models.Product{
+				Name:         "life",
+				NameDesc:     &nameDesc,
+				Version:      "v2",
+				NameTitle:    "Wopta per te",
+				NameSubtitle: "Vita",
+				Companies: []models.Company{
+					models.Company{
+						Name: "axa",
+					},
+				},
+				Steps: []models.Step{
+					models.Step{
+						Children: []models.Child{
+							{
+								Attributes: map[string]interface{}{
+									"consens": "Il sottoscritto, letta e compresa l'informativa sul trattamento dei dati personali, ACCONSENTE al trattamento dei propri dati personali da parte di Wopta Assicurazioni per l'invio di comunicazioni e proposte commerciali e di marketing, incluso l'invio di newsletter e ricerche di mercato, attraverso strumenti automatizzati (sms, mms, e-mail, ecc.) e non (posta cartacea e telefono con operatore).",
+									"key":     2,
+									"title":   "Privacy",
+								},
+								Widget: "privacyConsent",
+							}},
+						Widget: "guaranteeconfigurationstep",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"companyPrivacy":      "PRESTO IL CONSENSO al trattamento dei miei dati personali ad AXA France VIE S.A. – Rappresentanza Generale per l’Italia, ivi inclusi quelli eventualmente da me conferiti in riferimento al mio stato di salute, per le finalità indicate nell’informativa, consultabile all’interno dei documenti precontrattuali (ricevuti via mail o consultabili al link nella pagina che precede), nonché alla loro comunicazione, per successivo trattamento, da parte dei soggetti indicati nella informativa predetta.",
+							"companyPrivacyTitle": "Privacy Assicurativa",
+							"statementsEndpoint":  "question/v1/surveys",
+						},
+						Widget: "quotersurvey",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"statementsEndpoint": "question/v1/statements",
+						},
+						Widget: "quoterstatements",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"beneficiaryText":              "Per procedere indicare chi sono i beneficiari della polizza in caso di decesso dell'assicurato.\\nPuoi indicare genericamente i tuoi eredi legittimi e/o testamentari. Oppure inserire in maniera puntuale i nomi dei Beneficiari (Massimo due).",
+							"guaranteeSlug":                "death",
+							"maximumNumberOfBeneficiaries": 2,
+							"thirdPartyReferenceText":      "In caso di specifiche esigenze di riservatezza, potrai indicare il nominativo ed i dati di recapito (inluso email e/o telefono) di un soggetto terno (diverso dal Beneficiario) a cui l'impresa di Assicurazione potrà rivolgersi in caso di decesso dell'Assicurato al fine di contattare il Beneficiario.",
+						},
+						Widget: "quoterbeneficiary",
+					},
+					models.Step{
+						Widget: "quotercontractordata",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"guaranteeSlug": "death",
+						},
+						Widget: "quoteruploaddocuments",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"showDuration":   false,
+							"showEndDate":    true,
+							"showGuarantees": true,
+						},
+						Widget: "quoterrecap",
+					},
+					models.Step{
+						Widget: "quotersignpay",
+					},
+					models.Step{
+						Attributes: map[string]interface{}{
+							"productLogo": "assets/images/wopta-logo-vita-magenta.png",
+						},
+						Widget: "quoterthankyou",
+					},
+				},
+			},
+		},
+	}
+
+	_, err = network.CreateNode(partnershipModel)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
---
+++ _script/update_network_node.go
@@ -0,0 +1,117 @@
+package _script
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
+	"google.golang.org/api/iterator"
+	"log"
+	"os"
+	"time"
+)
+
+func updateNetworkNodes() {
+	networkNodes := make([]models.NetworkNode, 0)
+	updatedNodes := 0
+
+	ctx := context.Background()
+	client, _ := firestore.NewClient(ctx, os.Getenv("GOOGLE_PROJECT_ID"))
+	iter := client.Collection(models.NetworkNodesCollection).Documents(ctx)
+	defer iter.Stop() // add this line to ensure resources cleaned up
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Println(err.Error())
+		}
+		var networkNode models.NetworkNode
+		if err := doc.DataTo(&networkNode); err != nil {
+			log.Println(err.Error())
+		}
+		networkNodes = append(networkNodes, networkNode)
+	}
+
+	log.Printf("Total Nodes: %d", len(networkNodes))
+	notUpdatedNodes := 0
+
+	for _, nn := range networkNodes {
+		if nn.Type == models.PartnershipNetworkNodeType {
+			notUpdatedNodes++
+			log.Printf("Partnership Uid: %s", nn.Uid)
+			nn.SaveBigQuery("")
+			continue
+		} else if nn.Type != models.AgencyNetworkNodeType && nn.Type != models.AgentNetworkNodeType {
+			log.Printf("Node Uid: %s", nn.Uid)
+			notUpdatedNodes++
+			continue
+		}
+		if nn.Warrant == "mga_life_agent" {
+			nn.Products = []models.Product{
+				{
+					Name: models.LifeProduct,
+					Companies: []models.Company{
+						{
+							Name:         models.AxaCompany,
+							ProducerCode: nn.Code,
+						},
+					},
+				},
+			}
+		} else if nn.Warrant == "finaip_gap_agency" || nn.Warrant == "mga_life_gap_agent" {
+			nn.Products = []models.Product{
+				{
+					Name: models.GapProduct,
+					Companies: []models.Company{
+						{
+							Name:         models.SogessurCompany,
+							ProducerCode: nn.Code,
+						},
+					},
+				},
+			}
+		} else if nn.Warrant == "mga_multi-product_multi-node" {
+			nn.Products = []models.Product{
+				{
+					Name: models.LifeProduct,
+					Companies: []models.Company{
+						{
+							Name:         models.AxaCompany,
+							ProducerCode: nn.Code,
+						},
+					},
+				},
+				{
+					Name: models.GapProduct,
+					Companies: []models.Company{
+						{
+							Name:         models.SogessurCompany,
+							ProducerCode: nn.Code,
+						},
+					},
+				},
+			}
+		} else {
+			log.Printf("Node Uid: %s", nn.Uid)
+			notUpdatedNodes++
+			continue
+		}
+		nn.ExternalNetworkCode = nn.Code
+		nn.UpdatedDate = time.Now().UTC()
+		err := lib.SetFirestoreErr(models.NetworkNodesCollection, nn.Uid, nn)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		err = nn.SaveBigQuery("")
+		if err != nil {
+			log.Println(nn.Uid + " error: " + err.Error())
+		}
+		updatedNodes++
+		log.Println("done")
+	}
+
+	log.Printf("Updated Nodes: %d", updatedNodes)
+	log.Printf("Not Updated Nodes: %d", notUpdatedNodes)
+}
