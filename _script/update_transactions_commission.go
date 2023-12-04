79d3020c30e49fd776fecb93ddc7ac73305d5f8f
Diogo Carvalho <diogo.carvalho@wopta.it>
2023-12-04 16:28:37 +0100

    feat: add UpdateTransactions script

--- _script/update_transactions_commission.go
+++ _script/update_transactions_commission.go
@@ -1,25 +1,82 @@
 package _script
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"time"
 
+	"cloud.google.com/go/firestore"
 	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/models"
 	"github.com/wopta/goworkspace/policy"
 	"github.com/wopta/goworkspace/product"
 	"github.com/wopta/goworkspace/transaction"
+	"google.golang.org/api/iterator"
 )
+
+func UpdateTransactions() {
+	var (
+		trs                  = make([]models.Transaction, 0)
+		tr                   models.Transaction
+		modifiedTransactions = make([]string, 0)
+	)
+
+	// GET all transactions from firestore
+	ctx := context.Background()
+	client, _ := firestore.NewClient(ctx, os.Getenv("GOOGLE_PROJECT_ID"))
+	iter := client.Collection(models.TransactionsCollection).Documents(ctx)
+	defer iter.Stop()
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Printf("[UpdateTransactions] ERROR iterator: %s", err.Error())
+			return
+		}
+		if err := doc.DataTo(&tr); err != nil {
+			fmt.Printf("[UpdateTransactions] ERROR datato: %s", err.Error())
+			return
+		}
+		trs = append(trs, tr)
+	}
+
+	fmt.Printf("[UpdateTransactions] Found %d transactions/n", len(trs))
+
+	for _, tr := range trs {
+		// set updateDate to now
+		tr.UpdateDate = time.Now().UTC()
+
+		// save to firestore
+		err := lib.SetFirestoreErr(models.TransactionsCollection, tr.Uid, tr)
+		if err != nil {
+			fmt.Printf("[UpdateTransactions] ERROR saving to firestore: %s", err.Error())
+			return
+		}
+		modifiedTransactions = append(modifiedTransactions, tr.Uid)
+		// save to bigquery
+		tr.BigQuerySave("")
+	}
+
+	fmt.Printf("[UpdateTransactions] Modified %d transactions: %v/n", len(modifiedTransactions), modifiedTransactions)
+	fmt.Println("[UpdateTransactions] done")
+}
+
+type QueryResult struct {
+	Uid string `bigquery:"uid"`
+}
 
 func UpdateTransactionsCommission() {
 	var modifiedTransactions = make([]string, 0)
 
-	// TODO: wait for the updated schema where we should have isDelete field and updateDate
 	query := fmt.Sprintf(
-		"SELECT uid FROM `%s.%s` WHERE commissions = 0",
+		"SELECT uid FROM `%s.%s` WHERE isDelete = false AND commissions = 0",
 		models.WoptaDataset,
 		models.TransactionsViewCollection,
 	)
-	transactionUids, err := lib.QueryRowsBigQuery[string](query)
+	transactionUids, err := lib.QueryRowsBigQuery[QueryResult](query)
 	if err != nil {
 		fmt.Printf("[UpdateNetworkTransactions] error getting network transactions: %s", err.Error())
 		return
@@ -46,8 +103,8 @@ func UpdateTransactionsCommission() {
 
 	policyMap := make(map[string]*models.Policy)
 
-	for _, uid := range transactionUids {
-		tr := transaction.GetTransactionByUid(uid, "")
+	for _, t := range transactionUids {
+		tr := transaction.GetTransactionByUid(t.Uid, "")
 		var plc = policyMap[tr.PolicyUid]
 
 		if plc == nil {
@@ -55,7 +112,6 @@ func UpdateTransactionsCommission() {
 			plc = &temp
 			policyMap[tr.PolicyUid] = plc
 		}
-		fmt.Printf("[UpdateTransactionsCommission] transaction.PolicyUid - '%s' | policy.Uid - '%s'\n", tr.PolicyUid, plc.Uid)
 
 		err := updateTransactionCommission(tr, plc, productMap[tr.PolicyName])
 		if err != nil {
@@ -73,13 +129,14 @@ func updateTransactionCommission(tr *models.Transaction, policy *models.Policy, 
 	fmt.Printf("[updateTransactionCommission] new commission %.2f\n", commissionMga)
 
 	tr.Commissions = commissionMga
+	tr.UpdateDate = time.Now().UTC()
 
-	// err := lib.SetFirestoreErr(models.TransactionsCollection, tr.Uid, tr)
-	// if err != nil {
-	// 	log.Printf("[updateTransactionCommission] error saving transaction to firestore: %s", err.Error())
-	// 	return err
-	// }
-	// tr.BigQuerySave("")
+	err := lib.SetFirestoreErr(models.TransactionsCollection, tr.Uid, tr)
+	if err != nil {
+		fmt.Printf("[updateTransactionCommission] error saving transaction to firestore: %s", err.Error())
+		return err
+	}
+	tr.BigQuerySave("")
 
 	return nil
 }
