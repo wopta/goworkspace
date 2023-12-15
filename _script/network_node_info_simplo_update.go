8cd02b7e8c6a231b0e47513fae2fbf356cb4b140
Yousef Hammar <yousef.hammar@wopta.it>
2023-12-15 19:39:23 +0100

    feat: script to update simplo info network nodes

---
+++ _script/network_node_info_simplo_update.go
@@ -0,0 +1,32 @@
+package _script
+
+import (
+	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
+	"log"
+)
+
+func UpdateNetworkNodeInfoSimplo() {
+	var (
+		networkNodes []models.NetworkNode
+	)
+
+	docsnap := lib.WhereFirestore(models.NetworkNodesCollection, "authId", "!=", "")
+	networkNodes = models.NetworkNodeToListData(docsnap)
+
+	for _, nn := range networkNodes {
+		nn.HasAnnex = true
+		nn.Designation = "Addetto Attività intermediazione al di fuori dei locali"
+		nn.WorksForUid = "__wopta__"
+
+		if nn.Type == models.AgencyNetworkNodeType || nn.Type == models.AgentNetworkNodeType {
+			err := lib.SetFirestoreErr(models.NetworkNodesCollection, nn.Uid, nn)
+			if err != nil {
+				log.Printf("error updating network node %s: %s", nn.Code, err.Error())
+				continue
+			}
+
+			nn.SaveBigQuery("")
+		}
+	}
+}
