f81edd4500553b4c9187f074a0fe9b38b40adc1d
Yousef Hammar <yousef.hammar@wopta.it>
2023-12-22 14:48:19 +0100

    feat: add functions to lower, upper, trim strings

--- lib/format-string.go
+++ lib/format-string.go
@@ -1,7 +1,37 @@
 package lib
 
-import "github.com/dustin/go-humanize"
+import (
+	"github.com/dustin/go-humanize"
+	"strings"
+)
 
 func HumanaizePriceEuro(price float64) string {
 	return "€ " + humanize.FormatFloat("#.###,##", price)
+}
+
+func Capitalize(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	return strings.ToUpper(s[:1]) + strings.ToLower(s[1:])
+}
+
+func TrimSpace(s string) string {
+	return strings.TrimSpace(s)
+}
+
+func ToUpper(s string) string {
+	return strings.ToUpper(TrimSpace(s))
+}
+
+func ToLower(s string) string {
+	return strings.ToLower(TrimSpace(s))
+}
+
+func ReplaceAll(s, old, new string) string {
+	return strings.ReplaceAll(s, old, new)
+}
+
+func Replace(s, old, new string, n int) string {
+	return strings.Replace(s, old, new, n)
 }
