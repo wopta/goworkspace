664fc745a027958782b0ae0e5460ac6d4c29ea29
Nelson Hidalgo <nelson.herrera@wopta.it>
2024-01-03 16:46:20 +0100

    Updated the GetPoliciesByQuery to use Firestore instead of BigQuery

--- policy/get-policies-by-query-handler.go
+++ policy/get-policies-by-query-handler.go
@@ -10,13 +10,23 @@ import (
 	"net/http"
 )
 
+type GetPoliciesReq struct {
+	Queries []models.Query `json:"queries,omitempty"`
+	Limit   int            `json:"limit"`
+	Page    int            `json:"page"`
+}
+
+type GetPoliciesResp struct {
+	Policies []models.Policy `json:"policies"`
+}
+
 func GetPoliciesByQueryFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
 	var (
 		req        GetPoliciesReq
 		response   GetPoliciesResp
 		limitValue = 10
 	)
-	log.Println("GetPolicies")
+	log.Println("[GetPoliciesByQueryFx]")
 
 	body := lib.ErrorByte(io.ReadAll(r.Body))
 	err := json.Unmarshal(body, &req)
@@ -30,43 +40,13 @@ func GetPoliciesByQueryFx(w http.ResponseWriter, r *http.Request) (string, inter
 		limitValue = req.Limit
 	}
 
-	firePolicy := lib.GetDatasetByEnv(r.Header.Get("origin"), "policy")
-
-	fireQueries := lib.Firequeries{
-		Queries: make([]lib.Firequery, 0),
-	}
-
-	for index, q := range req.Queries {
-		log.Printf("query %d/%d field: \"%s\" op: \"%s\" value: \"%v\"", index+1, len(req.Queries), q.Field, q.Op, q.Value)
-		fireQueries.Queries = append(fireQueries.Queries, lib.Firequery{
-			Field:      q.Field,
-			Operator:   q.Op,
-			QueryValue: q.Value,
-		})
-	}
-
-	docsnap, err := fireQueries.FirestoreWhereLimitFields(firePolicy, limitValue)
+	response.Policies, err = GetPoliciesByQueriesBigQuery(models.WoptaDataset, models.PoliciesViewCollection, req.Queries, limitValue)
 	if err != nil {
+		log.Println("[GetPoliciesByQueryFx] query error: ", err.Error())
 		return "", nil, err
 	}
-
-	response.Policies = models.PolicyToListData(docsnap)
-	log.Printf("GetPolicies: found %d policies", len(response.Policies))
+	log.Printf("[GetPoliciesByQueryFx]: found %d policies", len(response.Policies))
 
 	jsonOut, err := json.Marshal(response)
 	return string(jsonOut), response, err
-}
-
-type GetPoliciesResp struct {
-	Policies []models.Policy `json:"policies"`
-}
-
-type GetPoliciesReq struct {
-	Queries []struct {
-		Field string      `json:"field"`
-		Op    string      `json:"op"`
-		Value interface{} `json:"value"`
-	} `json:"queries,omitempty"`
-	Limit int `json:"limit"`
-	Page  int `json:"page"`
 }
