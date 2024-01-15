4b3a2b3593046751a38e24ad71e522e187e937e8
Yousef Hammar <yousef.hammar@wopta.it>
2024-01-15 14:17:22 +0100

    feat: init get policy single attachment API

--- policy/get-policy-attachment-handler.go
+++ policy/get-policy-attachment-handler.go
@@ -3,137 +3,82 @@ package policy
 import (
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
+	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
 	"io"
 	"log"
 	"net/http"
-	"regexp"
+	"os"
 	"strings"
-
-	"github.com/wopta/goworkspace/lib"
-	"github.com/wopta/goworkspace/models"
-	"github.com/wopta/goworkspace/wiseproxy"
 )
 
-func GetPolicyAttachmentFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
-	w.Header().Set("Access-Control-Allow-Methods", "GET")
-
-	var (
-		response GetPolicyAttachmentsResponse
-		e        error
-	)
-
-	if e != nil {
-		return "{}", nil, nil
-	}
-
-	attachments, err := GetPolicyAttachments(r.Header.Get("policyUid"), r.Header.Get("origin"))
-	if err != nil {
-		log.Println("GetPolicyAttachments Error: " + err.Error())
-		return "{}", nil, nil
-	}
-
-	response.Attachments = attachments
-	res, err := json.Marshal(response)
-	if err != nil {
-		log.Println("AttachmentsMarshal Error: " + err.Error())
-	}
-
-	return string(res), nil, nil
+type GetAttachmentReq struct {
+	PolicyUid string `json:"policyUid"`
+	Filename  string `json:"filename"`
 }
 
-func GetPolicyAttachments(policyUid string, origin string) ([]models.Attachment, error) {
+type GetAttachmentResp struct {
+	RawDoc string `json:"rawDoc"`
+}
+
+func GetAttachmentFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
 	var (
-		err         error
-		wiseToken   *string = nil
-		attachments []models.Attachment
+		err    error
+		policy models.Policy
+		req    GetAttachmentReq
+		resp   GetAttachmentResp
 	)
-	if strings.HasPrefix(policyUid, "wise:") {
-		// get attachment from wise
-		var (
-			wisePolicy         WiseCompletePolicyResponse
-			attachmentResponse WiseAttachmentResponse
-			wiseResponseData   []byte
-		)
-		wiseAttachmentId := strings.Split(policyUid, ":")[1]
-		request := []byte(fmt.Sprintf(`{"idPolizza": "%s", "cdLingua": "it"}`, wiseAttachmentId))
-		ioReader := wiseproxy.WiseProxyObj("WebApiProduct/Api/GetPolizzaCompleta", request, http.MethodPost)
 
-		defer ioReader.Close()
-		wiseResponseData, err = io.ReadAll(ioReader)
+	log.SetPrefix("[GetAttachmentFx]")
 
-		if err != nil {
-			return make([]models.Attachment, 0), err
-		}
-		err = json.Unmarshal(wiseResponseData, &wisePolicy)
+	log.Println("Handler start -----------------------------------------------")
 
-		for _, wiseAttachment := range wisePolicy.Policy.Attachments {
-			var attachment models.Attachment
+	body := lib.ErrorByte(io.ReadAll(r.Body))
+	defer r.Body.Close()
+	log.Printf("request body: %s", string(body))
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		log.Printf("error unmarshaling request: %s", err.Error())
+		return "", nil, err
+	}
 
-			request := []byte(fmt.Sprintf(`{"txRifAllegato": "%s", "cdLingua": "it"}`, wiseAttachment.Id))
-			ioReader, wiseToken = wiseproxy.WiseBatch("WebApiProduct/Api/recuperaAllegato", request, http.MethodPost, wiseToken)
+	log.Printf("retrieving policy %s from Firestore...", req.PolicyUid)
 
-			defer ioReader.Close()
-			wiseResponseData, err = io.ReadAll(ioReader)
+	policy, err = GetPolicy(req.PolicyUid, "")
+	if err != nil {
+		log.Printf("error retrieving policy %s from Firestore: %s", req.PolicyUid, err.Error())
+		return "", nil, err
+	}
 
-			if err != nil {
-				return make([]models.Attachment, 0), err
+	log.Println("checking if requested attachment is present...")
+
+	if policy.Attachments != nil {
+		for _, att := range *policy.Attachments {
+			if strings.EqualFold(att.FileName, req.Filename) {
+				rawDoc, err := downloadAttachment(att.Link)
+				if err != nil {
+					log.Printf("error downloading attacchment %s from Google Bucket: %s", att.FileName, err.Error())
+					return "", nil, err
+				}
+				resp = GetAttachmentResp{
+					RawDoc: rawDoc,
+				}
+				rawResp, err := json.Marshal(resp)
+				return string(rawResp), rawResp, err
 			}
-			err = json.Unmarshal(wiseResponseData, &attachmentResponse)
-			attachment.Byte = attachmentResponse.Base64Attachment
-			attachment.Name = wiseAttachment.Name
-			attachment.FileName = wiseAttachment.Name
-			attachments = append(attachments, attachment)
 		}
-		return attachments, err
 	}
-
-	var policy models.Policy
-
-	log.Println("Getting attachments for policy saved in Wopta")
-	if policy, err = GetPolicy(policyUid, origin); err != nil {
-		log.Println("Error when getting policy: " + err.Error())
-		return make([]models.Attachment, 0), err
-	}
-
-	expr, err := regexp.Compile("gs://(?P<bucketName>(?:[^/])*)/(?P<fileName>((?:[^/]*/)*)(.*))")
-	log.Printf("Found %d attachment(s) for policy %s", len(*policy.Attachments), policy.Uid)
-	for _, attachment := range *policy.Attachments {
-		var responseAttachment models.Attachment
-		if len(attachment.Link) == 0 {
-			log.Printf("Attachment %s has empty link, skipping", attachment.FileName)
-			continue
-		}
-		matches := findNamedMatches(expr, attachment.Link)
-		log.Printf("Found %s with bucketName=%s and fileName=%s", attachment.FileName, matches["bucketName"], matches["fileName"])
-		fileData, _ := lib.GetFromStorageErr(matches["bucketName"], matches["fileName"], "")
-
-		responseAttachment.FileName = attachment.FileName
-		responseAttachment.ContentType = attachment.ContentType
-		responseAttachment.Name = attachment.Name
-		responseAttachment.Byte = base64.StdEncoding.EncodeToString(fileData)
-
-		attachments = append(attachments, responseAttachment)
-	}
-
-	log.Printf("Sending %d attachment(s)", len(attachments))
-	return attachments, err
+	return "", nil, err
 }
 
-func findNamedMatches(regex *regexp.Regexp, str string) map[string]string {
-	match := regex.FindStringSubmatch(str)
-
-	results := map[string]string{}
-	for i, name := range match {
-		results[regex.SubexpNames()[i]] = name
+func downloadAttachment(gsLink string) (string, error) {
+	if !strings.Contains(gsLink, "gs://") {
+		gsLink = "gs://" + os.Getenv("GOOGLE_STORAGE_BUCKET") + "/" + gsLink
 	}
-	return results
-}
-
-type WiseAttachmentResponse struct {
-	Base64Attachment string `json:"fileAllegato"`
-}
-
-type GetPolicyAttachmentsResponse struct {
-	Attachments []models.Attachment `json:"attachments"`
+	rawDoc, err := lib.ReadFileFromGoogleStorage(gsLink)
+	if err != nil {
+		log.Printf("[GetPolicyFx] error reading document from Google Storage: %s", err.Error())
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(rawDoc), nil
 }
