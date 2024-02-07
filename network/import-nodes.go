06921a10b9777cd41ea5cb44273680449a09030f
Yousef Hammar <yousef.hammar@wopta.it>
2024-02-07 15:02:11 +0100

    feat: implemented request unmarshaling, file decode and save to google bucket

--- network/import-nodes.go
+++ network/import-nodes.go
@@ -1,7 +1,83 @@
 package network
 
-import "net/http"
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/wopta/goworkspace/lib"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type ImportNodesReq struct {
+	Filename      string `json:"filename"`
+	Bytes         string `json:"bytes"`
+	MimeType      string `json:"mimeType"`
+	StartPipeline *bool  `json:"startPipeline,omitempty"`
+}
 
 func ImportNodesFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
+	var (
+		err           error
+		req           ImportNodesReq
+		startPipeline = false
+	)
+
+	log.SetPrefix("ImportNodesFx ")
+	defer log.SetPrefix("")
+
+	log.Println("Handler Start -----------------------------------------------")
+
+	body := lib.ErrorByte(io.ReadAll(r.Body))
+	log.Printf("Request body: %s", string(body))
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		log.Printf("Error unmarshiling request body: %s", err.Error())
+		return "", nil, err
+	}
+
+	// check file mimetype
+	if req.MimeType != "text/csv" {
+		log.Printf("File format %s not supported", req.MimeType)
+		return "", nil, errors.New("file format not supported")
+	}
+
+	// convert csv to bytes
+	data, err := base64.StdEncoding.DecodeString(req.Bytes)
+	if err != nil {
+		log.Printf("Error decoding file: %s", err.Error())
+		return "", nil, err
+	}
+
+	// load dataframe
+	df := lib.CsvToDataframe(data)
+	log.Printf("#row: %02d #col: %02d", df.Nrow(), df.Ncol())
+	for k, v := range df.Records() {
+		log.Printf("k: %d", k)
+		log.Printf("v: %s", v)
+	}
+
+	if req.StartPipeline != nil {
+		startPipeline = *req.StartPipeline
+	}
+
+	if !startPipeline {
+		log.Printf("Saving import file to Google Bucket...")
+		filePath := fmt.Sprintf("dataflow/in_network_node/%s", "prova.csv")
+		_, err = lib.PutToGoogleStorage(os.Getenv("GOOGLE_STORAGE_BUCKET"), filePath, data)
+		if err != nil {
+			log.Printf("Error saving import file to Google Bucket: %s", err.Error())
+			return "", nil, err
+		}
+		log.Printf("Import file saved into Google Bucket")
+
+		// TODO: start dataflow pipeline
+	}
+
+	log.Println("Handler End -------------------------------------------------")
+
 	return "", nil, nil
 }
