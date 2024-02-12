4c0fcdbf9c7ca76c28d6428353c625c448c32f49
Yousef Hammar <yousef.hammar@wopta.it>
2024-02-12 15:43:50 +0100

    feat: add fiscalCode regex to check format

--- network/import-nodes.go
+++ network/import-nodes.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -480,6 +481,16 @@ func validateRow(row []string) error {
 			requiredFields = append(requiredFields, 30)
 			requiredFields = append(requiredFields, 31)
 		}
+	}
+
+	// check fiscalCode format
+	fiscalCodePattern := "^(?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\\dLMNP-V]{2}(?:[" +
+		"A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[" +
+		"AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\\dLMNP-V]" +
+		"{2}|[A-M][0L](?:[1-9MNP-V][\\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$"
+	regExp, _ := regexp.Compile(fiscalCodePattern)
+	if lib.SliceContains(requiredFields, 22) && !regExp.MatchString(row[22]) {
+		return errors.New("invalid fiscal code")
 	}
 
 	for fieldIndex, fieldValue := range row {
