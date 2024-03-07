906a9a363dc470c768853aa39c4d584ee9c83c07
Yousef Hammar <yousef.hammar@wopta.it>
2024-03-07 10:58:15 +0100

    feat: implemented DFS algorithm

--- _script/create_node_tree_structure.go
+++ _script/create_node_tree_structure.go
@@ -5,18 +5,74 @@ import (
 	"github.com/wopta/goworkspace/models"
 	"github.com/wopta/goworkspace/network"
 	"log"
+	"strings"
 )
 
-func CreateNodeTreeStructure() {
+type nodeInfo struct {
+	Uid        string
+	Level      int
+	BreadCrumb string
+	Parents    []string
+}
 
+func CreateNodeTreeStructure() {
 	log.Println("function start ----------------------------------------------")
 
-	allNodes, err := getAllNodes()
+	//nodesMap := make(map[string][]Child)
+
+	nodesList, err := getAllNodes()
 	if err != nil {
 		panic(err)
 	}
 
-	log.Println(len(allNodes))
+	nodesList = lib.SliceFilter(nodesList, func(node models.NetworkNode) bool {
+		return node.Type != models.PartnershipNetworkNodeType
+	})
+
+	dbNodes := lib.SliceFilter(nodesList, func(node models.NetworkNode) bool {
+		return node.ParentUid == ""
+	})
+
+	firstLevelNodes := lib.SliceMap(dbNodes, func(node models.NetworkNode) models.NetworkNode {
+		return node
+	})
+
+	toBeVisitedNodes := make([]nodeInfo, 0)
+	visitedNodes := make([]nodeInfo, 0)
+
+	for _, node := range firstLevelNodes {
+		toBeVisitedNodes = append(toBeVisitedNodes, nodeInfo{
+			Uid:        node.Uid,
+			Level:      1,
+			BreadCrumb: node.Code,
+			Parents:    []string{},
+		})
+	}
+
+	for len(toBeVisitedNodes) > 0 {
+		index := len(toBeVisitedNodes) - 1
+		currentNode := toBeVisitedNodes[index]
+		children := lib.SliceFilter(nodesList, func(node models.NetworkNode) bool {
+			return node.ParentUid == currentNode.Uid
+		})
+		toBeVisitedNodes = toBeVisitedNodes[:index]
+		parents := append(currentNode.Parents, currentNode.Uid)
+		for _, child := range children {
+			toBeVisitedNodes = append(toBeVisitedNodes, nodeInfo{
+				Uid:        child.Uid,
+				Level:      currentNode.Level + 1,
+				BreadCrumb: currentNode.BreadCrumb + " > " + child.Code,
+				Parents:    parents,
+			})
+		}
+		visitedNodes = append(visitedNodes, currentNode)
+
+	}
+
+	for _, nn := range visitedNodes {
+		log.Printf("uid: %s\tparents: %s\tbreadCrumb: %s\tlevel: %02d\n", nn.Uid,
+			strings.Join(nn.Parents, ", "), nn.BreadCrumb, nn.Level)
+	}
 
 	log.Println("function end ------------------------------------------------")
 }
@@ -29,7 +85,7 @@ func getAllNodes() ([]models.NetworkNode, error) {
 		return nil, err
 	}
 	filteredNodes := lib.SliceFilter(allNodes, func(node models.NetworkNode) bool {
-		return node.Type != models.PartnershipNetworkNodeType && node.Type != models.AreaManagerNetworkNodeType
+		return node.Type != models.PartnershipNetworkNodeType
 	})
 	log.Printf("found %02d nodes", len(filteredNodes))
 	return filteredNodes, nil
