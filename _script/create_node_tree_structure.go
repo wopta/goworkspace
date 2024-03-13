1d2f6c01328e78295060b4ceef888d6201e1add6
Yousef Hammar <yousef.hammar@wopta.it>
2024-03-13 11:32:06 +0100

    refactor: using new NetworkTreeElement struct and constant

--- _script/create_node_tree_structure.go
+++ _script/create_node_tree_structure.go
@@ -1,7 +1,6 @@
 package _script
 
 import (
-	"cloud.google.com/go/bigquery"
 	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/models"
 	"github.com/wopta/goworkspace/network"
@@ -10,28 +9,14 @@ import (
 )
 
 type nodeInfo struct {
-	Uid        string
-	Code       string
-	Level      int
-	BreadCrumb string
-	RootUid    string
-	ParentUid  string
-	Name       string
-	Ancestors  []nodeInfo
-}
-
-type bigQueryNodeInfo struct {
-	RootUid       string                `bigquery:"rootUid"`
-	ParentUid     string                `bigquery:"parentUid"`
-	ChildUid      string                `bigquery:"childUid"`
-	Name          string                `bigquery:"name"`
-	AbsoluteLevel int                   `bigquery:"absoluteLevel"`
-	RelativeLevel int                   `bigquery:"relativeLevel"`
-	CreationDate  bigquery.NullDateTime `bigquery:"creationDate"`
+	models.NetworkTreeElement
+	Ancestors []nodeInfo
 }
 
 func CreateNodeTreeStructure() {
 	log.Println("function start ----------------------------------------------")
+
+	creationDate := lib.GetBigQueryNullDateTime(time.Now().UTC())
 
 	nodesList, err := getAllNodes()
 	if err != nil {
@@ -55,12 +40,15 @@ func CreateNodeTreeStructure() {
 
 	for _, node := range firstLevelNodes {
 		toBeVisitedNodes = append(toBeVisitedNodes, nodeInfo{
-			Uid:       node.Uid,
-			Code:      node.Code,
-			Level:     1,
-			RootUid:   node.Uid,
-			ParentUid: "",
-			Name:      node.GetName(),
+			NetworkTreeElement: models.NetworkTreeElement{
+				RootUid:       node.Uid,
+				ParentUid:     "",
+				NodeUid:       node.Uid,
+				Name:          node.GetName(),
+				AbsoluteLevel: 1,
+				RelativeLevel: 0,
+				CreationDate:  creationDate,
+			},
 			Ancestors: []nodeInfo{},
 		})
 	}
@@ -69,18 +57,20 @@ func CreateNodeTreeStructure() {
 		index := len(toBeVisitedNodes) - 1
 		currentNode := toBeVisitedNodes[index]
 		children := lib.SliceFilter(nodesList, func(node models.NetworkNode) bool {
-			return node.ParentUid == currentNode.Uid
+			return node.ParentUid == currentNode.NodeUid
 		})
 		toBeVisitedNodes = toBeVisitedNodes[:index]
 		parents := append(currentNode.Ancestors, currentNode)
 		for _, child := range children {
 			toBeVisitedNodes = append(toBeVisitedNodes, nodeInfo{
-				Uid:       child.Uid,
-				Code:      child.Code,
-				Level:     currentNode.Level + 1,
-				RootUid:   currentNode.RootUid,
-				ParentUid: currentNode.Uid,
-				Name:      child.GetName(),
+				NetworkTreeElement: models.NetworkTreeElement{
+					RootUid:       currentNode.RootUid,
+					ParentUid:     currentNode.NodeUid,
+					NodeUid:       child.Uid,
+					Name:          child.GetName(),
+					AbsoluteLevel: currentNode.AbsoluteLevel + 1,
+					CreationDate:  creationDate,
+				},
 				Ancestors: parents,
 			})
 		}
@@ -89,29 +79,10 @@ func CreateNodeTreeStructure() {
 	}
 
 	for _, nn := range visitedNodes {
-		if len(nn.Ancestors) > 0 {
-			dbNode := bigQueryNodeInfo{
-				ChildUid:      nn.Uid,
-				AbsoluteLevel: nn.Level,
-				RootUid:       "",
-				ParentUid:     nn.ParentUid,
-				Name:          nn.Name,
-				CreationDate:  lib.GetBigQueryNullDateTime(time.Now().UTC()),
-			}
-
-			for _, p := range nn.Ancestors {
-				dbNode.RootUid = p.Uid
-				dbNode.RelativeLevel = nn.Level - p.Level
-
-				log.Printf("rootUid: %s\tparentUid: %s\tchildUid: %s\tchildLevel: %02d\tparentLevel: %02d\trelativeLevel: %02d\t\n",
-					dbNode.RootUid, dbNode.ParentUid, nn.Uid, dbNode.AbsoluteLevel, p.Level, dbNode.RelativeLevel)
-
-				err = lib.InsertRowsBigQuery("wopta", "node-tree-structure", dbNode)
-				if err != nil {
-					log.Printf("insert error: %s", err.Error())
-					return
-				}
-			}
+		err = writeNodeToBigQuery(nn)
+		if err != nil {
+			log.Printf("error writing node %s to BigQuery: %s", nn.NodeUid, err.Error())
+			return
 		}
 	}
 
@@ -130,4 +101,31 @@ func getAllNodes() ([]models.NetworkNode, error) {
 	})
 	log.Printf("found %02d nodes", len(filteredNodes))
 	return filteredNodes, nil
+}
+
+func writeNodeToBigQuery(node nodeInfo) error {
+	if len(node.Ancestors) > 0 {
+		dbNode := models.NetworkTreeElement{
+			NodeUid:       node.NodeUid,
+			AbsoluteLevel: node.AbsoluteLevel,
+			ParentUid:     node.ParentUid,
+			Name:          node.Name,
+			CreationDate:  lib.GetBigQueryNullDateTime(time.Now().UTC()),
+		}
+
+		for _, p := range node.Ancestors {
+			dbNode.RootUid = p.NodeUid
+			dbNode.RelativeLevel = node.AbsoluteLevel - p.AbsoluteLevel
+
+			log.Printf("rootUid: %s\tparentUid: %s\tnodeUid: %s\tchildLevel: %02d\tparentLevel: %02d\trelativeLevel: %02d\t\n",
+				dbNode.RootUid, dbNode.ParentUid, node.NodeUid, dbNode.AbsoluteLevel, p.AbsoluteLevel, dbNode.RelativeLevel)
+
+			err := lib.InsertRowsBigQuery(models.WoptaDataset, models.NetworkTreeStructureTable, dbNode)
+			if err != nil {
+				log.Printf("insert error: %s", err.Error())
+				return err
+			}
+		}
+	}
+	return nil
 }
