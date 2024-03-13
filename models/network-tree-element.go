f8150ea09c9686b5aee436f3040ffe2e18228f98
Yousef Hammar <yousef.hammar@wopta.it>
2024-03-13 11:31:30 +0100

    feat: add NetworkTreeElement struct

---
+++ models/network-tree-element.go
@@ -0,0 +1,13 @@
+package models
+
+import "cloud.google.com/go/bigquery"
+
+type NetworkTreeElement struct {
+	RootUid       string                `json:"rootUid" bigquery:"rootUid"`
+	ParentUid     string                `json:"parentUid" bigquery:"parentUid"`
+	NodeUid       string                `json:"nodeUid" bigquery:"nodeUid"`
+	Name          string                `json:"name" bigquery:"name"`
+	AbsoluteLevel int                   `json:"absoluteLevel" bigquery:"absoluteLevel"`
+	RelativeLevel int                   `json:"relativeLevel" bigquery:"relativeLevel"`
+	CreationDate  bigquery.NullDateTime `bigquery:"creationDate"`
+}
