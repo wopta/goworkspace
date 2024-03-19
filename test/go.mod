c3a150ebaa2d7b7b719768bfd996ca3520c8df76
Diogo Carvalho <diogo.carvalho@wopta.it>
2024-03-19 12:18:42 +0100

    feat: chi router

--- test/go.mod
+++ test/go.mod
@@ -5,47 +5,54 @@ go 1.21
 replace github.com/wopta/goworkspace/test => ./
 
 require (
-	github.com/GoogleCloudPlatform/functions-framework-go v1.6.1
-	github.com/wopta/goworkspace/lib v1.0.63
-	github.com/wopta/goworkspace/models v1.1.18
+	firebase.google.com/go/v4 v4.10.0
+	github.com/GoogleCloudPlatform/functions-framework-go v1.8.1
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/cors v1.2.1
+	github.com/wopta/goworkspace/lib v1.0.105
+	github.com/wopta/goworkspace/mail v1.0.92
+	github.com/wopta/goworkspace/models v1.1.92
+	github.com/wopta/goworkspace/policy v1.0.82
 )
 
 require (
-	cloud.google.com/go v0.110.2 // indirect
-	cloud.google.com/go/bigquery v1.51.1 // indirect
-	cloud.google.com/go/compute v1.19.0 // indirect
+	cloud.google.com/go v0.110.8 // indirect
+	cloud.google.com/go/bigquery v1.53.0 // indirect
+	cloud.google.com/go/compute v1.23.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/firestore v1.9.0 // indirect
-	cloud.google.com/go/iam v0.13.0 // indirect
-	cloud.google.com/go/longrunning v0.4.1 // indirect
-	cloud.google.com/go/storage v1.29.0 // indirect
+	cloud.google.com/go/firestore v1.12.0 // indirect
+	cloud.google.com/go/iam v1.1.1 // indirect
+	cloud.google.com/go/longrunning v0.5.1 // indirect
+	cloud.google.com/go/pubsub v1.33.0 // indirect
+	cloud.google.com/go/storage v1.30.1 // indirect
 	firebase.google.com/go v3.13.0+incompatible // indirect
-	firebase.google.com/go/v4 v4.10.0 // indirect
 	github.com/MicahParks/keyfunc v1.5.1 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220527190237-ee62e23da966 // indirect
 	github.com/apache/arrow/go/v12 v12.0.0 // indirect
 	github.com/apache/thrift v0.16.0 // indirect
 	github.com/bmatcuk/doublestar v1.3.2 // indirect
-	github.com/cloudevents/sdk-go/v2 v2.6.1 // indirect
+	github.com/boombuler/barcode v1.0.1 // indirect
+	github.com/cloudevents/sdk-go/v2 v2.14.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/emirpasic/gods v1.12.0 // indirect
-	github.com/go-chi/chi/v5 v5.0.12 // indirect
 	github.com/go-gota/gota v0.12.0 // indirect
+	github.com/go-pdf/fpdf v0.8.0 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/flatbuffers v2.0.8+incompatible // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/s2a-go v0.1.3 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
-	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
+	github.com/google/s2a-go v0.1.4 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.4 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/hyperjumptech/grule-rule-engine v1.12.0 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/johnfercher/maroto v0.42.0 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
 	github.com/kevinburke/ssh_config v0.0.0-20190725054713-01f96b0aa0cd // indirect
 	github.com/klauspost/asmfmt v1.3.2 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
@@ -61,9 +68,16 @@ require (
 	github.com/pkg/sftp v1.13.5 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.3 // indirect
+	github.com/ruudk/golang-pdf417 v0.0.0-20201230142125-a7e3863a1245 // indirect
 	github.com/sergi/go-diff v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/src-d/gcfg v1.4.0 // indirect
+	github.com/ttacon/builder v0.0.0-20170518171403-c099f663e1c2 // indirect
+	github.com/ttacon/libphonenumber v1.2.1 // indirect
+	github.com/wopta/goworkspace/document v1.1.60 // indirect
+	github.com/wopta/goworkspace/network v1.0.44 // indirect
+	github.com/wopta/goworkspace/product v1.0.92 // indirect
+	github.com/wopta/goworkspace/wiseproxy v1.0.3 // indirect
 	github.com/xanzy/ssh-agent v0.2.1 // indirect
 	github.com/xuri/efp v0.0.0-20220603152613-6918739fd470 // indirect
 	github.com/xuri/excelize/v2 v2.7.0 // indirect
@@ -73,23 +87,26 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/mod v0.8.0 // indirect
-	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/oauth2 v0.7.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	golang.org/x/time v0.1.0 // indirect
-	golang.org/x/tools v0.6.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.11.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/tools v0.10.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gonum.org/v1/gonum v0.11.0 // indirect
-	google.golang.org/api v0.122.0 // indirect
+	google.golang.org/api v0.128.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/appengine/v2 v2.0.2 // indirect
-	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
-	google.golang.org/grpc v1.55.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	google.golang.org/genproto v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/src-d/go-billy.v4 v4.3.2 // indirect
 	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
--- test/routes.go
+++ test/routes.go
@@ -1,13 +1,19 @@
 package test
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"slices"
+	"strings"
 
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/auth"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 // for local testing only
@@ -16,20 +22,7 @@ func init() {
 	functions.HTTP("Test", Test)
 }
 
-func newMux() *http.ServeMux {
-	prefix := ""
-
-	if os.Getenv("env") == "local" {
-		prefix = "/test"
-	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc(prefix+"/test1", test1)
-	mux.HandleFunc(prefix+"/test2/param", test2)
-	return mux
-}
-
-func newChiMux(module string, routes []struct{}) *chi.Mux {
+func Test(w http.ResponseWriter, r *http.Request) {
 	prefix := ""
 
 	if os.Getenv("env") == "local" {
@@ -37,21 +30,84 @@ func newChiMux(module string, routes []struct{}) *chi.Mux {
 	}
 
 	mux := chi.NewRouter()
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.RealIP)
 	mux.Use(middleware.Logger)
-	mux.Get(prefix+"/test1", test1)
-	mux.Get(prefix+"/test2/{param}", test2)
+	mux.Use(middleware.Recoverer)
+	mux.Use(CorsMiddleware)
+	w.Header().Add("Content-type", "application/json")
 
-	mux.Route("/articles", func(r chi.Router) {
-		r.Use(middleware.BasicAuth("", map[string]string{}))
-		r.Get("/", test1)
+	mux.Route(prefix, func(r chi.Router) {
+		r.Mount("/public", publicRouter())
+		r.Mount("/network", networkRouter())
+		r.Mount("/internal", internalRouter())
+		r.Mount("/admin", adminRouter())
 	})
 
-	return mux
+	mux.ServeHTTP(w, r)
 }
 
-func Test(w http.ResponseWriter, r *http.Request) {
-	mux := newChiMux("test", nil)
-	mux.ServeHTTP(w, r)
+func publicRouter() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.WithValue("roles", []string{"all"}))
+	r.Use(AppCheckMiddleware)
+	r.Use(CheckEntitlement)
+
+	r.Route("/test2", func(r chi.Router) {
+		r.Get("/", test2)
+		r.Get("/{param}", test2)
+	})
+
+	return r
+}
+
+func networkRouter() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.WithValue("roles", []string{"admin", "agent", "agency", "manager"}))
+	r.Use(AppCheckMiddleware)
+	r.Use(CheckEntitlement)
+
+	r.Route("/v1/policy", func(r chi.Router) {
+		r.Get("/", test1)
+		r.Route("/{policyUid}", func(r chi.Router) {
+			r.Get("/", test2)
+			r.Get("/transactions", test2)
+		})
+	})
+
+	return r
+}
+
+func internalRouter() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.WithValue("roles", []string{"internal"}))
+	r.Use(AppCheckMiddleware)
+	r.Use(CheckEntitlement)
+
+	r.Get("/", test1)
+
+	return r
+}
+
+func adminRouter() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.WithValue("roles", []string{"admin"}))
+	r.Use(AppCheckMiddleware)
+	r.Use(CheckEntitlement)
+
+	r.Route("/v1/policy", func(r chi.Router) {
+		r.Get("/", test1)
+		r.Route("/{policyUid}/transactions", func(r chi.Router) {
+			r.Get("/", test2)
+			r.Get("/{transactionUid}", test2)
+		})
+	})
+
+	return r
 }
 
 func test1(w http.ResponseWriter, r *http.Request) {
@@ -63,6 +119,135 @@ func test1(w http.ResponseWriter, r *http.Request) {
 func test2(w http.ResponseWriter, r *http.Request) {
 	log.Println("test2 handler!")
 	log.Printf("Request: %s", r.RequestURI)
-	w.Header().Add("Content-type", "application/json")
+
+	policyUid := chi.URLParam(r, "policyUid")
+	log.Printf("PolicyUid: %s", policyUid)
+	transactionUid := chi.URLParam(r, "transactionUid")
+	log.Printf("TransactionUid: %s", transactionUid)
+
+	filterQuery := r.URL.Query().Get("filter")
+	log.Printf("FilterQuery: %s", filterQuery)
+
 	w.Write([]byte(`{"success":true}`))
+}
+
+// lib
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		options := cors.Options{
+			AllowedHeaders: []string{"*"},
+		}
+
+		if r.Method == http.MethodOptions || os.Getenv("env") == "local" {
+			options.AllowedOrigins = []string{"*"}
+		}
+
+		if r.Method == http.MethodOptions {
+			options.AllowedMethods = []string{"*"}
+			options.AllowCredentials = true
+			options.MaxAge = 3600
+			w.WriteHeader(http.StatusNoContent)
+		} else {
+			options.AllowCredentials = false
+		}
+
+		cors.Handler(options)(next).ServeHTTP(w, r)
+	})
+}
+
+func AppCheckMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		roles := ctx.Value("roles").([]string)
+		log.Printf("AppCheckMiddleware roles: %v", roles)
+
+		if os.Getenv("env") == "local" || slices.Contains(roles, "internal") {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		app, err := firebase.NewApp(ctx, &firebase.Config{ProjectID: os.Getenv("GOOGLE_PROJECT_ID")})
+		if err != nil {
+			log.Printf("error initializing app: %s", err.Error())
+			http.Error(w, "get out of here", http.StatusForbidden)
+			return
+		}
+
+		appCheck, err := app.AppCheck(ctx)
+		if err != nil {
+			log.Printf("error initializing app: %s\n", err.Error())
+			http.Error(w, "get out of here", http.StatusForbidden)
+			return
+		}
+
+		appCheckToken, ok := r.Header[http.CanonicalHeaderKey("X-Firebase-AppCheck")]
+		if !ok {
+			log.Printf("error missing token")
+			http.Error(w, "get out of here", http.StatusForbidden)
+			return
+		}
+
+		_, err = appCheck.VerifyToken(appCheckToken[0])
+		if err != nil {
+			log.Printf("error invalid token: %s", err.Error())
+			http.Error(w, "get out of here", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func VerifyUserIdToken(idToken string) (*auth.Token, error) {
+	ctx := context.Background()
+	app, err := firebase.NewApp(ctx, &firebase.Config{ProjectID: os.Getenv("GOOGLE_PROJECT_ID")})
+	if err != nil {
+		log.Fatalf("error creating app: %v\n", err)
+	}
+	client, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+	token, err := client.VerifyIDTokenAndCheckRevoked(ctx, idToken)
+	return token, err
+}
+
+func CheckEntitlement(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		roles := ctx.Value("roles").([]string)
+		log.Printf("CheckEntitlement roles: %v", roles)
+
+		if /*len(roles) == 0 || os.Getenv("env") == "local" ||*/ slices.Contains(roles, "internal") || slices.Contains(roles, "all") {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		idToken := strings.ReplaceAll(r.Header.Get("Authorization"), "Bearer ", "")
+		if idToken == "" {
+			log.Println("VerifyAuthorization: empty token")
+			http.Error(w, "who do you think you are", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := VerifyUserIdToken(idToken)
+		if err != nil {
+			log.Printf("VerifyAuthorization: verify id token error: %s", err.Error())
+			http.Error(w, "who do you think you are", http.StatusUnauthorized)
+			return
+		}
+
+		userRole := "customer"
+		if role, ok := token.Claims["role"].(string); ok {
+			userRole = role
+		}
+
+		if !slices.Contains(roles, userRole) {
+			log.Printf("VerifyAuthorization: userRole '%s' not allowed", userRole)
+			http.Error(w, "who do you think you are", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
