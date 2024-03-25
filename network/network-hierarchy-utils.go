4dcd0831f24ba7866dced9f67b3003a2605d21b4
Yousef Hammar <yousef.hammar@wopta.it>
2024-03-25 09:52:49 +0100

    feat: add network node hierarchy utils functions

---
+++ network/network-hierarchy-utils.go
@@ -0,0 +1,66 @@
+package network
+
+import (
+	"fmt"
+	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
+	"log"
+)
+
+func GetNodeAncestors(nodeUid string) ([]models.NetworkTreeElement, error) {
+	var (
+		err error
+	)
+
+	query := fmt.Sprintf("SELECT rootUid, ntr.parentUid, nodeUid, COALESCE(nnv.name, '') AS name, relativeLevel, "+
+		"ntr.creationDate  FROM `%s.%s` ntr INNER JOIN `%s.%s` nnv ON ntr.nodeUid = nnv.uid  "+
+		"WHERE nodeUid = @nodeUid ORDER BY relativeLevel", models.WoptaDataset,
+		models.NetworkTreeStructureTable, models.WoptaDataset, models.NetworkNodesView)
+	params := map[string]interface{}{
+		"nodeUid": nodeUid,
+	}
+
+	ancestors, err := lib.QueryParametrizedRowsBigQuery[models.NetworkTreeElement](query, params)
+	if err != nil {
+		log.Printf("error fetching ancestors from BigQuery for node %s: %s", nodeUid, err.Error())
+		return nil, err
+	}
+
+	return ancestors, nil
+}
+
+func GetNodeChildren(nodeUid string) ([]models.NetworkTreeElement, error) {
+	var (
+		err error
+	)
+
+	query := fmt.Sprintf("SELECT rootUid, ntr.parentUid, nodeUid, COALESCE(nnv.name, '') AS name, relativeLevel, "+
+		"ntr.creationDate  FROM `%s.%s` ntr INNER JOIN `%s.%s` nnv ON ntr.nodeUid = nnv.uid  "+
+		"WHERE rootUid = @rootUid ORDER BY relativeLevel", models.WoptaDataset,
+		models.NetworkTreeStructureTable, models.WoptaDataset, models.NetworkNodesView)
+	params := map[string]interface{}{
+		"rootUid": nodeUid,
+	}
+
+	children, err := lib.QueryParametrizedRowsBigQuery[models.NetworkTreeElement](query, params)
+	if err != nil {
+		log.Printf("error fetching children from BigQuery for node %s: %s", nodeUid, err.Error())
+		return nil, err
+	}
+
+	return children, nil
+}
+
+func IsParentOf(parentUid, childUid string) bool {
+	children, _ := GetNodeChildren(parentUid)
+	return len(lib.SliceFilter(children, func(child models.NetworkTreeElement) bool {
+		return child.NodeUid == childUid
+	})) == 1
+}
+
+func IsChildOf(parentUid, childUid string) bool {
+	ancestors, _ := GetNodeAncestors(childUid)
+	return len(lib.SliceFilter(ancestors, func(ancestor models.NetworkTreeElement) bool {
+		return ancestor.NodeUid == parentUid
+	})) == 1
+}
