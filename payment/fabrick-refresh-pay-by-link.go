82ef81563299618bae1759b7660ba9448a5a7dff
Yousef Hammar <yousef.hammar@wopta.it>
2024-04-05 12:20:08 +0200

    refactor: fabrick refresh pay by link adapted to new PaymentControllerV2 function

--- payment/fabrick-refresh-pay-by-link.go
+++ payment/fabrick-refresh-pay-by-link.go
@@ -2,13 +2,8 @@ package payment
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"time"
-
-	"github.com/google/uuid"
 	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/mail"
 	"github.com/wopta/goworkspace/models"
@@ -16,6 +11,9 @@ import (
 	plc "github.com/wopta/goworkspace/policy"
 	prd "github.com/wopta/goworkspace/product"
 	"github.com/wopta/goworkspace/transaction"
+	"io"
+	"log"
+	"net/http"
 )
 
 type FabrickRefreshPayByLinkRequest struct {
@@ -50,11 +48,38 @@ func FabrickRefreshPayByLinkFx(w http.ResponseWriter, r *http.Request) (string, 
 		return "", nil, fmt.Errorf("payment mode not supported")
 	}
 
-	err = fabrickRefreshPayByLink(&policy, origin)
+	unpaidTransactions := transaction.GetPolicyUnpaidTransactions(policy.Uid)
+	if len(unpaidTransactions) == 0 {
+		log.Printf("no unpaid transactions found for policy %s", policy.Uid)
+		return "", nil, errors.New("no unpaid transactions found")
+	}
+	log.Printf("found %02d unpaid transactions for policy %s", len(unpaidTransactions), policy.Uid)
+
+	product := prd.GetProductV2(policy.Name, policy.ProductVersion, policy.Channel, nil, nil)
+
+	payUrl, updatedTransactions, err := PaymentControllerV2(policy, *product, unpaidTransactions)
 	if err != nil {
-		log.Printf("error refreshing payment link: %s", err.Error())
+		log.Printf("error scheduling transactions on fabrick: %s", err.Error())
 		return "", nil, err
 	}
+
+	for _, tr := range updatedTransactions {
+		err = lib.SetFirestoreErr(models.TransactionsCollection, tr.Uid, tr)
+		if err != nil {
+			return "{}", nil, err
+		}
+
+		tr.BigQuerySave("")
+	}
+
+	policy.PayUrl = payUrl
+
+	err = lib.SetFirestoreErr(models.PolicyCollection, policy.Uid, policy)
+	if err != nil {
+		return "{}", nil, err
+	}
+
+	policy.BigquerySave("")
 
 	models.CreateAuditLog(r, string(body))
 
@@ -67,171 +92,6 @@ func FabrickRefreshPayByLinkFx(w http.ResponseWriter, r *http.Request) (string, 
 	log.Println("Handler end -------------------------------------------------")
 
 	return "{}", nil, nil
-}
-
-func fabrickRefreshPayByLink(policy *models.Policy, origin string) error {
-	var (
-		paymentMethods          []string
-		toBeDeletedTransactions []models.Transaction = make([]models.Transaction, 0)
-		fabrickResponse         *FabrickPaymentResponse
-		err                     error
-	)
-
-	if policy.Payment != models.FabrickPaymentProvider {
-		return fmt.Errorf("payment provider '%s' not supported", policy.Payment)
-	}
-
-	mgaProduct := prd.GetProductV2(policy.Name, policy.ProductVersion, models.MgaChannel, nil, nil)
-	product := prd.GetProductV2(policy.Name, policy.ProductVersion, policy.Channel, nil, nil)
-
-	// extract payment methods
-	paymentMethods = getPaymentMethods(*policy, product)
-
-	// check policy payment split
-	switch policy.PaymentMode {
-	// yearly/singleInstallment
-	case models.PaymentModeSingle:
-		fabrickResponse, toBeDeletedTransactions, err =
-			fabrickRefreshPayByLinkSingleRate(policy, origin, paymentMethods, mgaProduct)
-	case models.PaymentModeRecurrent:
-		fabrickResponse, toBeDeletedTransactions, err =
-			fabrickRefreshPayByLinkMultiRate(policy, origin, paymentMethods, mgaProduct)
-	default:
-		err = fmt.Errorf("unhandle payment split '%s'", policy.PaymentSplit)
-	}
-
-	if err != nil {
-		log.Printf("error refreshing pay by link: %s", err.Error())
-		return err
-	}
-
-	if fabrickResponse == nil || fabrickResponse.Payload.PaymentPageURL == nil {
-		return fmt.Errorf("fabrickResponse does not contain payment url: %v", fabrickResponse)
-	}
-
-	// update policy payurl and update date
-	policy.PayUrl = *fabrickResponse.Payload.PaymentPageURL
-	policy.Updated = time.Now().UTC()
-
-	// loop all to be deleted transactions
-	for _, tr := range toBeDeletedTransactions {
-		// delete on Fabrick
-		/*
-			err = fabrickExpireBill(tr.ProviderId)
-			if err != nil {
-				log.Printf("error with fabrick: %s", err.Error())
-				return err
-			}
-		*/
-		// delete
-		err = transaction.DeleteTransaction(&tr, origin, "Cancellata per ricreazione link di pagamento")
-		if err != nil {
-			log.Printf("error deleting transaction '%s': %s", tr.Uid, err.Error())
-			return err
-		}
-	}
-
-	// save policy firestore
-	firePolicy := lib.GetDatasetByEnv(origin, models.PolicyCollection)
-
-	log.Println("saving policy to firestore...")
-	err = lib.SetFirestoreErr(firePolicy, policy.Uid, policy)
-	if err != nil {
-		log.Printf("error saving policy to firestore: %s", err.Error())
-		return err
-	}
-
-	// save policy bigquery
-	log.Println("saving policy to bigquery...")
-	policy.BigquerySave(origin)
-
-	return nil
-}
-
-func fabrickRefreshPayByLinkSingleRate(
-	policy *models.Policy,
-	origin string,
-	paymentMethods []string,
-	mgaProduct *models.Product,
-) (fabrickResponse *FabrickPaymentResponse, toBeDeletedTransactions []models.Transaction, err error) {
-	// paid
-	if policy.IsPay {
-		// exit with error - cannot recreated paid annual policy
-		return nil, nil, fmt.Errorf(
-			"cannot refresh pay by link for policy with isPay '%T' and paymentSplit '%s'",
-			policy.IsPay,
-			policy.PaymentSplit,
-		)
-	}
-	// not paid
-	// // add old transaction to to be deleted array
-	toBeDeletedTransactions = transaction.GetPolicyTransactions(origin, policy.Uid)
-	// // create new one with current date - fabrickYearPay
-	payRes := FabrickYearPay(*policy, origin, paymentMethods, mgaProduct)
-
-	return &payRes, toBeDeletedTransactions, nil
-}
-
-func fabrickRefreshPayByLinkMultiRate(
-	policy *models.Policy,
-	origin string,
-	paymentMethods []string,
-	mgaProduct *models.Product,
-) (fabrickResponse *FabrickPaymentResponse, toBeDeletedTransactions []models.Transaction, err error) {
-	var refreshScheduleDates []time.Time = make([]time.Time, 0)
-
-	// get all transactions
-	allTransactions := transaction.GetPolicyTransactions(origin, policy.Uid)
-	// add all unpaid transactions to be deleted array
-	for _, tr := range allTransactions {
-		// TODO: check control flow
-		if (tr.IsPay && !tr.IsDelete) || (!tr.IsPay && tr.IsDelete) {
-			continue
-		}
-		scheduleDate, err := time.Parse(models.TimeDateOnly, tr.ScheduleDate)
-		if err != nil {
-			log.Printf("error parsing schedule date '%s' of transaction '%s': %s", tr.ScheduleDate, tr.Uid, err.Error())
-			return nil, nil, err
-		}
-		log.Printf("adding transcation with schedule date '%s' to be recreated", tr.ScheduleDate)
-		refreshScheduleDates = append(refreshScheduleDates, scheduleDate)
-		if !tr.IsDelete {
-			log.Printf("adding transcation with schedule date '%s' to be deleted", tr.ScheduleDate)
-			toBeDeletedTransactions = append(toBeDeletedTransactions, tr)
-		}
-	}
-	// create (totalRates - n paid transactions)
-	payRes := fabrickMultiRatePayment(*policy, origin, paymentMethods, mgaProduct, refreshScheduleDates)
-
-	return payRes, toBeDeletedTransactions, nil
-}
-
-func fabrickMultiRatePayment(
-	policy models.Policy,
-	origin string,
-	paymentMethods []string,
-	mgaProduct *models.Product,
-	rateScheduleDates []time.Time,
-) *FabrickPaymentResponse {
-	log.Printf("creating payments for %d transactions", len(rateScheduleDates))
-
-	customerId := uuid.New().String()
-	// first transaction schedule date == now
-
-	firstres := <-FabrickPayObj(policy, true, "", "", customerId, policy.PriceGrossMonthly, policy.PriceNettMonthly, origin, paymentMethods, mgaProduct, rateScheduleDates[0])
-	time.Sleep(100)
-
-	// we skip the first element since it is always created with todays date
-	for _, sd := range rateScheduleDates[1:] {
-		// other transactions same schedule date as before to respect policy.StartDate/EndDate
-		expireDate := sd.AddDate(10, 0, 0)
-
-		res := <-FabrickPayObj(policy, false, sd.Format(models.TimeDateOnly), expireDate.Format(models.TimeDateOnly), customerId, policy.PriceGrossMonthly, policy.PriceNettMonthly, origin, paymentMethods, mgaProduct, sd)
-		log.Printf("ScheduleDate: '%s' - response: %v", sd, res)
-		time.Sleep(100)
-	}
-
-	return &firstres
 }
 
 func sendPayByLinkEmail(policy models.Policy) error {
