fe1dce95be8c717eb3b9ee285916bb51d71790e4
Yousef Hammar <yousef.hammar@wopta.it>
2024-04-26 16:57:24 +0200

    refactor: getProductsByPolicyType now returns map[string]models.Product

--- renew/utils.go
+++ renew/utils.go
@@ -1,19 +1,21 @@
 package renew
 
 import (
+	"fmt"
 	"github.com/wopta/goworkspace/models"
 	"github.com/wopta/goworkspace/product"
 	"strings"
 )
 
-func getProductsByPolicyType(policyType string) []models.Product {
-	products := make([]models.Product, 0)
+func getProductsMapByPolicyType(policyType string) map[string]models.Product {
+	products := make(map[string]models.Product)
 
 	productsInfo := product.GetAllProductsByChannel(models.MgaChannel)
 	for _, pr := range productsInfo {
 		prd := product.GetProductV2(pr.Name, pr.Version, models.MgaChannel, nil, nil)
 		if strings.EqualFold(prd.PolicyType, policyType) {
-			products = append(products, *prd)
+			key := fmt.Sprintf("%s-%s", prd.Name, prd.Version)
+			products[key] = *prd
 		}
 	}
 
