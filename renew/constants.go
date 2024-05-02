3f209da22f969f293d5a78be865e558054a7a169
Diogo Carvalho <diogo.carvalho@wopta.it>
2024-05-02 14:55:22 +0200

    feat: renew constants

---
+++ renew/constants.go
@@ -0,0 +1,8 @@
+package renew
+
+const (
+	policyStatusRenewed         string = "Renewed"
+	policyStatusPaymentUnsolved string = "Unsolved" // INSOLUTO
+	renewPolicyCollection       string = "renewPolicyDraft"
+	renewTransactionCollection  string = "renewTransactionDraft"
+)
