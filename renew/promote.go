712d35436451124522adf32a204b7ace16dbf6ec
Diogo Carvalho <125029611+diogo-wopta@users.noreply.github.com>
2024-05-03 14:22:18 +0200

    TG-568 | Renew promete draft (#229)
    * wip: poc goroutine
    
    * wip: promote pipeline
    
    * feat: router logger config middleware
    
    * feat: policy bigquery parse method
    
    * feat: renew constants
    
    * fix: remove duplicated log config on renew router
    
    * feat: add renew promote utils
    
    * feat: renew promote
    
    * fix: use correct collections
    
    * feat: add technology mail
    
    * feat: send report mail
    
    * refactor: move collection constants to lib
    
    * refactor: extract report struct into models
    
    * feat: dry run flag
    
    * style: delete empty line
    
    * refactor: upload report file to bucket
    
    * fix: add policy updated and comment out unused function

---
+++ renew/promote.go
@@ -0,0 +1,209 @@
+package renew
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
+)
+
+type PromoteReq struct {
+	Date             string `json:"date"`
+	DryRun           *bool  `json:"dryRun"`
+	CollectionPrefix string `json:"collectionPrefix"`
+}
+
+func PromoteFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
+	var (
+		dryRun     bool = true
+		err        error
+		targetDate time.Time = time.Now().UTC()
+		request    PromoteReq
+		response   RenewResp
+	)
+
+	log.SetPrefix("[PromoteFx] ")
+	log.Println("Handler start -----------------------------------------------")
+
+	reqBytes := lib.ErrorByte(io.ReadAll(r.Body))
+	err = json.Unmarshal(reqBytes, &request)
+	if err != nil {
+		log.Printf("error unmarshalling request: %s", err.Error())
+		return "", nil, err
+	}
+
+	if request.Date != "" {
+		if targetDate, err = time.Parse(time.DateOnly, request.Date); err != nil {
+			log.Printf("error parsing request.Date: %s", err.Error())
+			return "", nil, err
+		}
+	}
+	if request.DryRun != nil {
+		dryRun = *request.DryRun
+	}
+
+	policies, err := getRenewingPolicies(targetDate)
+	if err != nil {
+		log.Printf("error querying bigquery: %s", err.Error())
+		return "", nil, err
+	}
+
+	saveFn := func(p models.Policy, trs []models.Transaction) error {
+		data := createSaveBatch(p, trs, request.CollectionPrefix)
+
+		if !dryRun {
+			return saveToDatabases(data)
+		}
+
+		return nil
+	}
+
+	response, err = Promote(policies, saveFn)
+	if err != nil {
+		return "", nil, err
+	}
+
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		return "", nil, err
+	}
+	if !dryRun {
+		filename := fmt.Sprintf("renew/promote/report-%s-%d.json", targetDate.Format(time.DateOnly), time.Now().Unix())
+		if _, err = lib.PutToGoogleStorage(os.Getenv("GOOGLE_STORAGE_BUCKET"), filename, responseJson); err != nil {
+			return "", nil, err
+		}
+	}
+
+	sendReportMail(targetDate, response, false)
+
+	return string(responseJson), response, err
+}
+
+func Promote(policies []models.Policy, saveFn func(models.Policy, []models.Transaction) error) (RenewResp, error) {
+	var (
+		err            error
+		promoteChannel chan RenewReport = make(chan RenewReport, len(policies))
+		wg             sync.WaitGroup
+		response       RenewResp
+	)
+
+	wg.Add(len(policies))
+	for _, p := range policies {
+		if p.IsPay {
+			go promotePolicyData(p, promoteChannel, &wg, saveFn)
+		} else {
+			go setPolicyNotPaid(p, promoteChannel, &wg, saveFn)
+		}
+	}
+
+	go func() {
+		wg.Wait()
+		close(promoteChannel)
+	}()
+
+	for res := range promoteChannel {
+		if res.Error != nil {
+			response.Failure = append(response.Failure, res)
+		} else {
+			response.Success = append(response.Success, res)
+		}
+	}
+
+	return response, err
+}
+
+func getRenewingPolicies(renewDate time.Time) ([]models.Policy, error) {
+	var (
+		query  bytes.Buffer
+		params = make(map[string]interface{})
+	)
+
+	params["month"] = int64(renewDate.Month())
+	params["day"] = int64(renewDate.Day())
+
+	query.WriteString(fmt.Sprintf("SELECT * FROM `%s.%s` WHERE "+
+		"EXTRACT(MONTH FROM startDate) = @month AND "+
+		"EXTRACT(DAY FROM startDate) = @day",
+		models.WoptaDataset,
+		lib.RenewPolicyCollection))
+
+	policies, err := lib.QueryParametrizedRowsBigQuery[models.Policy](query.String(), params)
+	if err != nil {
+		return nil, err
+	}
+
+	for index, policy := range policies {
+		var temp models.Policy
+		err := json.Unmarshal([]byte(policy.Data), &temp)
+		if err != nil {
+			return nil, err
+		}
+		policies[index] = temp
+	}
+
+	return policies, nil
+}
+
+func getTransactionsByPolicyAnnuity(policyUid string, annuity int) ([]models.Transaction, error) {
+	queries := []firestoreQuery{
+		{field: "policyUid", operator: "==", queryValue: policyUid},
+		{field: "annuity", operator: "==", queryValue: annuity},
+	}
+
+	return firestoreWhere[models.Transaction](lib.RenewTransactionCollection, queries)
+}
+
+func promotePolicyData(p models.Policy, promoteChannel chan<- RenewReport, wg *sync.WaitGroup, saveFn func(models.Policy, []models.Transaction) error) {
+	var (
+		err          error
+		transactions []models.Transaction
+	)
+
+	defer func() {
+		promoteChannel <- RenewReport{
+			Policy:       p,
+			Transactions: transactions,
+			Error:        err,
+		}
+
+		wg.Done()
+	}()
+
+	if transactions, err = getTransactionsByPolicyAnnuity(p.Uid, p.Annuity); err != nil {
+		return
+	}
+
+	p.Status = policyStatusRenewed
+	p.StatusHistory = append(p.StatusHistory, p.Status)
+	p.Updated = time.Now().UTC()
+
+	err = saveFn(p, transactions)
+}
+
+func setPolicyNotPaid(p models.Policy, promoteChannel chan<- RenewReport, wg *sync.WaitGroup, saveFn func(models.Policy, []models.Transaction) error) {
+	var (
+		err error
+	)
+
+	defer func() {
+		promoteChannel <- RenewReport{
+			Policy: p,
+			Error:  err,
+		}
+		wg.Done()
+	}()
+
+	p.Status = policyStatusPaymentUnsolved
+	p.StatusHistory = append(p.StatusHistory, p.Status)
+	p.Updated = time.Now().UTC()
+
+	err = saveFn(p, nil)
+}
---
+++ renew/utils.go
@@ -0,0 +1,173 @@
+package renew
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"cloud.google.com/go/firestore"
+	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/mail"
+	"github.com/wopta/goworkspace/models"
+	"google.golang.org/api/iterator"
+)
+
+func createSaveBatch(policy models.Policy, transactions []models.Transaction, collectionPrefix string) map[string]map[string]interface{} {
+	var (
+		polCollection string = collectionPrefix + lib.PolicyCollection
+		trsCollection string = collectionPrefix + lib.TransactionsCollection
+	)
+
+	policy.Updated = time.Now().UTC()
+	policy.BigQueryParse()
+	batch := map[string]map[string]interface{}{
+		polCollection: {
+			policy.Uid: policy,
+		},
+		trsCollection: {},
+	}
+
+	for idx, tr := range transactions {
+		tr.UpdateDate = time.Now().UTC()
+		tr.BigQueryParse()
+		batch[trsCollection][tr.Uid] = tr
+		transactions[idx] = tr
+	}
+
+	return batch
+}
+
+func saveToDatabases(data map[string]map[string]interface{}) error {
+	err := lib.SetBatchFirestoreErr(data)
+	if err != nil {
+		return err
+	}
+
+	for collection, values := range data {
+		dataToSave := make([]interface{}, 0)
+		for _, value := range values {
+			dataToSave = append(dataToSave, value)
+		}
+		err = lib.InsertRowsBigQuery(models.WoptaDataset, collection, dataToSave)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type firestoreQuery struct {
+	field      string
+	operator   string
+	queryValue interface{}
+}
+
+func firestoreWhere[T any](collection string, queries []firestoreQuery) (documents []T, err error) {
+	var (
+		client *firestore.Client
+		query  firestore.Query
+		ctx    context.Context = context.Background()
+	)
+
+	if client, err = firestore.NewClient(ctx, os.Getenv("GOOGLE_PROJECT_ID")); err != nil {
+		return nil, err
+	}
+
+	colRef := client.Collection(collection)
+
+	for idx, q := range queries {
+		if idx == 0 {
+			query = colRef.Where(q.field, q.operator, q.queryValue)
+			continue
+		}
+		query = query.Where(q.field, q.operator, q.queryValue)
+	}
+
+	docIterator := query.Documents(ctx)
+
+	for {
+		var (
+			snapshot *firestore.DocumentSnapshot
+			document T
+		)
+		if snapshot, err = docIterator.Next(); err != nil && err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		err = snapshot.DataTo(&document)
+		if err != nil {
+			return nil, err
+		}
+		documents = append(documents, document)
+	}
+
+	return documents, nil
+}
+
+// In case we need to get the data from BigQuery. Shouldn't be used now
+// because bigquery does not have all data
+// func getTransactionsByPolicyAnnuity(policyUid string, annuity int) ([]models.Transaction, error) {
+// 	var (
+// 		query  bytes.Buffer
+// 		params = make(map[string]interface{})
+// 	)
+
+// 	params["policyUid"] = policyUid
+// 	params["annuity"] = annuity
+
+// 	query.WriteString(fmt.Sprintf("SELECT * FROM `%s.%s` WHERE "+
+// 		"policyUid = '@policyUid' AND "+
+// 		"annuity = @annuity",
+// 		models.WoptaDataset,
+// 		lib.RenewTransactionCollection))
+
+// 	return lib.QueryParametrizedRowsBigQuery[models.Transaction](query.String(), params)
+// }
+
+func sendReportMail(date time.Time, report RenewResp, isDraft bool) {
+	var (
+		message string = fmt.Sprintf(`
+		<p>Quietanzamento del %s</p>
+		<p>Con successo: %d</p>
+		<p>Con errori: %d</p>
+		<p>Per report completo vedere file json in allegato.</p>
+		`, date.Format(time.DateOnly), len(report.Success), len(report.Failure))
+		title   string = "Report quietanzamento"
+		subject string = fmt.Sprintf("Report quietanzamento del %s", date.Format(time.DateOnly))
+	)
+
+	if isDraft {
+		title = "Report quietanzamento provvisorio"
+		subject = fmt.Sprintf("Report quietanzamento provvisorio del %s", date.Format(time.DateOnly))
+		message = fmt.Sprintf(`
+		<p>Quietanzamento provvisorio del %s</p>
+		<p>Con successo: %d</p>
+		<p>Con errori: %d</p>
+		<p>Per report completo vedere file json in allegato.</p>
+		`, date.Format(time.DateOnly), len(report.Success), len(report.Failure))
+	}
+
+	responseJson, _ := json.Marshal(report)
+
+	mail.SendMail(mail.MailRequest{
+		FromAddress:  mail.AddressAnna,
+		To:           []string{mail.AddressTechnology.Address},
+		Message:      message,
+		Title:        title,
+		Subject:      subject,
+		IsHtml:       true,
+		IsAttachment: true,
+		Attachments: &[]mail.Attachment{{
+			Name:        fmt.Sprintf("report-%s-%d.json", date.Format(time.DateOnly), time.Now().Unix()),
+			Byte:        base64.StdEncoding.EncodeToString(responseJson),
+			FileName:    fmt.Sprintf("report-%s-%d.json", date.Format(time.DateOnly), time.Now().Unix()),
+			ContentType: "application/json",
+		}},
+	})
+}
