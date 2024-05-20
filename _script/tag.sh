75d900e3c1d54fa4889e5bbaa40a58f5159490b5
luca <luca.barbieri@wopta.it>
2024-05-20 15:13:28 +0200

    fix update banck account

--- _script/tag.sh
+++ _script/tag.sh
@@ -1,14 +1,13 @@
 
-
 cd ..
-echo %1
-echo %2
+echo $1
+echo $2
 git add .
-git commit -m %2
+git commit -m $2
 git push work master 
 git push google master  
- git tag %1 -m %2
- git push work %1
- git push google %1
+git tag $1 -m $2
+git push work $1
+git push google $1
 
  
