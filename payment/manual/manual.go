7602544c8fb73c0c1e9b759b23aa55a02473c7ad
Diogo Carvalho <125029611+diogo-wopta@users.noreply.github.com>
2024-06-19 15:04:58 +0200

    fix: update policy when paying first transaction of annuity (#240)

--- payment/manual/manual.go
+++ payment/manual/manual.go
@@ -226,3 +226,8 @@ func manualPayment(transaction *models.Transaction, origin string, payload *Manu
 
 	return nil
 }
+
+// TODO: move elsewhere
+func areDatesEqual(d1, d2 time.Time) bool {
+	return d1.Truncate(time.Hour * 24).Equal(d2.Truncate(time.Hour * 24))
+}
