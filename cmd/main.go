fe1c6a7cea4f4dc6d0a0d4358314e402612d4677
Yousef Hammar <yousef.hammar@wopta.it>
2024-06-26 11:38:56 +0200

    refactor: changed log

--- cmd/main.go
+++ cmd/main.go
@@ -31,7 +31,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading env variables file")
 	}
 
 	// Use PORT environment variable, or default to 8080.
