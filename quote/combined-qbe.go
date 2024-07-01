8e93ed647bf56133bb604c57ab66c7e978ccc86c
luca <luca.barbieri@wopta.it>
2024-07-01 15:40:45 +0200

    test excel 2.0

--- quote/combined-qbe.go
+++ quote/combined-qbe.go
@@ -587,14 +587,6 @@ func getBuildingGuaranteCellsBySlug(guarante models.Guarante, colum int) []Cell 
 				Cell:  col[colum] + "45",
 				Value: guarante.Value.SumInsuredLimitOfIndemnity,
 			},
-			{
-				Cell:  "C46",
-				Value: guarante.Value.StartDateString,
-			},
-			{
-				Cell:  "E46",
-				Value: guarante.Value.Duration.Day,
-			},
 		}
 		if guarante.Value.StartDateString != "" {
 			cells = append(cells, []Cell{
