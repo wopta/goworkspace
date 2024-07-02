997115e45f1fc8a0aeb72a5f4bb377f179aadcda
luca <luca.barbieri@wopta.it>
2024-07-02 17:37:09 +0200

    add asset qbe fields

--- models/guarante.go
+++ models/guarante.go
@@ -12,6 +12,10 @@ type Guarante struct {
 	PolicyUid                  string                     `firestore:"-" json:"-"  bigquery:"policyUid"`
 	DailyAllowance             string                     `firestore:"dailyAllowance" json:"dailyAllowance,omitempty"  bigquery:"-"`
 	OrderAsset                 int                        `firestore:"orderAsset,omitempty" json:"orderAsset,omitempty"  bigquery:"-"`
+	DependsOr                  []string                   `firestore:"dependsOr,omitempty" json:"dependsOr,omitempty"  bigquery:"-"`
+	Depends                    []string                   `firestore:"depends,omitempty" json:"depends,omitempty"  bigquery:"-"`
+	Childrens                  []string                   `firestore:"dependOr,omitempty" json:"dependOr,omitempty"  bigquery:"-"`
+	SelectedEsclude            []string                   `firestore:"selectedEsclude,omitempty" json:"selectedEsclude,omitempty"  bigquery:"-"`
 	LegalDefence               string                     `firestore:"legalDefence" json:"legalDefence,omitempty"  bigquery:"legalDefence"`
 	Assistance                 string                     `firestore:"assistance" json:"assistance,omitempty"  bigquery:"-"`
 	Type                       string                     `firestore:"type,omitempty" json:"type,omitempty"  bigquery:"type"`
@@ -26,7 +30,7 @@ type Guarante struct {
 	CompanyName                string                     `firestore:"companyName,omitempty" json:"companyName,omitempty"  bigquery:"companyName"`
 	Group                      string                     `firestore:"group,omitempty" json:"group,omitempty"  bigquery:"group"`
 	Value                      *GuaranteValue             `firestore:"value,omitempty" json:"value,omitempty"  bigquery:"-"`
-	Config                     *GuaranteValue             `firestore:"config,omitempty" json:"config,omitempty"  bigquery:"-"`
+	Config                     *GuaranteConfig            `firestore:"config,omitempty" json:"config,omitempty"  bigquery:"-"`
 	ExtraValue                 string                     `firestore:"extraValue,omitempty" json:"extraValue,omitempty"  bigquery:"-"`
 	ValueDesc                  string                     `firestore:"valueDesc,omitempty" json:"valueDesc,omitempty"  bigquery:"-"`
 	Offer                      map[string]*GuaranteValue  `firestore:"offer,omitempty" json:"offer,omitempty"  bigquery:"-"`
@@ -85,12 +89,53 @@ type GuaranteValue struct {
 	MinimumGrossMonthly        float64             `firestore:"minimumGrossMonthly,omitempty" json:"minimumGrossMonthly,omitempty"`
 	MinimumGrossYearly         float64             `firestore:"minimumGrossYearly,omitempty" json:"minimumGrossYearly,omitempty"`
 }
-
+type GuaranteConfig struct {
+	StartDate                      *time.Time           `firestore:"startDate,omitempty" json:"startDate,omitempty" bigquery:"-"`
+	StartDateString                string               `firestore:"startDateString,omitempty" json:"startDateString,omitempty" bigquery:"-"`
+	TypeOfSumInsured               string               `firestore:"typeOfSumInsured,omitempty" json:"typeOfSumInsured,omitempty"`
+	Deductible                     string               `firestore:"deductible,omitempty" json:"deductible,omitempty"`
+	DeductibleValues               GuaranteFieldValue   `firestore:"deductibleValues,omitempty" json:"deductibleValues,omitempty"`
+	DeductibleType                 string               `firestore:"deductibleType,omitempty" json:"deductibleType,omitempty"`
+	DeductibleUnit                 string               `firestore:"deductibleUnit,omitempty" json:"deductibleUnit,omitempty"`
+	SumInsuredLimitOfIndemnity     float64              `json:"sumInsuredLimitOfIndemnity,omitempty" json:"sumInsuredLimitOfIndemnity,omitempty"`
+	SumInsured                     float64              `json:"sumInsured,omitempty" json:"sumInsured,omitempty"`
+	LimitOfIndemnity               float64              `json:"limitOfIndemnity,omitempty" json:"limitOfIndemnity,omitempty"`
+	SelfInsurance                  string               `firestore:"selfInsurance,omitempty" json:"selfInsurance,omitempty"`
+	SumInsuredValues               GuaranteFieldValue   `firestore:"sumInsuredValues,omitempty" json:"sumInsuredValues,omitempty"`
+	DeductibleDesc                 string               `firestore:"deductibleDesc,omitempty" json:"deductibleDesc,omitempty"`
+	SelfInsuranceValues            GuaranteFieldValue   `firestore:"selfInsuranceValues,omitempty" json:"selfInsuranceValues,omitempty"`
+	SelfInsuranceDesc              string               `firestore:"selfInsuranceDesc,omitempty" json:"selfInsuranceDesc,omitempty"`
+	Duration                       *Duration            `firestore:"duration,omitempty" json:"duration,omitempty"`
+	DurationValues                 *DurationFieldValue  `firestore:"durationValues,omitempty" json:"durationValues,omitempty"`
+	Tax                            float64              `firestore:"tax" json:"tax"`
+	Percentage                     float64              `firestore:"percentage" json:"percentage"`
+	PremiumNetYearly               float64              `firestore:"premiumNetYearly,omitempty" json:"premiumNetYearly"`
+	PremiumTaxAmountYearly         float64              `firestore:"premiumTaxAmountYearly,omitempty" json:"premiumTaxAmountYearly"`
+	PremiumGrossYearly             float64              `firestore:"premiumGrossYearly,omitempty" json:"premiumGrossYearly"`
+	PremiumNetMonthly              float64              `firestore:"premiumNetMonthly,omitempty" json:"premiumNetMonthly,omitempty"`
+	PremiumTaxAmountMonthly        float64              `firestore:"premiumTaxAmountMonthly,omitempty" json:"premiumTaxAmountMonthly"`
+	PremiumGrossMonthly            float64              `firestore:"premiumGrossMonthly,omitempty" json:"premiumGrossMonthly"`
+	MinimumGrossMonthly            float64              `firestore:"minimumGrossMonthly,omitempty" json:"minimumGrossMonthly,omitempty"`
+	MinimumGrossYearly             float64              `firestore:"minimumGrossYearly,omitempty" json:"minimumGrossYearly,omitempty"`
+	DurationValuesSlider           *GuaranteFieldConfig `firestore:"durationValuesSlider,omitempty" json:"durationValuesSlider,omitempty"`
+	SumInsuredValuesSliderdropdown *GuaranteFieldConfig `firestore:"sumInsuredValuesSliderdropdown,omitempty" json:"sumInsuredValuesSliderdropdown,omitempty"`
+	SumInsuredValuesSlider         *GuaranteFieldConfig `firestore:"sumInsuredValuesSlider,omitempty" json:"sumInsuredValuesSlider,omitempty"`
+	CalendarDatePicker             *GuaranteFieldConfig `firestore:"calendarDatePicker,omitempty" json:"calendarDatePicker,omitempty"`
+}
 type GuaranteFieldValue struct {
 	Min    float64   `firestore:"min,omitempty" json:"min,omitempty"`
 	Max    float64   `firestore:"max,omitempty" json:"max,omitempty"`
 	Step   float64   `firestore:"step,omitempty" json:"step,omitempty"`
 	Values []float64 `firestore:"values,omitempty" json:"values,omitempty"`
+}
+type GuaranteFieldConfig struct {
+	Min            float64   `firestore:"min,omitempty" json:"min,omitempty"`
+	Max            float64   `firestore:"max,omitempty" json:"max,omitempty"`
+	Step           float64   `firestore:"step,omitempty" json:"step,omitempty"`
+	Values         []float64 `firestore:"values,omitempty" json:"values,omitempty"`
+	Label          string    `firestore:"label,omitempty" json:"label,omitempty"`
+	Description    string    `firestore:"description,omitempty" json:"description,omitempty"`
+	TextFieldLabel string    `firestore:"textFieldLabel,omitempty" json:"textFieldLabel,omitempty"`
 }
 
 type DurationFieldValue struct {
