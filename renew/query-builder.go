10b118a4168ffc73593827b57fefbaf82cc9f87a
Yousef Hammar <yousef.hammar@wopta.it>
2024-07-04 17:43:16 +0200

    fix: solved bug in parameters hierarchy definition

--- renew/query-builder.go
+++ renew/query-builder.go
@@ -2,7 +2,6 @@ package renew
 
 import (
 	"fmt"
-	"log"
 	"strings"
 
 	"github.com/wopta/goworkspace/lib"
@@ -10,14 +9,35 @@ import (
 
 var (
 	paramsHierarchy = []map[string][]string{
-		{
-			"codeCompany":       []string{"codeCompany"},
-			"insuredFiscalCode": []string{"insuredFiscalCode"},
-		},
+		{"codeCompany": []string{"codeCompany"}},
+
+		{"insuredFiscalCode": []string{"insuredFiscalCode"}},
+
+		{"contractorName": []string{"contractorName", "contractorSurname"}},
+		{"contractorSurname": []string{"contractorName", "contractorSurname"}},
+
+		{"startDateFrom": []string{"startDateFrom", "startDateTo", "company", "product", "producerCode", "reserved"}},
+		{"startDateTo": []string{"startDateFrom", "startDateTo", "company", "product", "producerCode", "reserved"}},
+		{"company": []string{"startDateFrom", "startDateTo", "company", "product", "producerCode", "reserved"}},
+		{"product": []string{"startDateFrom", "startDateTo", "company", "product", "producerCode", "reserved"}},
+		{"producerCode": []string{"startDateFrom", "startDateTo", "company", "product", "producerCode", "reserved"}},
+		{"reserved": []string{"startDateFrom", "startDateTo", "company", "product", "producerCode", "reserved"}},
 	}
+
 	paramsQuery = map[string]string{
-		"codeCompany":       "(JSON_VALUE(p.data, '$.codeCompany') = \"%s\")",
+		"codeCompany": "(JSON_VALUE(p.data, '$.codeCompany') = \"%s\")",
+
 		"insuredFiscalCode": "(JSON_VALUE(p.data, '$.assets[0].person.fiscalCode') = \"%s\")",
+
+		"contractorName":    "(REGEXP_CONTAINS(LOWER(JSON_VALUE(p.data, '$.contractor.name')), LOWER(%s))",
+		"contractorSurname": "(REGEXP_CONTAINS(LOWER(JSON_VALUE(p.data, '$.contractor.surname')), LOWER(%s))",
+
+		"startDateFrom": "(JSON_VALUE(p.data, '$.startDate') >= \"%s\")",
+		"startDateTo":   "(JSON_VALUE(p.data, '$.startDate') <= \"%s\")",
+		"company":       "(JSON_VALUE(p.data, '$.company') = LOWER(\"%s\"))",
+		"product":       "(JSON_VALUE(p.data, '$.product') = LOWER(\"%s\"))",
+		"producerCode":  "(JSON_VALUE(p.data, '$.producerCode') = \"%s\")",
+		"reserved":      "(JSON_VALUE(p.data, '$.reserved') = %s)",
 	}
 )
 
@@ -31,29 +51,43 @@ func NewQueryBuilder() BigQueryQueryBuilder {
 	return BigQueryQueryBuilder{}
 }
 
-func (qb *BigQueryQueryBuilder) BuildQuery(params map[string]string) string {
+func (qb *BigQueryQueryBuilder) getAllowedParams(params map[string]string) []string {
 	paramsKeys := lib.GetMapKeys(params)
-	allowedParams := make([]string, 0)
-
 	for _, value := range paramsHierarchy {
 		for k, v := range value {
 			if lib.SliceContains(paramsKeys, k) {
-				allowedParams = v
-				break
+				return v
 			}
 		}
 	}
+	return nil
+}
 
-	log.Println(allowedParams)
-
+func (qb *BigQueryQueryBuilder) filterParams(params map[string]string, allowedParams []string) map[string]string {
+	paramsKeys := lib.GetMapKeys(params)
 	for _, key := range paramsKeys {
 		if !lib.SliceContains(allowedParams, key) {
 			delete(params, key)
 		}
 	}
+	return params
+}
 
-	queries := make([]string, 0)
-	for k, v := range params {
+func (qb *BigQueryQueryBuilder) BuildQuery(params map[string]string) string {
+	var (
+		queries       = make([]string, 0)
+		allowedParams = make([]string, 0)
+	)
+
+	allowedParams = qb.getAllowedParams(params)
+	if allowedParams == nil {
+		// TODO: handle error
+		return ""
+	}
+
+	filteredParams := qb.filterParams(params, allowedParams)
+
+	for k, v := range filteredParams {
 		queries = append(queries, fmt.Sprintf(paramsQuery[k], v))
 	}
 
--- renew/query-builder_test.go
+++ renew/query-builder_test.go
@@ -25,4 +25,65 @@ func TestQueryBuilder(t *testing.T) {
 		}
 	})
 
+	t.Run("fiscalCode overcome third-level parameters", func(t *testing.T) {
+		params := map[string]string{
+			"insuredFiscalCode": "LLLRRR85E05R94Z330F",
+			"producerCode":      "a1b2c3d4",
+		}
+
+		want := `(JSON_VALUE(p.data, '$.assets[0].person.fiscalCode') = "LLLRRR85E05R94Z330F")`
+
+		got := qb.BuildQuery(params)
+
+		if !strings.EqualFold(got, want) {
+			t.Fatalf("expected: %s, got: %s", want, got)
+		}
+	})
+
+	t.Run("combine third-level parameters", func(t *testing.T) {
+		params := map[string]string{
+			"producerCode":  "a1b2c3d4",
+			"startDateFrom": "2024-07-04",
+			"startDateTo":   "2024-07-14",
+		}
+
+		want := `(JSON_VALUE(p.data, '$.producerCode') = "a1b2c3d4") AND (JSON_VALUE(p.data, '$.startDate') >= "2024-07-04") AND (JSON_VALUE(p.data, '$.startDate') <= "2024-07-14")`
+
+		got := qb.BuildQuery(params)
+
+		if !strings.EqualFold(got, want) {
+			t.Fatalf("expected: %s, got: %s", want, got)
+		}
+	})
+
+	t.Run("combine parameters from differents level", func(t *testing.T) {
+		params := map[string]string{
+			"producerCode":  "a1b2c3d4",
+			"startDateFrom": "2024-07-04",
+			"startDateTo":   "2024-07-14",
+			"codeCompany":   "100100",
+		}
+
+		want := `(JSON_VALUE(p.data, '$.codeCompany') = "100100")`
+
+		got := qb.BuildQuery(params)
+
+		if !strings.EqualFold(got, want) {
+			t.Fatalf("expected: %s, got: %s", want, got)
+		}
+	})
+
+	t.Run("not supported parameter", func(t *testing.T) {
+		params := map[string]string{
+			"test": "test",
+		}
+
+		want := ""
+		got := qb.BuildQuery(params)
+
+		if !strings.EqualFold(got, want) {
+			t.Fatalf("expected: %s, got: %s", want, got)
+		}
+	})
+
 }
