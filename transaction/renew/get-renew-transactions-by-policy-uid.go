91e187671bf8b68ed79ff49b34b92d49f371d5e9
Diogo Carvalho <diogo.carvalho@wopta.it>
2024-07-10 16:50:14 +0200

    wip: get renew transactions by policy uid

---
+++ transaction/renew/get-renew-transactions-by-policy-uid.go
@@ -0,0 +1,120 @@
+package renew
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"slices"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/models"
+	plc "github.com/wopta/goworkspace/policy"
+	"google.golang.org/api/iterator"
+)
+
+var errUnauthorized = errors.New("unauthorized")
+
+type response struct {
+	Transactions []models.Transaction `json:"transactions"`
+}
+
+func GetRenewTransactionsByPolicyUidFx(w http.ResponseWriter, r *http.Request) (string, any, error) {
+	var (
+		err    error
+		policy models.Policy
+		resp   response
+	)
+
+	log.SetPrefix("[GetRenewTransactionsByPolicyUidFx] ")
+	defer func() {
+		if err != nil {
+			log.Printf("error: %s", err)
+		}
+		log.Println("Handler end ---------------------------------------------")
+		log.SetPrefix("")
+	}()
+	log.Println("Handler start -----------------------------------------------")
+
+	idToken := r.Header.Get("Authorization")
+	authToken, err := lib.GetAuthTokenFromIdToken(idToken)
+	if err != nil {
+		return "", nil, err
+	}
+
+	userUid := authToken.UserID
+	policyUid := chi.URLParam(r, "policyUid")
+
+	// TODO: change into renewpolicy
+	if policy, err = plc.GetPolicy(policyUid, ""); err != nil {
+		log.Printf("error fetching policy '%s'", policyUid)
+		return "", nil, err
+	}
+
+	if !plc.CanBeAccessedBy(authToken.Role, policy.ProducerUid, authToken.UserID) {
+		log.Printf("policy %s is not included in %s %s portfolio", policyUid, authToken.Role, userUid)
+		return "", nil, errUnauthorized
+	}
+
+	transactions, err := getRenewTransactionsByPolicyUid(policyUid, policy.Annuity)
+	if err != nil {
+		return "", nil, err
+	}
+
+	resp.Transactions = transactions
+
+	bytes, err := json.Marshal(resp)
+
+	return string(bytes), nil, err
+}
+
+func getRenewTransactionsByPolicyUid(policyUid string, annuity int) ([]models.Transaction, error) {
+	result := make([]models.Transaction, 0)
+	q := lib.Firequeries{
+		Queries: []lib.Firequery{
+			{
+				Field:      "policyUid",
+				Operator:   "==",
+				QueryValue: policyUid,
+			},
+			{
+				Field:      "annuity",
+				Operator:   "==",
+				QueryValue: annuity,
+			},
+			{
+				Field:      "isDelete",
+				Operator:   "==",
+				QueryValue: false,
+			},
+		},
+	}
+	docsnap, err := q.FirestoreWherefields(lib.RenewTransactionCollection)
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		d, err := docsnap.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		var value models.Transaction
+		if err = d.DataTo(&value); err != nil {
+			return nil, err
+		}
+		result = append(result, value)
+	}
+
+	slices.SortFunc(result, sortByEffectiveDate)
+
+	return result, nil
+}
+
+func sortByEffectiveDate(a, b models.Transaction) int {
+	return a.EffectiveDate.Compare(b.EffectiveDate)
+}
