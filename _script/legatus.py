b384489a06d1b0b5393082890da25383ad010ba3
Yousef Hammar <yousef.hammar@wopta.it>
2024-09-13 09:55:31 +0200

    feat: print script name ASCII

---
+++ _script/legatus.py
@@ -0,0 +1,16 @@
+#    LEGATUS prenderà in input una lista di nomi di function (es. broker, callback). Confrontare le versioni presenti
+#    in DEV e PROD e aggiornare quelle di produzione.
+
+#    Se il nome della function contiene anche la versione, allora lo script dovrà aggiornare produzione con quella
+#    versione specifica.
+
+print(r"""
+.____                                __                  
+|    |      ____     ____  _____   _/  |_  __ __   ______
+|    |    _/ __ \   / ___\ \__  \  \   __\|  |  \ /  ___/
+|    |___ \  ___/  / /_/  > / __ \_ |  |  |  |  / \___ \ 
+|_______ \ \___  > \___  / (____  / |__|  |____/ /____  >
+        \/     \/ /_____/       \/                    \/ 
+"""
+)
+
