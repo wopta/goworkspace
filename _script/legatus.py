827be1bec5d3091fe66adc98dffde82fcb803a94
Yousef Hammar <yousef.hammar@wopta.it>
2024-09-13 15:04:31 +0200

    feat: switch to GitPython library

--- _script/legatus.py
+++ _script/legatus.py
@@ -1,6 +1,8 @@
 #    LEGATUS prenderà in input una lista di nomi di function (es. broker, callback). Confrontare le versioni presenti
 #    in DEV e PROD e aggiornare quelle di produzione.
-import subprocess
+import os
+
+from git import Repo
 
 #    Se il nome della function contiene anche la versione, allora lo script dovrà aggiornare produzione con quella
 #    versione specifica.
@@ -41,27 +43,34 @@ def main():
 """
           )
 
+    repo = Repo(os.curdir)
+    git = repo.git
+
     for function_name in changed_functions:
         if function_name not in updatable_functions:
             print(f"ERROR: {function_name} unknown function...skipping")
             continue
 
-        command = "git tag --list --sort=-taggerdate '{}/*.dev' | head -1".format(
-            function_name)
-        last_tag = subprocess.check_output(command, shell=True, text=True)
+        tags = git.tag('--list', '--sort=-taggerdate', f'{function_name}/*.dev').splitlines()
+        dev_tag = tags[0]
+
+        #command = "git tag --list --sort=-taggerdate '{}/*.dev' | head -1".format(
+        #    function_name)
+        #last_tag = subprocess.check_output(command, shell=True, text=True)
 
         # TODO: implement checkout to last_tag
-        command = f"git checkout --quiet {last_tag}"
-        subprocess.check_output(command, shell=True, text=True)
+        git.checkout(dev_tag)
+        #command = f"git checkout --quiet {last_tag}"
+        # subprocess.check_output(command, shell=True, text=True)
 
         # TODO: implement production tag creation
-        production_tag = last_tag.replace(DEV, PROD)
-        if dry_run:
-            print(production_tag)
-            continue
+        #production_tag = last_tag.replace(DEV, PROD)
+        #if dry_run:
+        #    print(production_tag)
+        #    continue
 
-        command = f"git tag -a {production_tag} -m \"Release Sprint {sprint_number}\""
-        subprocess.check_output(command, shell=True, text=True)
+        #command = f"git tag -a {production_tag} -m \"Release Sprint {sprint_number}\""
+        #subprocess.check_output(command, shell=True, text=True)
 
 
         # TODO: implement push to GitHub and Cloud Repository, if DryRun = false
