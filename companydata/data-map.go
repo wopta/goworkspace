b3255e58497e8bd32a815e2551f4a06ccb1cb9e9
luca <luca.barbieri@wopta.it>
2024-09-13 09:26:25 +0200

    test emit fx product

--- companydata/data-map.go
+++ companydata/data-map.go
@@ -1,14 +1,52 @@
 package companydata
 
-type fn func(interface{})interface{}
-func GetMapFx(name string,value interface{})interface{}{
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
 
-res:= map[string]fn{
-    "test": Test,
-   
-}
+type fn func(interface{}) interface{}
+
+func GetMapFx(name string, value interface{}) interface{} {
+
+	res := map[string]fn{
+		"test": Test,
+		"formatDateDDMMYYYYSlash": formatDateDDMMYYYYSlash,
+		"formatBithdateDDMMYYYYSlash": formatBithdateDDMMYYYYSlash,
+	}
 	return res[name](value)
 }
-func Test(s interface{})interface{} {
+func Test(s interface{}) interface{} {
 	return ""
+}
+func formatDateDDMMYYYYSlash(s interface{}) interface{} {
+
+	return s.(time.Time).Format("02/01/2006")
+}
+func formatBithdateDDMMYYYYSlash(d interface{}) interface{}{
+	var res string
+	if d.(string) != "" {
+		splitD := strings.Split(d.(string), "-")
+		split2 := strings.Split(splitD[2], "T")
+		day, _ := strconv.Atoi(split2[0])
+		month, _ := strconv.Atoi(splitD[1])
+		res = fmt.Sprintf("%02d", day) + "/" + fmt.Sprintf("%02d", month)+ "/" + splitD[0]
+	}
+
+	return res
+
+}
+func formatSplitPaymentNumber(s interface{}) interface{} {
+	var res string
+	if s=="monthly"{
+		res="1"
+
+	}
+	if s=="yearly"{
+		res="12"
+
+	}
+	return res
 }
