2ec447582ea982a6416acd7a70b146139a427d85
Yousef Hammar <yousef.hammar@wopta.it>
2024-10-22 18:07:09 +0200

    feat: added check on policy producer

--- broker/get-policy.go
+++ broker/get-policy.go
@@ -2,11 +2,13 @@ package broker
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/network"
 	plc "github.com/wopta/goworkspace/policy"
 )
 
@@ -19,9 +21,32 @@ func GetPolicyFx(w http.ResponseWriter, r *http.Request) (string, interface{}, e
 	policyUid := chi.URLParam(r, "uid")
 	log.Printf("policyUid: %s", policyUid)
 
-	firePolicy := lib.GetDatasetByEnv(r.Header.Get("Origin"), lib.PolicyCollection)
+	idToken := r.Header.Get("Authorization")
+	authToken, err := lib.GetAuthTokenFromIdToken(idToken)
+	if err != nil {
+		log.Printf("error fetching authToken: %s", err.Error())
+		return "", nil, err
+	}
 
-	policy, _ := plc.GetPolicy(policyUid, firePolicy)
+	policy, err := plc.GetPolicy(policyUid, lib.PolicyCollection)
+	if err != nil {
+		log.Printf("error fetching policy: %s", err.Error())
+		return "", nil, err
+	}
+
+	switch authToken.Role {
+	case lib.UserRoleCustomer:
+		if policy.Contractor.Uid != authToken.UserID {
+			log.Printf("error fetching policy: invalid user id: %s", authToken.UserID)
+			return "", nil, errors.New("invalid user id")
+		}
+	case lib.UserRoleAreaManager, lib.UserRoleAgency, lib.UserRoleAgent:
+		if policy.ProducerUid != authToken.UserID && !network.IsChildOf(authToken.UserID, policy.ProducerUid) {
+			log.Printf("error fetching policy: invalid producer uid: %s", authToken.UserID)
+			return "", nil, errors.New("invalid producer uid")
+		}
+	}
+
 	res, err := json.Marshal(policy)
 
 	log.Println("Handler end -------------------------------------------------")
