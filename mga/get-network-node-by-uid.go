8b2ae54ac8aca619b0dfd5369164353c96f2e3ba
Yousef Hammar <yousef.hammar@wopta.it>
2024-11-12 11:20:13 +0100

    feat: GetNetworkNode API changes for network

--- mga/get-network-node-by-uid.go
+++ mga/get-network-node-by-uid.go
@@ -1,6 +1,7 @@
 package mga
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
@@ -10,13 +11,33 @@ import (
 )
 
 func GetNetworkNodeByUidFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error) {
-	log.SetPrefix("[GetNetworkNodeByUidFx] ")
-	defer log.SetPrefix("")
+	var (
+		err error
+	)
 
+	defer func() {
+		if err != nil {
+			log.Printf("error: %+v", err.Error())
+		}
+		log.SetPrefix("")
+		log.Println("Handler end -------------------------------------------------")
+	}()
+
+	log.SetPrefix("[GetNetworkNodeByUidFx] ")
 	log.Println("Handler start -----------------------------------------------")
+
+	idToken := r.Header.Get("Authorization")
+	authToken, err := lib.GetAuthTokenFromIdToken(idToken)
+	if err != nil {
+		return "", nil, err
+	}
 
 	nodeUid := chi.URLParam(r, "uid")
 	log.Printf("Uid %s", nodeUid)
+
+	if authToken.IsNetworkNode && !network.IsParentOf(authToken.UserID, nodeUid) {
+		return "", nil, errors.New("cannot access this node")
+	}
 
 	networkNode := network.GetNetworkNodeByUid(nodeUid)
 
@@ -24,9 +45,7 @@ func GetNetworkNodeByUidFx(w http.ResponseWriter, r *http.Request) (string, inte
 	networkNode.JwtConfig = lib.JwtConfig{}
 	networkNode.CallbackConfig = nil
 
-	jsonOut, err := networkNode.Marshal()
+	rawResp, err := networkNode.Marshal()
 
-	log.Println("Handler end -------------------------------------------------")
-
-	return string(jsonOut), networkNode, err
+	return string(rawResp), networkNode, err
 }
