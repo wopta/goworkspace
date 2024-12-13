8b63b2af06fdfef20a9ac3f6bc8f4e10b499866e
Yousef Hammar <yousef.hammar@wopta.it>
2024-12-13 11:07:48 +0100

    feat: added tableCell struct

---
+++ document/internal/domain/table-cell.go
@@ -0,0 +1,12 @@
+package domain
+
+type TableCell struct {
+	Text      string
+	Height    float64
+	Width     float64
+	TextBold  bool
+	Fill      bool
+	FillColor Color
+	Align     string
+	Border    string
+}
