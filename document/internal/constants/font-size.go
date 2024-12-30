df5f64b6141d68952a9d884cad14eef8ea633ca2
Yousef Hammar <yousef.hammar@wopta.it>
2024-12-30 16:13:56 +0100

    feat: added MediumFontSize

--- document/internal/constants/font-size.go
+++ document/internal/constants/font-size.go
@@ -4,6 +4,7 @@ import "github.com/wopta/goworkspace/document/internal/domain"
 
 const (
 	SmallFontSize   domain.FontSize = 6
+	MediumFontSize  domain.FontSize = 8
 	RegularFontSize domain.FontSize = 9
 	LargeFontSize   domain.FontSize = 10
 )
