4e40da3c184f91ccbbd2de477cbbde165b657622
Yousef Hammar <yousef.hammar@wopta.it>
2024-12-31 16:02:46 +0100

    feat: added wopta header to base generato

--- document/pkg/contract/generator.go
+++ document/pkg/contract/generator.go
@@ -1,8 +1,23 @@
 package contract
 
-import "github.com/wopta/goworkspace/document/internal/engine"
+import (
+	"github.com/wopta/goworkspace/document/internal/constants"
+	"github.com/wopta/goworkspace/document/internal/engine"
+	"github.com/wopta/goworkspace/lib"
+)
 
 type baseGenerator struct {
 	engine     *engine.Fpdf
 	isProposal bool
+}
+
+func (bg *baseGenerator) woptaHeader() {
+	bg.engine.SetHeader(func() {
+		bg.engine.InsertImage(lib.GetAssetPathByEnvV2()+"logo_qbe.png", 10, 6, 0, 10)
+		bg.engine.NewLine(10)
+
+		if bg.isProposal {
+			bg.engine.DrawWatermark(constants.Proposal)
+		}
+	})
 }
