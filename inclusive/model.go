dcd4c1b669e31101c8684610c1a24d47bdbba8b6
luca <luca.barbieri@wopta.it>
2025-01-02 14:57:01 +0100

    test emit fx product

---
+++ inclusive/model.go
@@ -0,0 +1,61 @@
+package inclusive
+
+import (
+	"encoding/json"
+	"errors"
+	"time"
+
+	"cloud.google.com/go/civil"
+)
+
+type BankAccountMovement struct {
+	Tenant         string         `firestore:"-" json:"tenant" bigquery:"tenant"`
+	Uid            string         `firestore:"-" json:"-" bigquery:"uid"`
+	Status         string         `firestore:"-" json:"-" bigquery:"status"`
+	Name           string         `firestore:"-" json:"name,omitempty" bigquery:"name"`             //h-Nome
+	Surname        string         `firestore:"-" json:"surname,omitempty" bigquery:"surname"`       //Cognome
+	FiscalCode     string         `firestore:"-" json:"fiscalCode,omitempty" bigquery:"fiscalCode"` //Codice fiscale
+	HypeId         string         `firestore:"-" json:"hypeId,omitempty" bigquery:"hypeId"`         //h-Ultime 3 / 5 cifre conto corrente
+	StartDate      time.Time      `bigquery:"-" firestore:"-" json:"startDate,omitempty"`           //h-Data ingresso (inizio validità copertura)
+	EndDate        time.Time      `bigquery:"-" firestore:"-" json:"endDate,omitempty"`
+	BigStartDate   civil.DateTime `bigquery:"startDate" firestore:"-" json:"-"` //Data ingresso (inizio validità copertura)
+	BigEndDate     civil.DateTime `bigquery:"endDate" firestore:"-" json:"-"`
+	Address        string         `firestore:"-" json:"address,omitempty" bigquery:"address"`
+	City           string         `firestore:"-" json:"city,omitempty" bigquery:"city"`
+	CityCode       string         `firestore:"-" json:"cityCode,omitempty" bigquery:"cityCode"`             //Data uscita ()
+	MovementType   string         `firestore:"-" json:"movementType,omitempty" bigquery:"movementType"`     //Movimento (ingresso o uscita)
+	PolicyNumber   string         `firestore:"-" json:"policyNumber,omitempty" bigquery:"policyNumber"`     //NUMERO POLIZZA
+	PolicyType     string         `firestore:"-" json:"policyType,omitempty" bigquery:"policyType"`         //TIPOLOGIA POLIZZA
+	GuaranteesCode string         `firestore:"-" json:"guaranteesCode,omitempty" bigquery:"guaranteesCode"` //CODICE CONFIGURAZIONE pacchetti
+	AssetType      string         `firestore:"-" json:"assetType,omitempty" bigquery:"assetType"`           //TIPO OGGETTO ASSICURATO
+	Customer       string         `firestore:"-" json:"-" bigquery:"customer"`
+	Company        string         `firestore:"-" json:"-" bigquery:"company"`   //Hype
+	PolicyUid      string         `firestore:"-" json:"-" bigquery:"policyUid"` //NUMERO POLIZZA
+	CustomerId     string         `firestore:"-" json:"-" bigquery:"customerId"`
+	BanckAccountId string         `firestore:"-" json:"-" bigquery:"banckAccountId"`
+	PolicyName     string         `firestore:"-" json:"-" bigquery:"policyName"`
+}
+type ErrorResponse struct {
+	Code    int    `firestore:"-" json:"code,omitempty" bigquery:"name"`
+	Type    string `firestore:"-" json:"type,omitempty" bigquery:"surname"`
+	Message string `firestore:"-" json:"message,omitempty" bigquery:"fiscalCode"`
+}
+
+func GetErrorJson(code int, typeEr string, message string) error {
+	var (
+		e     error
+		eResp ErrorResponse
+		b     []byte
+	)
+	eResp = ErrorResponse{Code: code, Type: typeEr, Message: message}
+	b, e = json.Marshal(eResp)
+	e = errors.New(string(b))
+	return e
+}
+
+type CountResponseModel struct {
+	Total     int `json:"total"`
+	Insert    int `json:"insert"`
+	Delete    int `json:"delete"`
+	Suspended int `json:"suspended"`
+}
---
+++ inclusive/scalapay.go
@@ -0,0 +1,136 @@
+package inclusive
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"cloud.google.com/go/civil"
+	"github.com/google/uuid"
+	lib "github.com/wopta/goworkspace/lib"
+)
+
+// TO DO security,payload,error,fasature
+func BankAccountScalapayFx(resp http.ResponseWriter, r *http.Request) (string, interface{}, error) {
+
+	var (
+		e   error
+		obj BankAccountMovement
+	)
+	e = CheckScalapayApikey(r)
+	if e != nil {
+		return "", nil, e
+	}
+	obj, e = CheckScalapayData(r)
+	if e != nil {
+		return "", nil, e
+	}
+	obj = SetScalapayData(obj)
+	e = lib.InsertRowsBigQuery("wopta", dataMovement, obj)
+	if obj.MovementType == "insert" {
+
+		e = lib.InsertRowsBigQuery("wopta", dataBanckAccount, obj)
+	}
+	/*
+		layout := "2006-01-02"
+		if obj.MovementType == "delete" || obj.MovementType == "suspended" {
+			e = lib.UpdateRowBigQuery("wopta", dataBanckAccount, map[string]string{
+				"status":  obj.Status,
+				"endDate": obj.EndDate.Format(layout) + " 00:00:00",
+			}, "fiscalCode='"+obj.FiscalCode+"' and guaranteesCode='"+obj.GuaranteesCode+"'")
+
+		}
+	*/
+	return `{"woptaUid":"` + obj.Uid + `"}`, nil, e
+}
+func CheckScalapayApikey(r *http.Request) error {
+	apikey := os.Getenv("HYPE_APIKEY")
+	apikeyReq := r.Header.Get("api_key")
+	if apikey != apikeyReq {
+		return GetErrorJson(401, "Unauthorized", "")
+	}
+	return nil
+}
+func CheckScalapayData(r *http.Request) (BankAccountMovement, error) {
+	req := lib.ErrorByte(io.ReadAll(r.Body))
+	log.Println(r.Header)
+	log.Println(string(req))
+	var obj BankAccountMovement
+	defer r.Body.Close()
+	json.Unmarshal([]byte(req), &obj)
+	if obj.Name == "" {
+		return obj, GetErrorJson(400, "Bad request", "field name miss")
+	}
+	if obj.Tenant == "" {
+		return obj, GetErrorJson(400, "Bad request", "field tenant miss")
+	}
+	if obj.Surname == "" {
+		return obj, GetErrorJson(400, "Bad request", "field Surname miss")
+	}
+	if obj.MovementType != "insert" && obj.MovementType != "delete" && obj.MovementType != "suspended" {
+		return obj, GetErrorJson(400, "Bad request", "field MovementType out of enum")
+	}
+	if obj.MovementType == "insert" {
+		if obj.StartDate.IsZero() {
+			return obj, GetErrorJson(400, "Bad request", "field StartDate miss")
+		}
+	}
+	if obj.MovementType == "delete" || obj.MovementType == "suspended" {
+		res, _ := QueryRowsBigQuery[BankAccountMovement]("wopta",
+			"inclusive_axa_bank_account",
+			"select * from `wopta."+dataMovement+"` where fiscalCode='"+obj.FiscalCode+"' and guaranteesCode ='"+obj.GuaranteesCode+"'")
+		log.Println(len(res))
+		if len(res) == 0 {
+			return obj, GetErrorJson(400, "Bad request", "insert movement miss")
+		}
+		if obj.StartDate.IsZero() {
+
+			return obj, GetErrorJson(400, "Bad request", "field StartDate miss")
+		}
+		if obj.EndDate.IsZero() {
+			return obj, GetErrorJson(400, "Bad request", "field EndDate miss")
+		}
+	}
+
+	if obj.HypeId == "" {
+		return obj, GetErrorJson(400, "Bad request", "field HypeId miss")
+	}
+	if obj.GuaranteesCode != "next" && obj.GuaranteesCode != "premium" {
+		return obj, GetErrorJson(400, "Bad request", "field GuaranteesCode out of enum")
+	}
+
+	return obj, nil
+}
+func SetScalapayData(obj BankAccountMovement) BankAccountMovement {
+
+	obj.BigStartDate = civil.DateTimeOf(obj.StartDate)
+	obj.BigEndDate = civil.DateTimeOf(obj.EndDate)
+	if obj.GuaranteesCode == "base" {
+		obj.PolicyNumber = "180623"
+		obj.Uid = uuid.New().String()
+		obj.Customer = "Scalapay"
+		obj.Company = "axa"
+		obj.PolicyType = ""
+		obj.PolicyUid = ""
+		obj.AssetType = ""
+		obj.PolicyName = "Scalapay base"
+	}
+
+	obj.CustomerId = obj.HypeId
+	if obj.MovementType == "insert" {
+
+		obj.Status = "active"
+	}
+	if obj.MovementType == "delete" {
+		obj.Status = "delete"
+
+	}
+	if obj.MovementType == "suspended" {
+		obj.Status = "suspended"
+
+	}
+
+	return obj
+}
