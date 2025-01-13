8516946e75384cce11eb93751a98dcc4e40b141c
Filippo Pittoni <filippo.pittoni@gmail.com>
2025-01-13 16:42:05 +0100

    Added routines to create policies

--- _script/init_dev_env.go
+++ _script/init_dev_env.go
@@ -24,6 +24,7 @@ type userEnv struct {
 	WoptaUser   woptaUser
 	DevEnvUsers []models.User
 	NodeGraph   []models.NetworkNode
+	Policies    []models.Policy
 }
 
 var (
@@ -117,28 +118,124 @@ func createNetworkNode(userMail string, role string, parentUid string) models.Ne
 	return node
 }
 
-func generatePoliciesForThisUser(user woptaUser) []models.Policy {
-	/*
-		REMINDER for myself
-		- CustomerUser must be created on the fly with fake data and then attached to policies
+func emptyPolicy(date time.Time) models.Policy {
+	var p models.Policy
+	p.Uid = uniqueID()
+	p.EmitDate = date
 
-		TODO
-			Per ogni agency/agent creati al punto 1 procedere con creazione:
-			•	Polizza pagata
-			•	Polizza eliminata
-			•	Polizza da pagare (emessa attualmente)
-			•	Polizza da pagare (emessa X mesi precedenti)  caso insoluto
-			•	Polizza da firmare
-			•	Proposta da emettere
-			•	Proposta da emettere (con startDate 30 giorni nel passato)
-			•	Proposta in riservato
-			•	Proposta approvata
-			•	Proposta rifiutata
-			•	Quietanza (polizza in draft renew) da pagare con mandato
-			•	Quietanza da pagare senza mandato
-			•	Quietanza eliminata
-			•	Quietanza pagata
-	*/
+	return p
+}
+
+func policyToSign(date time.Time) models.Policy {
+	p := emptyPolicy(date)
+	p.Status = models.PolicyStatusToSign
+	p.StatusHistory = []string{models.PolicyStatusInitLead, models.PolicyStatusProposal,
+		models.PolicyStatusContact, models.PolicyStatusToSign}
+	p.CompanyEmit = true
+	p.IsSign = false
+	p.IsPay = false
+	p.IsDeleted = false
+	p.CompanyEmitted = false
+	p.IsReserved = false
+	p.Annuity = 0
+
+	return p
+}
+
+func policyToPay(date time.Time) models.Policy {
+	p := emptyPolicy(date)
+	p.Status = models.PolicyStatusToPay
+	p.StatusHistory = []string{models.PolicyStatusInitLead, models.PolicyStatusProposal,
+		models.PolicyStatusContact, models.PolicyStatusToSign,
+		models.PolicyStatusSign, models.PolicyStatusToPay}
+	p.CompanyEmit = true
+	p.IsSign = true
+	p.IsPay = false
+	p.IsDeleted = false
+	p.CompanyEmitted = false
+	p.IsReserved = false
+	p.Annuity = 0
+
+	return p
+}
+
+func policyPaid(date time.Time) models.Policy {
+	p := emptyPolicy(date)
+	p.Status = models.PolicyStatusPay
+	p.StatusHistory = []string{models.PolicyStatusInitLead, models.PolicyStatusProposal,
+		models.PolicyStatusContact, models.PolicyStatusToSign,
+		models.PolicyStatusSign, models.PolicyStatusToPay,
+		models.PolicyStatusPay}
+	p.CompanyEmit = true
+	p.IsSign = true
+	p.IsPay = true
+	p.IsDeleted = false
+	p.CompanyEmitted = false
+	p.IsReserved = false
+	p.Annuity = 0
+
+	return p
+}
+
+func policyUnresolved(date time.Time) models.Policy {
+	p := emptyPolicy(date)
+	p.Status = models.PolicyStatusUnsolved
+	p.StatusHistory = []string{models.PolicyStatusInitLead, models.PolicyStatusProposal,
+		models.PolicyStatusContact, models.PolicyStatusToSign,
+		models.PolicyStatusSign, models.PolicyStatusToPay,
+		models.PolicyStatusPay, models.PolicyStatusDraftRenew,
+		models.PolicyStatusToPay, models.PolicyStatusUnsolved}
+	p.CompanyEmit = true
+	p.IsSign = true
+	p.IsPay = false
+	p.IsDeleted = false
+	p.CompanyEmitted = true
+	p.IsReserved = true
+	p.Annuity = 1
+
+	return p
+}
+
+func policyRenewed(date time.Time) models.Policy {
+	p := emptyPolicy(date)
+	p.Status = models.PolicyStatusRenewed
+	p.StatusHistory = []string{models.PolicyStatusInitLead, models.PolicyStatusProposal,
+		models.PolicyStatusContact, models.PolicyStatusToSign,
+		models.PolicyStatusSign, models.PolicyStatusToPay,
+		models.PolicyStatusPay, models.PolicyStatusDraftRenew,
+		models.PolicyStatusToPay, models.PolicyStatusPay,
+		models.PolicyStatusRenewed}
+	p.CompanyEmit = true
+	p.IsSign = true
+	p.IsPay = true
+	p.IsDeleted = false
+	p.CompanyEmitted = true
+	p.IsReserved = true
+	p.Annuity = 1
+
+	return p
+}
+
+func generatePolicies() []models.Policy {
+	var policies []models.Policy
+
+	//PolicyStatusToSign
+	policies = append(policies, policyToSign(time.Now()))
+	policies = append(policies, policyToSign(time.Now().AddDate(0, -3, 0)))
+
+	//PolicyStatusToPay:
+	policies = append(policies, policyToPay(time.Now()))
+
+	//PolicyStatusPay:
+	policies = append(policies, policyPaid(time.Now()))
+
+	//PolicyStatusUnsolved:
+	policies = append(policies, policyUnresolved(time.Now()))
+
+	//PolicyStatusRenewed:
+	policies = append(policies, policyRenewed(time.Now()))
+
+	return policies
 }
 
 func initDevEnvForThisUser(user woptaUser) userEnv {
@@ -167,10 +264,14 @@ func initDevEnvForThisUser(user woptaUser) userEnv {
 	ue.DevEnvUsers = append(ue.DevEnvUsers, agtRUsr)
 	aTRnn := createNetworkNode(agtRUsr.Mail, lib.UserRoleAgent, aYnn.Uid)
 	ue.NodeGraph = append(ue.NodeGraph, aTRnn)
-	//// Customer
-	//cstUsr := createUserWithRole(user, lib.UserRoleCustomer)
-	//ue.DevEnvUsers = append(ue.DevEnvUsers, cstUsr)
 
+	ue.Policies = generatePolicies()
+	cstUsr := createUserWithRole(user, lib.UserRoleCustomer)
+	contr := models.Contractor{Uid: uniqueID(), Name: cstUsr.Name, Surname: cstUsr.Surname, Mail: cstUsr.Mail}
+	for i := 0; i < len(ue.Policies); i++ {
+		ue.Policies[i].Agent = &agtOUsr
+		ue.Policies[i].Contractor = contr
+	}
 	return ue
 }
 
--- _script/init_dev_env_test.go
+++ _script/init_dev_env_test.go
@@ -3,6 +3,8 @@ package _script
 import (
 	"reflect"
 	"testing"
+
+	"github.com/wopta/goworkspace/models"
 )
 
 func TestLoadUsersFromPath(t *testing.T) {
@@ -24,6 +26,18 @@ func TestAddPrefixToEmailAddress(t *testing.T) {
 	expected := "vinz.clortho+keymaster@gozer.evil"
 	if result != expected {
 		t.Fatalf("expected %v got %v", expected, result)
+	}
+}
+
+func TestInitDevEnvForThisUser(t *testing.T) {
+	wu := woptaUser{"Vinz", "Clortho", "vinz.clortho@gozer.evil"}
+	u := models.User{Name: "Vinz"}
+	ue := initDevEnvForThisUser(wu)
+	p := ue.Policies[0]
+	got := p.Agent.Name
+	exp := u.Name
+	if got != exp {
+		t.Fatalf("expected %v got %v", exp, got)
 	}
 }
 
