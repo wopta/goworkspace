f944e5cdc589f1c393a069575a74a50fd1ea68c7
Yousef Hammar <yousef.hammar@wopta.it>
2025-01-15 10:55:54 +0100

    wip: added startDate and duration fields to guarantee-dto.go

--- document/internal/dto/guarantee-dto.go
+++ document/internal/dto/guarantee-dto.go
@@ -1,12 +1,17 @@
 package dto
 
-import "github.com/wopta/goworkspace/models"
+import (
+	"github.com/wopta/goworkspace/document/internal/constants"
+	"github.com/wopta/goworkspace/models"
+)
 
 type GuaranteeDTO struct {
 	Description                string
 	SumInsuredLimitOfIndemnity float64
 	LimitOfIndemnity           float64
 	SumInsured                 float64
+	StartDate                  string
+	Duration                   int64
 }
 
 func newGuaranteeDTO() *GuaranteeDTO {
@@ -15,6 +20,8 @@ func newGuaranteeDTO() *GuaranteeDTO {
 		SumInsuredLimitOfIndemnity: 0,
 		LimitOfIndemnity:           0,
 		SumInsured:                 0,
+		StartDate:                  emptyField,
+		Duration:                   0,
 	}
 }
 
@@ -25,4 +32,10 @@ func (g *GuaranteeDTO) fromPolicy(guarantee models.Guarante) {
 	g.SumInsuredLimitOfIndemnity = guarantee.Value.SumInsuredLimitOfIndemnity
 	g.LimitOfIndemnity = guarantee.Value.LimitOfIndemnity
 	g.SumInsured = guarantee.Value.SumInsured
+	if guarantee.Value.StartDate != nil && !guarantee.Value.StartDate.IsZero() {
+		g.StartDate = guarantee.Value.StartDate.Format(constants.DayMonthYearFormat)
+	}
+	if guarantee.Value.Duration != nil {
+		g.Duration = int64(guarantee.Value.Duration.Year)
+	}
 }
