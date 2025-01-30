f54831d57739fdbf9bce922607367704dc663d41
Diogo Carvalho <diogo.carvalho@wopta.it>
2025-01-30 14:22:01 +0100

    feat: commercial combined base reserved impl

--- reserved/commercial-combined.go
+++ reserved/commercial-combined.go
@@ -3,7 +3,7 @@ package reserved
 import (
 	"log"
 
-	// "github.com/wopta/goworkspace/lib"
+	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/models"
 )
 
@@ -12,19 +12,23 @@ func commercialCombinedReserved(p *models.Policy) (bool, *models.ReservedInfo) {
 	log.Println("[commercialCombinedReserved]")
 
 	var output = ReservedRuleOutput{
-		IsReserved:   false,
+		IsReserved: false,
 		ReservedInfo: &models.ReservedInfo{},
 	}
 
 	output.ReservedInfo.CompanyApproval.Mandatory = true
 
-	// fx := new(models.Fx)
-	// rulesFile := lib.GetRulesFileV2(p.Name, p.ProductVersion, "reserved")
+	fx := new(models.Fx)
+	rulesFile := lib.GetRulesFileV2(p.Name, p.ProductVersion, "reserved")
 
 	input := getCCInputData(p)
 	log.Printf("input data: %+v", string(input))
 
-	return false, nil
+	ruleOutputString, ruleOutput := lib.RulesFromJsonV2(fx, rulesFile, &output, input, nil)
+
+	log.Printf("[lifeReserved] rules output: %s", ruleOutputString)
+
+	return ruleOutput.(*ReservedRuleOutput).IsReserved, ruleOutput.(*ReservedRuleOutput).ReservedInfo
 }
 
 func getCCInputData(p *models.Policy) []byte {
