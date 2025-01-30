78f4315c255dc0afa2bf24390d17b4827dba041d
Diogo Carvalho <diogo.carvalho@wopta.it>
2025-01-30 16:50:22 +0100

    feat: commercial combined reserved input data

--- reserved/commercial-combined.go
+++ reserved/commercial-combined.go
@@ -39,7 +39,31 @@ func getCCInputData(p *models.Policy) []byte {
 		in              = make(map[string]interface{})
 	)
 
+	// enterprise data
 	in["revenue"] = float64(0)
+	in["northAmericanMarket"] = float64(0)
+	// enterprise guarantee
+	in["third-party-recourse"] = float64(0)
+	in["electrical-phenomenon"] = float64(0)
+	in["refrigeration-stock"] = float64(0)
+	in["machinery-breakdown"] = float64(0)
+	in["electronic-equipment"] = float64(0)
+	in["theft"] = float64(0)
+	in["daily-allowance"] = float64(0)
+	in["increased-cost"] = float64(0)
+	in["additional-compensation"] = float64(0)
+	in["loss-rent"] = float64(0)
+	in["third-party-liability-work-providers"] = float64(0)
+	in["product-liability"] = float64(0)
+	in["product-withdrawal"] = float64(0)
+	in["management-organization"] = float64(0)
+	in["cyber"] = float64(0)
+	// building guarantee
+	in["building"] = float64(0)
+	in["rental-risk"] = float64(0)
+	in["machinery"] = float64(0)
+	in["stock"] = float64(0)
+	in["stock-temporary-increase"] = float64(0)
 
 	for _, a := range p.Assets {
 		switch a.Type {
@@ -52,6 +76,22 @@ func getCCInputData(p *models.Policy) []byte {
 
 	if enterpriseAsset.Enterprise != nil {
 		in["revenue"] = enterpriseAsset.Enterprise.Revenue
+		in["northAmericanMarket"] = enterpriseAsset.Enterprise.NorthAmericanMarket
+		for _, g := range enterpriseAsset.Guarantees {
+			in[g.Slug] = g.Value.SumInsuredLimitOfIndemnity
+		}
+	}
+
+	for _, b := range buildingsAssets {
+		for _, g := range b.Guarantees {
+			switch v := in[g.Slug].(type) {
+			case float64:
+				v += g.Value.SumInsuredLimitOfIndemnity
+				in[g.Slug] = v
+			default:
+				log.Printf("%T", v)
+			}
+		}
 	}
 
 	out, err := json.Marshal(in)
