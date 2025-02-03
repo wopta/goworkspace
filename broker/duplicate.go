09e24fb3d11a4361375a0d2b4f9618dc910d4189
Diogo Carvalho <diogo.carvalho@wopta.it>
2025-02-03 12:12:18 +0100

    fix: policy duplicate do not modify original policy

--- broker/duplicate.go
+++ broker/duplicate.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/mohae/deepcopy"
 	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/models"
 	plc "github.com/wopta/goworkspace/policy"
@@ -24,7 +23,7 @@ const (
 func DuplicateFx(_ http.ResponseWriter, r *http.Request) (string, any, error) {
 	var (
 		err            error
-		originalPolicy models.Policy
+		policy models.Policy
 		responseBytes  []byte
 	)
 
@@ -40,50 +39,37 @@ func DuplicateFx(_ http.ResponseWriter, r *http.Request) (string, any, error) {
 
 	policyUid := chi.URLParam(r, "uid")
 
-	if originalPolicy, err = plc.GetPolicy(policyUid, ""); err != nil {
+	if policy, err = plc.GetPolicy(policyUid, ""); err != nil {
 		log.Println("could not retrieve policy from DB")
 		return "", nil, err
 	}
 
-	if originalPolicy.CompanyEmit {
+	if policy.CompanyEmit {
 		log.Println("cannot duplicate already emitted policy")
 		err = errOperationNotAllowed
 		return "", nil, err
 	}
 
-	duplicatedPolicy := deepcopy.Copy(originalPolicy).(models.Policy)
-
 	now := time.Now().UTC()
 
-	originalPolicy.Status = models.PolicyStatusDeleted
-	originalPolicy.StatusHistory = append(originalPolicy.StatusHistory, statusDuplicated, originalPolicy.Status)
-	originalPolicy.DeleteDesc = "Annulata per modifica"
-	originalPolicy.DeleteDate = now
-	originalPolicy.Updated = now
+	policy.Uid = lib.NewDoc(lib.PolicyCollection)
+	policy.ProposalNumber = 0
+	policy.Attachments = nil
+	policy.Status = models.PolicyStatusInit
+	policy.StatusHistory = []string{statusDuplicated, policy.Status}
+	policy.CreationDate = now
+	policy.Updated = now
 
-	duplicatedPolicy.Uid = lib.NewDoc(lib.PolicyCollection)
-	duplicatedPolicy.ProposalNumber = 0
-	duplicatedPolicy.Attachments = nil
-	duplicatedPolicy.Status = models.PolicyStatusInit
-	duplicatedPolicy.StatusHistory = []string{statusDuplicated, duplicatedPolicy.Status}
-	duplicatedPolicy.Updated = now
-
-	if err = lib.SetFirestoreErr(lib.PolicyCollection, originalPolicy.Uid, originalPolicy); err != nil {
-		log.Println("error updating original policy")
-		return "", nil, err
-	}
-	originalPolicy.BigquerySave("")
-
-	if err = lib.SetFirestoreErr(lib.PolicyCollection, duplicatedPolicy.Uid, duplicatedPolicy); err != nil {
+	if err = lib.SetFirestoreErr(lib.PolicyCollection, policy.Uid, policy); err != nil {
 		log.Println("error updating duplicated policy")
 		return "", nil, err
 	}
-	duplicatedPolicy.BigquerySave("")
+	policy.BigquerySave("")
 
-	if responseBytes, err = duplicatedPolicy.Marshal(); err != nil {
+	if responseBytes, err = policy.Marshal(); err != nil {
 		log.Println("error marshalling response")
 		return "", nil, err
 	}
 
-	return string(responseBytes), duplicatedPolicy, err
+	return string(responseBytes), policy, err
 }
