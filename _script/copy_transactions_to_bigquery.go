3d0bab2e19c80d3371560bd7e8080c00d32d8ca1
Filippo Pittoni <filippo.pittoni@gmail.com>
2025-02-12 12:27:00 +0100

    Copy to BigQuery all transactions for all policies

--- _script/copy_transactions_to_bigquery.go
+++ _script/copy_transactions_to_bigquery.go
@@ -1,9 +1,15 @@
 package _script
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/mohae/deepcopy"
+	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/models"
 	"github.com/wopta/goworkspace/transaction"
+	"google.golang.org/api/iterator"
+
 	"time"
 )
 
@@ -25,4 +31,50 @@ func CopyTransactionsToBigQuery() {
 		}
 		transactions[index].BigQuerySave("")
 	}
+}
+
+func CopyAllPoliciesTransactionToBigQuery() error {
+	var policyUids = make([]string, 0)
+
+	queries := lib.Firequeries{
+		Queries: []lib.Firequery{
+			{Field: "isDeleted", Operator: "==", QueryValue: false},
+		},
+	}
+
+	iter, err := queries.FirestoreWherefields(lib.PolicyCollection)
+	if err != nil {
+		return fmt.Errorf("unable to query firestore policies: %w", err)
+	}
+	defer iter.Stop()
+
+	for {
+		doc, err := iter.Next()
+		if errors.Is(err, iterator.Done) {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("unable to iterate over policies: %w", err)
+		}
+
+		var policy models.Policy
+		err = doc.DataTo(&policy)
+		if err != nil {
+			return fmt.Errorf("unable to populate policy: %w", err)
+		}
+
+		policyUids = append(policyUids, policy.Uid)
+	}
+
+	for _, uid := range policyUids {
+		transactions := transaction.GetPolicyTransactions("", uid)
+
+		for index, tr := range transactions {
+			t := deepcopy.Copy(tr).(models.Transaction)
+			t.BigQuerySave("")
+			transactions[index].BigQuerySave("")
+		}
+	}
+
+	return nil
 }
