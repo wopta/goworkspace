92757d0a2306dbf9e3b13c5401149f769076db51
luca <luca.barbieri@wopta.it>
2025-03-27 16:19:35 +0100

    test fatture in cloud

--- test/test.go
+++ test/test.go
@@ -35,7 +35,7 @@ var testRoutes []lib.Route = []lib.Route{
 	{
 		Route:   "/accounting/createinvoice",
 		Handler: lib.ResponseLoggerWrapper(createInvoice),
-		Method:  http.MethodPost,
+		Method:  http.MethodGet,
 		Roles:   []string{},
 	},
 }
