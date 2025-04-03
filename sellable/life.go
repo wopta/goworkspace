c2768f83a04378be4a3a37ff9f1b763a4cf005b2
Luca <luca.frangiamore@wopta.it>
2025-04-03 16:57:28 +0200

    feat: [sellable] logger

--- sellable/life.go
+++ sellable/life.go
@@ -3,10 +3,11 @@ package sellable
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/wopta/goworkspace/network"
 	"io"
-	"log"
 	"net/http"
+
+	"github.com/wopta/goworkspace/lib/log"
+	"github.com/wopta/goworkspace/network"
 
 	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/models"
@@ -20,11 +21,13 @@ func LifeFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error)
 		warrant *models.Warrant
 		err     error
 	)
+	log.AddPrefix("LifeFx")
+	defer log.PopPrefix()
 
-	log.Println("[LifeFx] handler start ----------- ")
+	log.Println("handler start ----------- ")
 
 	body := lib.ErrorByte(io.ReadAll(r.Body))
-	log.Printf("[LifeFx] body: %s", string(body))
+	log.Printf("body: %s", string(body))
 
 	err = json.Unmarshal(body, &policy)
 	if err != nil {
@@ -36,24 +39,24 @@ func LifeFx(w http.ResponseWriter, r *http.Request) (string, interface{}, error)
 	authToken, err := lib.GetAuthTokenFromIdToken(r.Header.Get("Authorization"))
 	lib.CheckError(err)
 
-	log.Println("[LifeFx] loading network node")
+	log.Println("loading network node")
 	networkNode := network.GetNetworkNodeByUid(authToken.UserID)
 	if networkNode != nil {
 		warrant = networkNode.GetWarrant()
 	}
 
-	log.Println("[LifeFx] calling vendibility rules function")
+	log.Println("calling vendibility rules function")
 
 	product, err := Life(policy, authToken.GetChannelByRoleV2(), networkNode, warrant)
 	if err != nil {
-		log.Printf("[LifeFx] vednibility rules error: %s", err.Error())
+		log.ErrorF("vednibility rules error: %s", err.Error())
 		return "", nil, err
 	}
 
 	jsonOut, err := product.Marshal()
 
-	log.Printf("[LifeFx] response: %s", string(jsonOut))
-	log.Println("[LifeFx] handler end -------------------")
+	log.Printf("response: %s", string(jsonOut))
+	log.Println("handler end -------------------")
 
 	return string(jsonOut), product, err
 }
@@ -63,34 +66,36 @@ func Life(policy *models.Policy, channel string, networkNode *models.NetworkNode
 		err     error
 		product *models.Product
 	)
+	log.AddPrefix("Life")
+	defer log.PopPrefix()
 
-	log.Println("[Life] function start -----------")
+	log.Println("function start -----------")
 
-	log.Println("[Life] loading input data")
+	log.Println("loading input data")
 
 	in, err := getInputData(*policy)
 	if err != nil {
-		log.Printf("[Life] error getting input data: %s", err.Error())
+		log.ErrorF("error getting input data: %s", err.Error())
 		return nil, err
 	}
 
-	log.Println("[Life] loading vendibility rules file")
+	log.Println("loading vendibility rules file")
 	rulesFile := lib.GetRulesFileV2(policy.Name, policy.ProductVersion, rulesFilename)
 
-	log.Println("[Life] loading product")
+	log.Println("loading product")
 	product = prd.GetProductV2(policy.Name, policy.ProductVersion, channel, networkNode, warrant)
 	if product == nil {
 		return nil, fmt.Errorf("no product found")
 	}
 
-	log.Println("[Life] applying vendibility rules")
+	log.Println("applying vendibility rules")
 
 	fx := new(models.Fx)
 	_, ruleOutput := lib.RulesFromJsonV2(fx, rulesFile, product, in, nil)
 
 	product = ruleOutput.(*models.Product)
 
-	log.Println("[Life] function end ----------")
+	log.Println("function end ----------")
 
 	return product, nil
 }
