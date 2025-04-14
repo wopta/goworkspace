071b2d75a6672379ae946367259344b445d1bb5a
luca <luca.barbieri@wopta.it>
2025-04-14 11:41:41 +0200

    test fatture in cloud

--- test/invoice.go
+++ test/invoice.go
@@ -34,7 +34,7 @@ func createInvoice(w http.ResponseWriter, r *http.Request) (string, interface{},
 			Date:      time.Now()}}}
 
 	//"inv.CreateInvoice(false, true)
-	accounting.DoInvicePaid(inv, "")
+	accounting.DoInvicePaid(inv, "/test/test.pdf")
 
 	return string(""), nil, nil
 }
