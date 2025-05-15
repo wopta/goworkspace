170bd424e7ac7b5146892c6e6df3d0230211cd34
Luca <luca.frangiamore@wopta.it>
2025-05-15 15:22:52 +0200

    feat: new api in client catnat to download certification

--- quote/catnat/client.go
+++ quote/catnat/client.go
@@ -2,37 +2,49 @@ package catnat
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
-	"github.com/wopta/goworkspace/lib"
 	"github.com/wopta/goworkspace/lib/log"
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 const authEndpoint = "/Identity/connect/token"
-const scope = "emettiPolizza_441-029-007"
 
 type NetClient struct {
 	*http.Client
 }
 
 type INetClient interface {
-	Quote(dto RequestDTO) (response ResponseDTO, err error)
-	Emit(dto RequestDTO) (response any, err error)
+	Quote(dto RequestDTO) (response QuoteResponse, err error)
+	Emit(dto RequestDTO) (response QuoteResponse, err error)
+	Download(numeroPoliza string) (response DownloadResponse, err error)
 }
 
 func NewNetClient() (client *NetClient) {
 	client = &NetClient{}
 	tokenUrl := os.Getenv("NET_BASEURL") + authEndpoint
-	client.Client = lib.ClientCredentials(os.Getenv("NETINS_ID"), os.Getenv("NETINS_SECRET"), scope, tokenUrl)
+	config := clientcredentials.Config{
+		ClientID:     os.Getenv("NETINS_ID"),
+		ClientSecret: os.Getenv("NETINS_SECRET"),
+		Scopes:       []string{"emettiPolizza_441-006-006", "emettiPolizza_441-027-056", "emettiPolizza_441-029-007", "IncassaTitolo_441", "InserisciAllegato_441", "StampaPolizza_441"},
+		TokenURL:     tokenUrl,
+		EndpointParams: url.Values{
+			"grant_type": {"client_credentials"},
+		}}
+
+	client.Client = config.Client(context.Background())
 	return client
 }
 
-func (c *NetClient) Quote(dto RequestDTO) (response ResponseDTO, err error) {
+func (c *NetClient) Quote(dto RequestDTO) (response QuoteResponse, err error) {
 	url := os.Getenv("NET_BASEURL") + "/PolizzeGateway24/emettiPolizza/441-029-007"
 	rBuff := new(bytes.Buffer)
 	log.PrintStruct("request: ", dto)
@@ -66,8 +78,7 @@ func (c *NetClient) Quote(dto RequestDTO) (response ResponseDTO, err error) {
 	return response, nil
 }
 
-func (c *NetClient) Emit(dto RequestDTO) (response any, err error) {
-	//TODO: to fix this
+func (c *NetClient) Emit(dto RequestDTO) (response QuoteResponse, err error) {
 	dto.Emission = "si"
 	url := os.Getenv("NET_BASEURL") + "/PolizzeGateway24/emettiPolizza/441-029-007"
 	rBuff := new(bytes.Buffer)
@@ -100,5 +111,41 @@ func (c *NetClient) Emit(dto RequestDTO) (response any, err error) {
 	//		return response, fmt.Errorf("%+v", response.Errors)
 	//	}
 
+	return response, nil
+}
+
+func (c *NetClient) Download(numeroPolizza string) (response DownloadResponse, err error) {
+	url := os.Getenv("NET_BASEURL") + "/OperationsGateway/StampaPolizzaAppendice/StampaPolizza"
+	rBuff := new(bytes.Buffer)
+	request := DownloadRequest{
+		CodiceCompagnia: "441",
+		NumeroPolizza:   numeroPolizza,
+		TipoOperazione:  "E",
+		DataOperazione:  time.Now(),
+	}
+	err = json.NewEncoder(rBuff).Encode(request)
+	log.PrintStruct("request download", request)
+	if err != nil {
+		return response, err
+	}
+	req, _ := http.NewRequest(http.MethodPost, url, rBuff)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return response, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		errBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return response, err
+		}
+		return response, errors.New(string(errBytes))
+	}
+	if err = json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		log.ErrorF("error decoding catnat response")
+		return response, err
+	}
 	return response, nil
 }
