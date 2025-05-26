c3a712dc0bc6564cfe280f252680004e84f5d85e
Luca <luca.frangiamore@wopta.it>
2025-05-26 10:41:15 +0200

    feat: calculate value for guarantees

--- quote/catnat/dtos.go
+++ quote/catnat/dtos.go
@@ -3,6 +3,7 @@ package catnat
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -388,13 +389,18 @@ func MappingQuoteResponseToGuarantee(quoteResponse QuoteResponse, policy *models
 			guaranteeCodes := strings.Split(guaranteeDetailCatnat.GuaranteeCode, "/")
 			currentGuaranteeCode, currentGuaranteeValueCode = guaranteeCodes[0], "/"+guaranteeCodes[1]
 			guarantee := getGuarantee(policy, currentGuaranteeCode)
+			value, e := strconv.ParseFloat(fmt.Sprintf("%.2f", guaranteeDetailCatnat.GuaranteeGross/100*quoteResponse.AnnualGross), 2)
+			if e != nil {
+				//should never happened
+				panic(e)
+			}
 			switch currentGuaranteeValueCode {
 			case buildingCode:
-				guarantee.Value.SumInsuredLimitOfIndemnity = guaranteeDetailCatnat.GuaranteeGross
+				guarantee.Value.SumInsuredLimitOfIndemnity = value
 			case contentCode:
-				guarantee.Value.SumInsured = guaranteeDetailCatnat.GuaranteeGross
+				guarantee.Value.SumInsured = value
 			case stockCode:
-				guarantee.Value.LimitOfIndemnity = guaranteeDetailCatnat.GuaranteeGross
+				guarantee.Value.LimitOfIndemnity = value
 
 			}
 		}
