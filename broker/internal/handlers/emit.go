95de01b5beee2d5b61465da7ec2527d14a27f666
Luca <luca.frangiamore@wopta.it>
2025-06-04 12:54:17 +0200

    fix: assign a placeholder in CompanyCode

--- broker/internal/handlers/emit.go
+++ broker/internal/handlers/emit.go
@@ -81,6 +81,7 @@ func emitBaseNoSequence(state bpmn.StorageData) error {
 	now := time.Now().UTC()
 
 	policy.CompanyEmit = true
+	policy.CodeCompany = models.CatNatProduct
 	policy.CompanyEmitted = false
 	policy.EmitDate = now
 	policy.BigEmitDate = civil.DateTimeOf(now)
