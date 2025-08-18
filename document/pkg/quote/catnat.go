41da8192abd9d1eda0826a57d7280dbec8b20c08
Luca <luca.frangiamore@wopta.it>
2025-08-18 12:50:46 +0200

    feat: change color sentence in document catnat

--- document/pkg/quote/catnat.go
+++ document/pkg/quote/catnat.go
@@ -338,7 +338,17 @@ func (c *CatnatGenerator) addFrazionamento() {
 }
 
 func (c *CatnatGenerator) addSetInformativoInfo() {
-	c.engine.WriteText(c.engine.GetTableCell("Il presente preventivo non ha validità di proposta assicurativa. Ha valore esclusivamente nel giorno di emissione e non impegna la compagnia alla sottoscrizione del rischio.", constants.BoldFontStyle))
+	c.engine.WriteText(c.engine.GetTableCell("Il presente preventivo non ha validità di proposta assicurativa.", constants.BoldFontStyle))
+	c.engine.WriteText(domain.TableCell{
+		Text:      "Ha valore esclusivamente nel giorno di emissione e non impegna la compagnia alla sottoscrizione del rischio.",
+		Fill:      true,
+		FontStyle: constants.BoldFontStyle,
+		FillColor: constants.YellowColor,
+		Height:    constants.CellHeight,
+		FontColor: constants.BlackColor,
+		FontSize:  constants.RegularFontSize,
+		Align:     constants.LeftAlign,
+	})
 	c.engine.NewLine(2)
 	c.engine.WriteText(c.engine.GetTableCell("Prima della sottoscrizione leggere il set informativo.", constants.BoldFontStyle))
 }
