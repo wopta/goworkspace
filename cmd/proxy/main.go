3f64d6dfddea927f031969c6adbfa4ec1354c60f
Luca <luca.frangiamore@wopta.it>
2025-09-12 14:24:25 +0200

    feat: proxy  server to run all functions

---
+++ cmd/proxy/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+var ALL_FUNCTIONS []string = []string{
+	"Auth",
+	"Broker",
+	"Callback",
+	"Claim",
+	"Companydata",
+	"Enrich",
+	"Form",
+	"Mail",
+	"Mga",
+	"Network",
+	"Partnership",
+	"Payment",
+	"Policy",
+	"Question",
+	"Quote",
+	"Renew",
+	"Reserved",
+	"Rules",
+	"Sellable",
+	"Transaction",
+	"User",
+	"Inclusive",
+	"Document",
+}
+
+func GetLocalIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddress := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddress.IP
+}
+func main() {
+	var bind map[string]string = make(map[string]string)
+	start := 8080
+	var serversErrors []string
+	for i, function := range ALL_FUNCTIONS {
+		cmd := exec.Command(os.Getenv("GOWORKSPACE") + "/../bin/api")
+		cmd.Env = append(cmd.Env, "FUNCTION_TARGET="+function, " ")
+		cmd.Env = append(cmd.Env, "PORT="+fmt.Sprint(start+i+1))
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		bind[function] = fmt.Sprint(start + i + 1)
+		if e := cmd.Start(); e != nil {
+			serversErrors = append(serversErrors, e.Error())
+		}
+		defer cmd.Process.Kill()
+	}
+	time.Sleep(time.Second * 2)
+	fmt.Printf("Server started %s:%v\n", GetLocalIP(), start)
+	if len(serversErrors) > 0 {
+		fmt.Printf("Errors: %v\n", serversErrors)
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Authorization,Content-Type,X-Firebase-Appcheck")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Max-Age", "600")
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		url := strings.Split(r.URL.String(), "/")[1]
+		url = strings.ToUpper(string(url[0])) + url[1:]
+		url = "http://localhost:" + bind[url] + r.URL.String()
+		nR, _ := http.NewRequest(r.Method, url, r.Body)
+		nR.Header = r.Header
+		nR.Body = r.Body
+		nR.Host = r.Host
+		client := &http.Client{
+			Timeout: time.Second * 10, // Timeout each requests
+		}
+		resp, e := client.Do(nR)
+		if e != nil {
+			panic(e.Error())
+			w.WriteHeader(resp.StatusCode)
+			w.Write([]byte(e.Error()))
+			return
+		}
+		defer resp.Body.Close()
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Content-Type", r.Header.Get("Content-Type"))
+		w.WriteHeader(resp.StatusCode)
+		body, _ := ioutil.ReadAll(resp.Body)
+		w.Write(body)
+
+	})
+
+	http.ListenAndServe(":"+fmt.Sprint(start), nil)
+
+}
