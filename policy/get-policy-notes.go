5489d3662e8fff6b90242d2958767647e0d3849a
Luca <luca.frangiamore@wopta.it>
2025-09-19 14:07:38 +0200

    feat: filter notes that arent  readable by user

--- policy/get-policy-notes.go
+++ policy/get-policy-notes.go
@@ -3,17 +3,30 @@ package policy
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
 
 	"github.com/go-chi/chi/v5"
+	"gitlab.dev.wopta.it/goworkspace/lib"
 	"gitlab.dev.wopta.it/goworkspace/models"
 )
 
 func getPolicyNotes(w http.ResponseWriter, r *http.Request) (string, any, error) {
 	policyUid := chi.URLParam(r, "policyUid")
-	GetPolicy(policyUid)
+	p, e := GetPolicy(policyUid)
+	if e != nil {
+		return "", nil, e
+	}
+
 	notes, err := models.GetPolicyNotes(policyUid)
 	if err != nil {
 		return "", "", err
+	}
+	idToken := r.Header.Get("Authorization")
+	user, err := lib.GetAuthTokenFromIdToken(idToken)
+	if user.Role == lib.UserRoleAgency || user.Role == lib.UserRoleAgent {
+		notes.Notes = slices.DeleteFunc(notes.Notes, func(element models.PolicyNote) bool {
+			return (element.ReadableByProducer == false) && user.UserID == p.ProducerUid
+		})
 	}
 	res, err := json.Marshal(notes)
 	if err != nil {
