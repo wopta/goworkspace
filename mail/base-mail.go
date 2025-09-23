9f7590270a104b6c41645b77dd283991fdba2ab2
Luca <luca.frangiamore@wopta.it>
2025-09-23 10:45:05 +0200

    feat: append subject if dev in email

--- mail/base-mail.go
+++ mail/base-mail.go
@@ -1,11 +1,17 @@
 package mail
 
+import env "gitlab.dev.wopta.it/goworkspace/lib/environment"
+
 func SendBaseEmail(body string, subject string, email string) {
 	var mailRequest MailRequest
 	mailRequest.IsHtml = true
 	mailRequest.FromAddress = AddressAnna
 	mailRequest.To = []string{email}
-	mailRequest.Subject = subject
+	if env.IsDevelopment() || env.IsLocal() {
+		mailRequest.Subject = "dev: "
+	}
+	mailRequest.Subject += subject
+
 	mailRequest.Message = body
 	mailRequest.Message = mailRequest.Message + ` 
 		<p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#000000;font-size:14px"><br></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#000000;font-size:14px">A presto,</p>
